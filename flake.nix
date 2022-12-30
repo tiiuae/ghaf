dbcb90ef0f45b4e0691d2dcf0aadc21aeb0df452
Mika Tammi <mika.tammi@unikie.com>
2022-12-30 13:42:02 +0200

    flake: Add substituters
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Ghaf - documentation for TII SSRC Common Tech";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.vedenemo.dev"
+    ];
+    extra-trusted-public-keys = [
+      "cache.vedenemo.dev:RGHheQnb6rXGK5v9gexJZ8iWTPX6OcSeS56YeXYzOcg="
+    ];
+  };
+
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
