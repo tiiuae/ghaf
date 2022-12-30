eb2cfecb0967ea330f2b66ea3879ce461b0c9f6c
Mika Tammi <mika.tammi@unikie.com>
2022-12-30 13:58:17 +0200

    flake: Added simple targets
    Add simple output targets for x86_64 and NVIDIA Jetson AGX Orin, which
    don't do anything yet.
    
    Add nixos-generators, microvm and jetpack-nixos to flake inputs.
    
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Ghaf - documentation for TII SSRC Common Tech";
+  description = "Ghaf - documentation and implementation for TII SSRC Common Tech";
 
   nixConfig = {
     extra-substituters = [
@@ -10,26 +10,50 @@
     ];
   };
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    microvm = {
+      url = "github:astro/microvm.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    jetpack-nixos = {
+      url = "github:anduril/jetpack-nixos";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    nixos-generators,
+    microvm,
+    jetpack-nixos,
   }: let
     systems = with flake-utils.lib.system; [
       x86_64-linux
       aarch64-linux
     ];
   in
-    flake-utils.lib.eachSystem systems (system: {
-      packages = let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        doc = pkgs.callPackage ./doc.nix {};
-      };
+    # Combine list of attribute sets together
+    nixpkgs.lib.foldr nixpkgs.lib.recursiveUpdate {} [
+      # Documentation
+      (flake-utils.lib.eachSystem systems (system: {
+        packages = let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          doc = pkgs.callPackage ./doc.nix {};
+        };
 
-      formatter = nixpkgs.legacyPackages.${system}.alejandra;
-    });
+        formatter = nixpkgs.legacyPackages.${system}.alejandra;
+      }))
+      # Final target images
+      (import ./targets.nix {inherit self nixos-generators microvm jetpack-nixos;})
+    ];
 }
---
+++ targets.nix
@@ -0,0 +1,30 @@
+{
+  self,
+  nixos-generators,
+  microvm,
+  jetpack-nixos,
+}: {
+  packages.x86_64-linux.vm = nixos-generators.nixosGenerate {
+    system = "x86_64-linux";
+    modules = [
+      microvm.nixosModules.host
+    ];
+    format = "vm";
+  };
+
+  packages.x86_64-linux.default = self.packages.x86_64-linux.vm;
+
+  packages.aarch64-linux.nvidia-jetson-orin = nixos-generators.nixosGenerate {
+    system = "aarch64-linux";
+    modules = [
+      jetpack-nixos.nixosModules.default
+      modules/hardware/nvidia-jetson-orin.nix
+
+      microvm.nixosModules.host
+    ];
+    format = "raw-efi";
+  };
+
+  # Using Orin as a default aarch64 target for now
+  packages.aarch64-linux.default = self.packages.aarch64-linux.nvidia-jetson-orin;
+}
