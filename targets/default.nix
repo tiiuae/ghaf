057e1725c36e01b9577773e456fae177d326ab1b
Mika Tammi <mika.tammi@unikie.com>
2023-01-10 15:05:31 +0200

    Refactor targets.nix
    * Refactor targets.nix into targets-subdirectory
    * Separate nvidia-jetson-orin configuration from targets.nix
    * Add nixosConfiguration.nvidia-jetson-orin, to be able to use
      nixos-rebuild to build the configuration
    
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- targets/default.nix
+++ targets/default.nix
@@ -8,24 +8,20 @@
     system = "x86_64-linux";
     modules = [
       microvm.nixosModules.host
-      configurations/host/configuration.nix
-      modules/development/authentication.nix
+      ../configurations/host/configuration.nix
+      ../modules/development/authentication.nix
     ];
     format = "vm";
   };
 
   packages.x86_64-linux.default = self.packages.x86_64-linux.vm;
 
-  packages.aarch64-linux.nvidia-jetson-orin = nixos-generators.nixosGenerate {
-    system = "aarch64-linux";
-    modules = [
-      jetpack-nixos.nixosModules.default
-      modules/hardware/nvidia-jetson-orin.nix
-
-      microvm.nixosModules.host
-    ];
-    format = "raw-efi";
-  };
+  packages.aarch64-linux.nvidia-jetson-orin = nixos-generators.nixosGenerate (
+    import ./nvidia-jetson-orin.nix {inherit jetpack-nixos microvm;}
+    // {
+      format = "raw-efi";
+    }
+  );
 
   # Using Orin as a default aarch64 target for now
   packages.aarch64-linux.default = self.packages.aarch64-linux.nvidia-jetson-orin;
---
+++ targets/nvidia-jetson-orin.nix
@@ -0,0 +1,12 @@
+{
+  jetpack-nixos,
+  microvm,
+}: {
+  system = "aarch64-linux";
+  modules = [
+    jetpack-nixos.nixosModules.default
+    ../modules/hardware/nvidia-jetson-orin.nix
+
+    microvm.nixosModules.host
+  ];
+}
