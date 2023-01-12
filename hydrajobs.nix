1727b800b197fc25572096086cf4896f45a22acb
Mika Tammi <mika.tammi@unikie.com>
2023-01-12 22:07:54 +0200

    Add Hydra Jobs for doc-packages
    Create Hydra Jobs for packages:
    * x86_64-linux.doc
    * aarch64-linux.doc
    
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- hydrajobs.nix
+++ hydrajobs.nix
@@ -2,5 +2,10 @@
   hydraJobs = {
     vm.x86_64-linux = self.packages.x86_64-linux.vm;
     nvidia-jetson-orin.aarch64-linux = self.packages.aarch64-linux.nvidia-jetson-orin;
+
+    doc = {
+      x86_64-linux = self.packages.x86_64-linux.doc;
+      aarch64-linux = self.packages.aarch64-linux.doc;
+    };
   };
 }
