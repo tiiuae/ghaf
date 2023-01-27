10b379307429a809cbe72a8cc82ed0286fcaba52
Mika Tammi <mika.tammi@unikie.com>
2023-01-27 14:02:33 +0200

    nvidia-jetson-orin: GICv3 devicetree patch
    Patch to NVIDIA Tegra kernel's devicetree to make rust-vmm -based
    hypervisors work on NVIDIA Jetson Orin hardware
    
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- modules/hardware/nvidia-jetson-orin.nix
+++ modules/hardware/nvidia-jetson-orin.nix
@@ -5,4 +5,17 @@
     carrierBoard = "devkit";
     modesetting.enable = true;
   };
+
+  boot.kernelPatches = [
+    # TODO: Remove when this patch gets merged to mainline.
+    #       Patch to devicetree for getting rust-vmm based VMMs to work on
+    #       NVIDIA Jetson Orin.
+    {
+      name = "gicv3-patch";
+      patch = pkgs.fetchpatch {
+        url = "https://github.com/OE4T/linux-tegra-5.10/commit/9ca6e31d17782e0cf5249eb59f71dcd7d8903303.patch";
+	sha256 = "sha256-PzEQO6Jh/kkoGu329LCYdhdR8mNmo6KGKKVKOeMRZrI=";
+      };
+    }
+  ];
 }
