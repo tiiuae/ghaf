ad07947fbeba1eb3520f7005f12af65629abb064
Mika Tammi <mika.tammi@unikie.com>
2023-02-10 17:25:35 +0200

    Fix microvm.nix on x86_64
    microvms should now work both on x86_64-linux and aarch64-linux.
    
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     };
     microvm = {
       # TODO: change back to url = "github:astro/microvm.nix";
-      url = "github:mikatammi/microvm.nix/wip_hacks";
+      url = "github:mikatammi/microvm.nix/wip_hacks_2";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
