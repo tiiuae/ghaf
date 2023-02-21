284b9f3ced4a8809804aa8f5766671186d9dc1da
Mika Tammi <mika.tammi@unikie.com>
2023-02-21 22:13:45 +0200

    WIP: Use systemd-boot to load devicetree
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- modules/hardware/nvidia-jetson-orin.nix
+++ modules/hardware/nvidia-jetson-orin.nix
@@ -1,6 +1,6 @@
 # Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{...}: {
+{config, ...}: {
   hardware.nvidia-jetpack = {
     enable = true;
     som = "orin-agx";
@@ -10,8 +10,19 @@
 
   nixpkgs.hostPlatform.system = "aarch64-linux";
 
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
-    systemd-boot.enable = true;
+  hardware.deviceTree = {
+    enable = true;
+    name = "tegra234-p3701-0000-p3737-0000.dtb";
+  };
+
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    # TODO: Maybe add store path or some unique identifier to the filename
+    extraFiles."dtbs/${config.hardware.deviceTree.name}" = "${config.hardware.deviceTree.package}/${config.hardware.deviceTree.name}";
+    extraInstallCommands = ''
+      default_cfg=$(cat /boot/loader/loader.conf | grep default | awk '{print $2}')
+      echo "devicetree /dtbs/${config.hardware.deviceTree.name}" >> /boot/loader/entries/$default_cfg
+    '';
   };
 }
