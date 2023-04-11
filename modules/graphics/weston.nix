32b7e77566afe1436dffd05fc50ecae420100e0d
Ivan Nikolaenko <ivan.nikolaenko@unikie.com>
2023-04-11 16:23:12 +0300

    weston: Run weston session as a user, not root
    Run weston after log in:
    systemctl --user start weston.service
    
    This change makes possible to run weston as a user (the correct
    way). It allows user to run graphical apps without sudo, which is
    crucial for chromium-based apps (electron, gala, etc).
    
    Signed-off-by: Ivan Nikolaenko <ivan.nikolaenko@unikie.com>

--- modules/graphics/weston.nix
+++ modules/graphics/weston.nix
@@ -9,5 +9,112 @@
   environment.noXlibs = false;
   environment.systemPackages = with pkgs; [
     weston
+    # Seatd is needed to manage log-in process for weston
+    seatd
   ];
+
+  # Next 4 services/targets are taken from official weston documentation:
+  # https://wayland.pages.freedesktop.org/weston/toc/running-weston.html
+  #
+  # To run weston, after log-in to VT or SSH run:
+  # systemctl --user start weston.service
+  #
+  # I am pretty sure it is possible to have it running automatically, I just
+  # haven't found the way yet.
+
+  # Weston socket
+  systemd.user.sockets."weston" = {
+    unitConfig = {
+      Description = "Weston, a Wayland compositor";
+      Documentation = "man:weston(1) man:weston.ini(5)";
+    };
+    socketConfig = {
+      ListenStream = "%t/wayland-0";
+    };
+    wantedBy = ["weston.service"];
+  };
+
+  # Weston service
+  systemd.user.services."weston" = {
+    enable = true;
+    description = "Weston, a Wayland compositor, as a user service TEST";
+    documentation = ["man:weston(1) man:weston.ini(5)" "https://wayland.freedesktop.org/"];
+    requires = ["weston.socket"];
+    after = ["weston.socket" "ghaf-session.service"];
+    serviceConfig = {
+      # Previously there was "notify" type, but for some reason
+      # systemd kills weston.service because of timeout (even if it is disabled).
+      # "simple" works pretty well, so let's leave it.
+      Type = "simple";
+      #TimeoutStartSec = "60";
+      #WatchdogSec = "20";
+      # Defaults to journal
+      StandardOutput = "journal";
+      StandardError = "journal";
+      ExecStart = "${pkgs.weston}/bin/weston";
+    };
+  };
+
+  systemd.user.targets."ghaf-session" = {
+    description = "Ghaf graphical session";
+    bindsTo = ["ghaf-session.target"];
+    before = ["ghaf-session.target"];
+  };
+
+  systemd.services."ghaf-session" = {
+    description = "Ghaf graphical session";
+
+    # Make sure we are started after logins are permitted.
+    after = ["systemd-user-sessions.service"];
+
+    # if you want you can make it part of the graphical session
+    #Before=graphical.target
+
+    # not necessary but just in case
+    #ConditionPathExists=/dev/tty7
+
+    serviceConfig = {
+      Type = "simple";
+      Environment = "XDG_SESSION_TYPE=wayland";
+      ExecStart = "${pkgs.systemd}/bin/systemctl --wait --user start ghaf-session.target";
+
+      # The user to run the session as. Pick one!
+      User = "ghaf";
+      Group = "ghaf";
+
+      # Set up a full user session for the user, required by Weston.
+      PAMName = "${pkgs.shadow}/bin/login";
+
+      # A virtual terminal is needed.
+      TTYPath = "/dev/tty7";
+      TTYReset = "yes";
+      TTYVHangup = "yes";
+      TTYVTDisallocate = "yes";
+
+      # Try to grab tty .
+      StandardInput = "tty-force";
+
+      # Defaults to journal, in case it doesn't adjust it accordingly
+      #StandardOutput=journal
+      StandardError = "journal";
+
+      # Log this user with utmp, letting it show up with commands 'w' and 'who'.
+      UtmpIdentifier = "tty7";
+      UtmpMode = "user";
+    };
+    wantedBy = ["multi-user.target"];
+  };
+
+  # systemd service for seatd
+  systemd.services."seatd" = {
+    description = "Seat management daemon";
+    documentation = ["man:seatd(1)"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.seatd}/bin/seatd -g video";
+      Restart = "always";
+      RestartSec = "1";
+    };
+    wantedBy = ["multi-user.target"];
+  };
 }
