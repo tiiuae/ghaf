d211902a02a51e48f2335854e78ea97986565a9e
Ivan Nikolaenko <ivan.nikolaenko@unikie.com>
2023-04-24 17:37:46 +0300

    Add GALA app as an example of user application
    Signed-off-by: Ivan Nikolaenko <ivan.nikolaenko@unikie.com>

---
+++ user-apps/default.nix
@@ -0,0 +1,9 @@
+# Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{pkgs, ...}: {
+  nixpkgs.overlays = [
+    (final: prev: {
+      gala-app = pkgs.callPackage ./gala {};
+    })
+  ];
+}
