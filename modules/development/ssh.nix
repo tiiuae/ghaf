7c5221c7eea725eacb1b4b341a7f08ad02c9a6ae
Brian McGillion <bmg.avoin@gmail.com>
2023-04-27 17:00:53 +0400

    Add developer key for ssh
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- modules/development/ssh.nix
+++ modules/development/ssh.nix
@@ -3,7 +3,7 @@
 {pkgs, ...}: let
   authorizedKeys = [
     # Add your SSH Public Keys here
-    # "ssh-ed25519 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA user@host"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIA/pwHnzGNM+ZU4lANGROTRe2ZHbes7cnZn72Oeun/MCAAAABHNzaDo= brian@arcadia"
   ];
 in {
   services.openssh.enable = true;
