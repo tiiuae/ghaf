277464b4de8bf91fecffa2cdaa87fe4dbae9ce12
Marko Lindqvist <marko.lindqvist@unikie.com>
2023-05-10 07:51:50 +0000

    hydrajobs: Add x86 VM build
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

--- hydrajobs.nix
+++ hydrajobs.nix
@@ -4,5 +4,6 @@
   hydraJobs = {
     generic-x86_64-debug.x86_64-linux = self.packages.x86_64-linux.generic-x86_64-debug;
     nvidia-jetson-orin-debug.aarch64-linux = self.packages.aarch64-linux.nvidia-jetson-orin-debug;
+    intel-vm-debug.x86_64-linux = self.packages.x86_64-linux.vm-debug;
   };
 }
