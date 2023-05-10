1d275c70a826d66e6b6a78243b60c259e3794939
Ivan Nikolaenko <ivan.nikolaenko@unikie.com>
2023-05-10 11:50:00 +0300

    Rename intel-nuc target to generic-x86_64
    Generic x86_64 is tested and approved to work with the Ghaf
    
    Signed-off-by: Ivan Nikolaenko <ivan.nikolaenko@unikie.com>

--- hydrajobs.nix
+++ hydrajobs.nix
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 {self}: {
   hydraJobs = {
-    intel-nuc-debug.x86_64-linux = self.packages.x86_64-linux.intel-nuc-debug;
+    generic-x86_64-debug.x86_64-linux = self.packages.x86_64-linux.generic-x86_64-debug;
     nvidia-jetson-orin-debug.aarch64-linux = self.packages.aarch64-linux.nvidia-jetson-orin-debug;
   };
 }
