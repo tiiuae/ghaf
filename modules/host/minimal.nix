dc623ece31d11f4247f290abe421724f32aefe3f
remimimimimi <remimimimimi@protonmail.com>
2023-05-17 16:01:15 +0300

    Implement minification module
    Signed-off-by: Valentin Kharin <valentin.kharin@unikie.com>

--- modules/host/minimal.nix
+++ modules/host/minimal.nix
@@ -1,11 +1,21 @@
 {
   pkgs,
-  modulesPath,
+  lib,
   ...
-}: {
-  imports = [
-    (modulesPath + "/profiles/minimal.nix")
-  ];
+}:
+with lib; {
+  environment.noXlibs = mkDefault true;
+
+  documentation.enable = mkDefault false;
+
+  documentation.nixos.enable = mkDefault false;
+
+  programs.command-not-found.enable = mkDefault false;
+
+  xdg.autostart.enable = mkDefault false;
+  xdg.icons.enable = mkDefault false;
+  xdg.mime.enable = mkDefault false;
+  xdg.sounds.enable = mkDefault false;
 
   appstream.enable = false;
 
