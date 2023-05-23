4208fbc16ce8193d41f9924139a41044945783bb
Marko Lindqvist <marko.lindqvist@unikie.com>
2023-05-23 12:07:51 +0000

    hydrajobs: Add IMX8QM build
    Signed-off-by: Marko Lindqvist <marko.lindqvist@unikie.com>

--- hydrajobs.nix
+++ hydrajobs.nix
@@ -5,5 +5,6 @@
     generic-x86_64-debug.x86_64-linux = self.packages.x86_64-linux.generic-x86_64-debug;
     nvidia-jetson-orin-debug.aarch64-linux = self.packages.aarch64-linux.nvidia-jetson-orin-debug;
     intel-vm-debug.x86_64-linux = self.packages.x86_64-linux.vm-debug;
+    imx8qm-mek-debug.aarch64-linux = self.packages.aarch64-linux.imx8qm-mek-debug;
   };
 }
