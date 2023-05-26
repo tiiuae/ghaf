cef2002935bd51a2b24293270241ad5a2ba8b203
Mika Tammi <mika.tammi@unikie.com>
2023-05-26 18:42:44 +0300

    Pin GCC version used to build OP-TEE to GCC 9
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- modules/hardware/nvidia-jetson-orin/mk-flash-script.nix
+++ modules/hardware/nvidia-jetson-orin/mk-flash-script.nix
@@ -17,6 +17,19 @@
   inherit (jetpack-nixos.legacyPackages.aarch64-linux) bspSrc l4tVersion;
   inherit
     (pkgs.callPackages (jetpack-nixos + "/optee.nix") {
+      # Taken from
+      # https://github.com/anduril/jetpack-nixos/commit/8200da1d7982a36c83fd9000bdc581a9724c9e1c
+      #
+      # Nvidia's recommended toolchain is gcc9:
+      # https://nv-tegra.nvidia.com/r/gitweb?p=tegra/optee-src/nv-optee.git;a=blob;f=optee/atf_and_optee_README.txt;h=591edda3d4ec96997e054ebd21fc8326983d3464;hb=5ac2ab218ba9116f1df4a0bb5092b1f6d810e8f7#l33
+      #
+      # https://github.com/anduril/jetpack-nixos/pull/98
+      # https://github.com/anduril/jetpack-nixos/issues/96
+      #
+      # NOTE: Using newer version of gcc resulted in unbootable system.
+      #
+      stdenv = pkgs.gcc9Stdenv;
+
       inherit l4tVersion bspSrc;
     })
     buildTOS
