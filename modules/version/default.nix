f32b9c2c52f4028a9aa6f4ccf9f4dcff84027950
Mika Tammi <mika.tammi@unikie.com>
2023-06-13 19:29:49 +0300

    Add ghaf-version executable
    Add ghaf-version executable which will print version information to
    stdout.
    
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

---
+++ modules/version/default.nix
@@ -0,0 +1,18 @@
+# Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# Generates an executable called ghaf-version which will print version
+# information to stdout
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  ghafVersion = pkgs.writeShellScriptBin "ghaf-version" ''
+    echo "${lib.ghaf.version}"
+  '';
+in {
+  environment.systemPackages = [
+    ghafVersion
+  ];
+}
