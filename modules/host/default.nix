0be030ae662724b3e05a17a64e13e88c2468e65b
Brian McGillion <bmg.avoin@gmail.com>
2023-06-15 18:03:29 +0400

    Revert "Implement minification module"
    This reverts commit 432bbd34b95123a16d10fac7ff77ca89186875ad.

--- modules/host/default.nix
+++ modules/host/default.nix
@@ -4,60 +4,18 @@
   self,
   microvm,
   netvm,
-}: {
-  lib,
-  config,
-  pkgs,
-  ...
-} @ args: let
-  cfg = config.ghaf.host.minification;
-in
-  with lib; {
-    options.ghaf.host.minification = with lib; {
-      reduceProfile = mkEnableOption "reduced profile";
-      disableNetwork = mkEnableOption "disableing of network";
-      disableGetty = mkEnableOption "disabling of getty";
-      removeNix = mkEnableOption "nix tooling";
-      minimalKernelConfig = {
-        enable = mkEnableOption "minimal kernel config";
-        structuredExtraConfig = mkOption {
-          default = types.attrs;
-          example = literalExpression ''
-            with lib.kernel; {
-              SCHED_MUQSS = yes;
-            }'';
-          description = "Extra config for the kernel. See https://nixos.wiki/wiki/Linux_kernel#Custom_configuration.";
-          type = types.attrs;
-        };
-      };
-    };
+}: {lib, ...}: {
+  imports = [
+    (import ./minimal.nix)
 
-    # HACK: import other modules with explicit passing of arguments, otherwise import
-    # cause infinite recursion.
-    config = lib.mkMerge [
-      {
-        networking.hostName = "ghaf-host";
-        system.stateVersion = lib.trivial.release;
-      }
-      (import ../overlays/custom-packages.nix {inherit lib pkgs;})
-      (lib.mkIf cfg.reduceProfile (import ./minimal.nix {inherit pkgs lib;}))
-      (lib.mkIf (!cfg.disableNetwork) (import ./networking.nix {}))
-      (lib.mkIf cfg.disableGetty {
-        # XXX: Incompatable with `services.getty` options.
-        systemd.services."getty@".enable = lib.mkForce false;
-      })
-      (lib.mkIf cfg.removeNix {
-        # TODO: Patch raw-efi image build script to support building without relying on
-        # image nix tools (replace with packages from nixpkgs).
-        nix.enable = false;
-      })
-      (lib.mkIf cfg.minimalKernelConfig.enable {
-        boot.kernelPatches = lib.singleton {
-          name = "minimal";
-          patch = null;
-          extraStructuredConfig = cfg.minimalKernelConfig.structuredExtraConfig;
-        };
-      })
-    ];
-  }
+    microvm.nixosModules.host
 
+    ../overlays/custom-packages.nix
+
+    (import ./microvm.nix {inherit self netvm;})
+    ./networking.nix
+  ];
+
+  networking.hostName = "ghaf-host";
+  system.stateVersion = lib.trivial.release;
+}
