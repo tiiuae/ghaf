bf384151d1ff3e0c7a0d8b256fc8b2273dd5f138
Brian McGillion <bmg.avoin@gmail.com>
2023-06-16 17:19:07 +0300

    Add default module template
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

---
+++ templates/modules/default.nix
@@ -0,0 +1,31 @@
+# Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# A template for creating modules for Ghaf
+# for more information on writing templates please refer to the manual
+#
+# https://nixos.org/manual/nixos/stable/index.html#sec-writing-modules
+#
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: let
+  # inherit (builtins) A B C;
+  # inherit (lib) D E F;
+  # inherit (lib.ghaf) G H I;
+  cfg = config.ghaf.X.Y;
+in
+  with lib; {
+    imports = [
+    ];
+
+    options.ghaf.X.Y = {
+      enable = mkEnableOption "Option";
+    };
+
+    config = mkIf cfg.enable {};
+  }
