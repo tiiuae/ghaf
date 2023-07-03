a7d55ee254586c1251c0d93c9a7d06047c940dd1
Alexander V. Nikolaev <avn@avnik.info>
2023-07-03 17:41:09 +0300

    switch to nixpkgs branch, which contains recent fixes
    Signed-off-by: Alexander V. Nikolaev <avn@avnik.info>

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:avnik/nixpkgs/ghaf-23.05";
     flake-utils.url = "github:numtide/flake-utils";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
