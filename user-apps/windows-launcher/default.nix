97126ac7bf4cfc09626bd4042251464de0f6cefe
Yuri Nesterov <yuriy.nesterov@unikie.com>
2023-07-04 17:27:17 +0300

    Add windows launcher for x86_64
    Signed-off-by: Yuri Nesterov <yuriy.nesterov@unikie.com>

--- user-apps/windows-launcher/default.nix
+++ user-apps/windows-launcher/default.nix
@@ -4,76 +4,131 @@
   stdenvNoCC,
   pkgs,
   lib,
+  stdenv,
   ...
 }: let
+  ovmfPrefix =
+    if stdenv.isx86_64
+    then "OVMF"
+    else if stdenv.isAarch64
+    then "AAVMF"
+    else throw "Unsupported architecture";
   windowsLauncher =
     pkgs.writeShellScript
     "windows-launcher"
-    ''
-      if [ $# -eq 0 ]; then
+    (''
+        IMG_FILE=$1
+        ISO_FILE=""
+        if [ $# -eq 0 ]; then
+      ''
+      + lib.optionalString stdenv.isAarch64 ''
         echo "Usage: windows-launcher ./Windows11_InsiderPreview_Client_ARM64_en-us_25324.VHDX"
-        exit
-      fi
+      ''
+      + lib.optionalString stdenv.isx86_64 ''
+        echo "Usage: windows-launcher ./Win11_22H2_English_x64v2.iso or ./win11.qcow2"
+      ''
+      + ''
+          exit
+        fi
 
-      if [[ -z "''${WAYLAND_DISPLAY}" ]]; then
-        echo "Wayland display not found"
-        exit
-      fi
+        if [[ -z "''${WAYLAND_DISPLAY}" ]]; then
+          echo "Wayland display not found"
+          exit
+        fi
 
-      IMG_DIR="$(dirname "$1")"
-      AAVMF_VARS="$IMG_DIR/AAVMF_VARS.fd"
+        IMG_DIR="$(dirname "$IMG_FILE")"
+        OVMF_VARS="$IMG_DIR/${ovmfPrefix}_VARS.fd"
+        OVMF_CODE="$IMG_DIR/${ovmfPrefix}_CODE.fd"
 
-      if [ ! -f $AAVMF_VARS ]; then
-        cp ${pkgs.OVMF.fd}/FV/AAVMF_VARS.fd $AAVMF_VARS
-        chmod 644 $AAVMF_VARS
-      fi
+        if [ ! -f $OVMF_VARS ] || [ ! -f $OVMF_CODE ]; then
+          cp ${pkgs.OVMF.fd}/FV/${ovmfPrefix}_VARS.fd $OVMF_VARS
+          cp ${pkgs.OVMF.fd}/FV/${ovmfPrefix}_CODE.fd $OVMF_CODE
+          chmod 644 $OVMF_VARS
+        fi
+      ''
+      + lib.optionalString stdenv.isx86_64 ''
+        if [[ $1 == *.iso || $1 == *.ISO ]]; then
+          ISO_FILE=$1
+          IMG_FILE="$IMG_DIR/win11.qcow2"
+          if [ ! -f $IMG_FILE ]; then
+            ${pkgs.qemu}/bin/qemu-img create -f qcow2 $IMG_FILE 64G
+          fi
+        fi
+      ''
+      + ''
+        QEMU_PARAMS=(
+          "-name \"Windows VM\""
+          "-cpu host"
+          "-enable-kvm"
+          "-smp 6"
+          "-m 8G"
+          "-drive file=$OVMF_CODE,format=raw,if=pflash,readonly=on"
+          "-drive file=$OVMF_VARS,format=raw,if=pflash"
+          "-vga none"
+          "-device ramfb"
+          "-device virtio-gpu-pci"
+          "-device qemu-xhci"
+          "-device usb-kbd"
+          "-device usb-tablet"
+          "-nic user,model=virtio"
+      ''
+      + lib.optionalString stdenv.isAarch64 ''
+        "-M virt,highmem=on,gic-version=max"
+        "-drive file=$IMG_FILE,format=vhdx,if=none,id=boot"
+        "-device usb-storage,drive=boot,serial=boot,bootindex=1"
+        )
+      ''
+      + lib.optionalString stdenv.isx86_64 ''
+        "-drive file=$IMG_FILE,format=qcow2,if=none,id=boot"
+        "-device nvme,drive=boot,serial=boot,bootindex=1"
+        )
 
-      ${pkgs.qemu}/bin/qemu-system-aarch64 \
-        -name "Windows VM" \
-        -M virt,highmem=on,gic-version=max \
-        -cpu host \
-        -enable-kvm \
-        -smp 6 \
-        -m 12G \
-        -drive file=${pkgs.OVMF.fd}/FV/AAVMF_CODE.fd,format=raw,if=pflash,readonly=on \
-        -drive file=$AAVMF_VARS,format=raw,if=pflash \
-        -device ramfb \
-        -device virtio-gpu-pci \
-        -device qemu-xhci \
-        -device usb-kbd \
-        -device usb-tablet \
-        -drive file=$1,format=vhdx,if=none,id=boot \
-        -device usb-storage,drive=boot,serial=boot \
-        -nic user,model=virtio \
-        ''${@:2}
-    '';
+        if [ ! -z "$ISO_FILE" ]; then
+          QEMU_PARAMS+=(
+            "-drive file=$ISO_FILE,media=cdrom,if=none,id=installcd"
+            "-device usb-storage,drive=installcd,bootindex=0"
+          )
+        fi
+      ''
+      + ''
+        eval "${pkgs.qemu}/bin/qemu-system-${stdenv.hostPlatform.qemuArch} ''${QEMU_PARAMS[@]} ''${@:2}"
+      '');
   windowsLauncherUI =
     pkgs.writeShellScript
     "windows-launcher-ui"
-    ''
-      if [[ -z "''${WAYLAND_DISPLAY}" ]]; then
-        echo "Wayland display not found"
-        exit
-      fi
-
-      CONFIG=~/.config/windows-launcher-ui.conf
-      if [ -f "$CONFIG" ]; then
-        source $CONFIG
-      fi
-
-      if [ ! -f "$FILE" ]; then
-        FILE=`${pkgs.gnome.zenity}/bin/zenity --file-selection --title="Select Windows VM image (VHDX)"`
-        if [ ''$? -ne 0 ]; then
+    (''
+        if [[ -z "''${WAYLAND_DISPLAY}" ]]; then
+          echo "Wayland display not found"
           exit
-        else
-          echo FILE="$FILE" > "$CONFIG"
         fi
-      fi
 
-      if ! ${windowsLauncher} $FILE; then
-        ${pkgs.gnome.zenity}/bin/zenity --error --text="Failed to run Windows VM: $?"
-      fi
-    '';
+        CONFIG=~/.config/windows-launcher-ui.conf
+        if [ -f "$CONFIG" ]; then
+          source $CONFIG
+        fi
+
+        if [ ! -f "$FILE" ]; then
+      ''
+      + lib.optionalString stdenv.isAarch64 ''
+        FILE=`${pkgs.gnome.zenity}/bin/zenity --file-selection --title="Select Windows VM image (VHDX)"`
+      ''
+      + lib.optionalString stdenv.isx86_64 ''
+        FILE=`${pkgs.gnome.zenity}/bin/zenity --file-selection --title="Select Windows VM image (QCOW2 or ISO)"`
+      ''
+      + ''
+          if [ ''$? -ne 0 ]; then
+            exit
+          else
+            if [[ $FILE != *.iso && $FILE != *.ISO ]]; then
+              echo FILE="$FILE" > "$CONFIG"
+            fi
+          fi
+        fi
+
+        if ! ${windowsLauncher} $FILE; then
+          ${pkgs.gnome.zenity}/bin/zenity --error --text="Failed to run Windows VM: $?"
+        fi
+      '');
 in
   stdenvNoCC.mkDerivation {
     name = "windows-launcher";
@@ -89,8 +144,9 @@ in
     '';
 
     meta = with lib; {
-      description = "Helper scripts for launching Windows ARM64 virtual machines using QEMU";
+      description = "Helper scripts for launching Windows virtual machines using QEMU";
       platforms = [
+        "x86_64-linux"
         "aarch64-linux"
       ];
     };
