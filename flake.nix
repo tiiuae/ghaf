df44527b5adb5d847accd2e7741efd3374b84540
Ville Ilvonen <ville.ilvonen@unikie.com>
2023-11-07 10:35:45 +0200

    devshell: for kernel development
    * Sets up dependencies for kernel configuration,
      compilation and kernel-hardening-checker for
      the dev shell.
    * Extracts and patches linux_latest kernel sources
      to current directory under ghaf if the directory
      is not already there. Enters the source directory.
    * Sets shell prompt to indicate ghaf kernel devshell.
    
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

--- flake.nix
+++ flake.nix
@@ -77,6 +77,37 @@
         };
 
         formatter = pkgs.alejandra;
+
+        devShells.kernel = pkgs.mkShell {
+          packages = [
+            pkgs.ncurses
+            pkgs.pkg-config
+            pkgs.python3
+            pkgs.python3Packages.pip
+          ];
+          inputsFrom = [pkgs.linux_latest];
+          shellHook = ''
+            export src=${pkgs.linux_latest.src}
+            if [ ! -d "linux-${pkgs.linux_latest.version}" ]; then
+              unpackPhase
+              patchPhase
+            fi
+            cd linux-${pkgs.linux_latest.version}
+
+            # python3+pip for kernel-hardening-checker
+            export PIP_PREFIX=$(pwd)/_build/pip_packages
+            export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+            export PATH="$PIP_PREFIX/bin:$PATH"
+
+            # install kernel-hardening-checker via pip under "linux-<version" for
+            # easy clean-up with directory removal - if not already installed
+            if [ ! -f "_build/pip_packages/bin/kernel-hardening-checker" ]; then
+              python3 -m pip install git+https://github.com/a13xp0p0v/kernel-hardening-checker
+            fi
+
+            export PS1="[ghaf-kernel-devshell:\w]$ "
+          '';
+        };
       }))
 
       # ghaf lib
