d060a6f3f7421ef55184eee3a843ac8cc0d74c70
Nikita Bazulin <nikita.bazulin@unikie.com>
2023-11-08 12:36:01 +0200

    [Development] Ssh: add host auth keys
    To be able to establish ssh connection
    to the host with no password
    
    Also enable GatewayPorts
    
    Signed-off-by: Nikita Bazulin <nikita.bazulin@unikie.com>

--- modules/development/ssh.nix
+++ modules/development/ssh.nix
@@ -3,24 +3,29 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.ghaf.development.ssh.daemon;
 
-  authorizedKeys = [
-    # Add your SSH Public Keys here
-    # NOTE: adding your pub ssh key here will make accessing and "nixos-rebuild switching" development mode
-    # builds easy but still secure. Given that you protect your private keys. Do not share your keypairs across hosts.
-    #
-    # Shared authorized keys access poses a minor risk for developers in the same network (e.g. office) cross-accessing
-    # each others development devices if:
-    # - the ip addresses from dhcp change between the developers without the noticing AND
-    # - you ignore the server fingerprint checks
-    # You have been helped and you have been warned.
-    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIA/pwHnzGNM+ZU4lANGROTRe2ZHbes7cnZn72Oeun/MCAAAABHNzaDo= brian@arcadia"
-    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEJ9ewKwo5FLj6zE30KnTn8+nw7aKdei9SeTwaAeRdJDAAAABHNzaDo= brian@minerva"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDdNDuKwAsAff4iFRfujo77W4cyAbfQHjHP57h/7tJde ville.ilvonen@unikie.com"
-  ];
+  authorizedKeys =
+    [
+      # Add your SSH Public Keys here
+      # NOTE: adding your pub ssh key here will make accessing and "nixos-rebuild switching" development mode
+      # builds easy but still secure. Given that you protect your private keys. Do not share your keypairs across hosts.
+      #
+      # Shared authorized keys access poses a minor risk for developers in the same network (e.g. office) cross-accessing
+      # each others development devices if:
+      # - the ip addresses from dhcp change between the developers without the noticing AND
+      # - you ignore the server fingerprint checks
+      # You have been helped and you have been warned.
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIA/pwHnzGNM+ZU4lANGROTRe2ZHbes7cnZn72Oeun/MCAAAABHNzaDo= brian@arcadia"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEJ9ewKwo5FLj6zE30KnTn8+nw7aKdei9SeTwaAeRdJDAAAABHNzaDo= brian@minerva"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDdNDuKwAsAff4iFRfujo77W4cyAbfQHjHP57h/7tJde ville.ilvonen@unikie.com"
+
+      # So GUIVM can establish ssh-connection with the host without a password
+    ]
+    ++ lib.optional pkgs.stdenv.isx86_64 (builtins.readFile "${pkgs.waypipe-ssh}/keys/waypipe-ssh.pub");
 in
   with lib; {
     options.ghaf.development.ssh.daemon = {
@@ -29,6 +34,8 @@ in
 
     config = mkIf cfg.enable {
       services.openssh.enable = true;
+      services.openssh.extraConfig = pkgs.dbus-over-ssh.host.sshdConfigExtra;
+      services.openssh.settings.GatewayPorts = "yes";
       users.users.root.openssh.authorizedKeys.keys = authorizedKeys;
       users.users.${config.ghaf.users.accounts.user}.openssh.authorizedKeys.keys = authorizedKeys;
     };
