1ef3e0e343582159630fba992f939545535d2f16
Mika Tammi <mika.tammi@unikie.com>
2023-11-20 20:00:30 +0200

    Forward port ACPI patch to QEMU 8.1
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- overlays/custom-packages/qemu/default.nix
+++ overlays/custom-packages/qemu/default.nix
@@ -1,7 +1,18 @@
 # Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 (final: prev: {
-  qemu_kvm = prev.qemu_kvm.overrideAttrs (_final: prev: {
-    patches = prev.patches ++ [./acpi-devices-passthrough.patch];
-  });
+  qemu_kvm = let
+    qemu_version = prev.qemu_kvm.version;
+    qemu_major = final.lib.versions.major qemu_version;
+    qemu_minor = final.lib.versions.minor qemu_version;
+  in
+    prev.qemu_kvm.overrideAttrs (
+      _final: prev:
+        (final.lib.optionalAttrs (qemu_major == "8" && qemu_minor == "0") {
+          patches = prev.patches ++ [./acpi-devices-passthrough-qemu-8.0.patch];
+        })
+        // (final.lib.optionalAttrs (qemu_major == "8" && qemu_minor == "1") {
+          patches = prev.patches ++ [./acpi-devices-passthrough-qemu-8.1.patch];
+        })
+    );
 })
