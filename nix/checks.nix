054edba8e6ce0cc845b5b60c700ca3892d41f1c7
Brian McGillion <bmg.avoin@gmail.com>
2023-11-28 22:28:41 +0400

    checks: Improve nix flake check performance
    This reduces the memory load of `nix flake check` to ~15GB by making it
    more sequential.
    
    Co-authored-by: Brian McGee <brian@bmcgee.ie>
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- nix/checks.nix
+++ nix/checks.nix
@@ -1,32 +1,22 @@
 # Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{inputs, ...}: {
-  perSystem = {pkgs, ...}: {
-    checks = {
-      reuse =
-        pkgs.runCommandLocal "reuse-lint" {
-          buildInputs = [pkgs.reuse];
-        } ''
-          cd ${../.}
-          reuse lint
-          touch $out
-        '';
-      nix-build-all = pkgs.writeShellApplication {
-        name = "nix-build-all";
-        runtimeInputs = let
-          devour-flake = pkgs.callPackage inputs.devour-flake {};
-        in [
-          pkgs.nix
-          devour-flake
-        ];
-        text = ''
-          # Make sure that flake.lock is sync
-          nix flake lock --no-update-lock-file
-
-          # Do a full nix build (all outputs)
-          devour-flake . "$@"
-        '';
-      };
-    };
+{lib, ...}: {
+  perSystem = {
+    pkgs,
+    self',
+    ...
+  }: {
+    checks =
+      {
+        reuse =
+          pkgs.runCommandLocal "reuse-lint" {
+            buildInputs = [pkgs.reuse];
+          } ''
+            cd ${../.}
+            reuse lint
+            touch $out
+          '';
+      }
+      // (lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages);
   };
 }
