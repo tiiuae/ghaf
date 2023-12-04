19d566b4ed3938ebe22e70cdfe7914d4187a82c5
Jon Sahlberg <jon.sahlberg@unikie.com>
2023-12-04 12:03:48 +0200

    architecture: Initial Audio VM adr
    Initial documentation for Audio virtual machine to separate
    the audio data away from host and separate the virtual machines
    audio data to separate streams.
    
    Signed-off-by: Jon Sahlberg <jon.sahlberg@unikie.com>
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

--- overlays/custom-packages/tcp-ssh/default.nix
+++ overlays/custom-packages/tcp-ssh/default.nix
@@ -1,5 +1,5 @@
 # Copyright 2022-2023 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-(final: prev: {
-  tcp-ssh = final.callPackage ../../../user-apps/tcp-ssh {};
+(final: _prev: {
+  tcp-ssh = final.callPackage ../../../packages/tcp-ssh {};
 })
--- targets/lenovo-x1-carbon.nix
+++ targets/lenovo-x1-carbon.nix
@@ -31,28 +31,36 @@
       }
     ];
     audio.pciDevices = [
+      # Passthrough of Intel Audio device 8086:51ca
+      # With the current implementation, the whole PCI IOMMU group 14:
+      #   00:1f.x in the example from Lenovo X1 Carbon
+      #   must be defined for passthrough to AudioVM
+
+      # ISA bridge: Intel Corporation Raptor Lake LPC/eSPI Controller (rev 01)
       {
         path = "0000:00:1f.0";
         vendorId = "8086";
         productId = "5194";
       }
+      # Audio device: Intel Corporation Raptor Lake-P/U/H cAVS (rev 01)
       {
         path = "0000:00:1f.3";
         vendorId = "8086";
         productId = "51ca";
       }
+      # SMBus: Intel Corporation Alder Lake PCH-P SMBus Host Controller (rev 01)
       {
         path = "0000:00:1f.4";
         vendorId = "8086";
         productId = "51a3";
       }
+      # Serial bus controller: Intel Corporation Alder Lake-P PCH SPI Controller (rev 01)
       {
         path = "0000:00:1f.5";
         vendorId = "8086";
         productId = "51a4";
       }
     ];
-    };
     virtioInputHostEvdevs = [
       # Lenovo X1 touchpad and keyboard
       "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
@@ -182,11 +190,65 @@
         ];
 
         time.timeZone = "Asia/Dubai";
+        # Tcp-ssh service runs in the GUIVM and makes ssh tunnel from localhost:4713 to AudioVM for pulseaudio
+        systemd.services.pulse-ssh = {
+          enable = true;
+          description = "tcp-ssh -tunnnel for pulseaudio";
+          after = ["network-online.target"];
+          serviceConfig = {
+            Type = "forking";
+            Environment = [
+              "PULSE_SERVER=\"tcp:localhost:4713\""
+              "PID_PATH=\"/run/tcp-ssh/\""
+            ];
+            PIDFile = "/run/tcp-ssh/tcp-ssh-4713.pid";
+            ExecStart = "${pkgs.tcp-ssh}/bin/tcp-ssh audio-vm.ghaf 4713";
+            ExecStop = "${pkgs.procps}/bin/pkill -F /run/tcp-ssh/tcp-ssh-4713.pid";
+            Restart = "on-failure";
+            RestartSec = 5;
+            User = "ghaf";
+          };
+          wantedBy = ["ghaf-session.target"];
+        };
+        systemd.tmpfiles.rules = ["d /run/tcp-ssh 0750 ghaf ghaf -"];
       })
     ];
-    audiovmConfig = hostConfiguration.config.ghaf.virtualization.microvm.audiovm;
-    audiovmExtraModules = [];
+    # Currently not needed
+    # audiovmConfig = hostConfiguration.config.ghaf.virtualization.microvm.audiovm;
+    audiovmExtraModules = [
+      ({pkgs, ...}: {
+        microvm = {
+          shares = [
+            {
+              tag = "waypipe-ssh-public-key";
+              source = "/run/waypipe-ssh-public-key";
+              mountPoint = "/run/waypipe-ssh-public-key";
+            }
+          ];
+        };
+        fileSystems."/run/waypipe-ssh-public-key".options = ["ro"];
 
+        # Waypipe-ssh key is used here to create keys for ssh tunneling to
+        # forward pulseaudio tcp connections.
+        # SSH is very picky about to file permissions and ownership and will
+        # accept neither direct path inside /nix/store or symlink that points
+        # there. Therefore we copy the file to /etc/ssh/get-auth-keys (by
+        # setting mode), instead of symlinking it.
+        environment.etc."ssh/get-auth-keys" = {
+          source = let
+            script = pkgs.writeShellScriptBin "get-auth-keys" ''
+              [[ "$1" != "ghaf" ]] && exit 0
+              ${pkgs.coreutils}/bin/cat /run/waypipe-ssh-public-key/id_ed25519.pub
+            '';
+          in "${script}/bin/get-auth-keys";
+          mode = "0555";
+        };
+        services.openssh = {
+          authorizedKeysCommand = "/etc/ssh/get-auth-keys";
+          authorizedKeysCommandUser = "nobody";
+        };
+      })
+    ];
     hostConfiguration = lib.nixosSystem {
       inherit system;
       specialArgs = {inherit lib;};
@@ -299,7 +361,6 @@
                   ]
                   ++ audiovmExtraModules;
               };
-              };
               virtualization.microvm.appvm = {
                 enable = true;
                 vms = [
@@ -316,14 +377,11 @@
                         hardware.pulseaudio.enable = true;
                         users.extraUsers.ghaf.extraGroups = ["audio"];
 
-<<<<<<< HEAD
                         time.timeZone = "Asia/Dubai";
-=======
                         hardware.pulseaudio.extraConfig = ''
                           load-module module-combine-sink
                           set-sink-volume @DEFAULT_SINK@ 60000
                         '';
->>>>>>> bd4d3fd (Initial setup for AudioVM with passthrough for X1)
 
                         microvm.qemu.extraArgs = [
                           # Lenovo X1 integrated usb webcam
@@ -333,7 +391,7 @@
                           "usb-host,vendorid=0x04f2,productid=0xb751"
                           # Connect sound device to hosts pulseaudio socket
                           "-audiodev"
-                          "pa,id=pa1,server=tcp:192.168.101.4"
+                          "pa,id=pa1,server=tcp:audio-vm.lan"
                           # Add HDA sound device to guest
                           "-device"
                           "intel-hda"
