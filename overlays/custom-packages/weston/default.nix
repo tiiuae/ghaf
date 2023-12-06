0bd2df9d7558c7580e4f4cce88c04bb062173ba7
Mika Tammi <mika.tammi@unikie.com>
2023-12-06 07:37:57 +0200

    Cleanup Weston overlay
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- overlays/custom-packages/weston/default.nix
+++ overlays/custom-packages/weston/default.nix
@@ -3,56 +3,24 @@
 #
 # This overlay customizes weston - see comments for details
 #
-(final: prev: {
-  # TODO: Remove this override if/when the fix is upstreamed.
-  # Disabling colord dependency for weston. Colord has argyllcms as
-  # a dependency, and this package is not cross-compilable.
-  # Nowadays, colord even marked as deprecated option for weston.
+(_final: prev: {
   weston =
-    # First, weston package is overridden (passing colord = null)
+    # First, weston package is overridden
     (
-      prev.weston.override (
-        {
-          pipewire = null;
-          freerdp = null;
-          xwayland = null;
-        }
-        # Only override colord if the package takes such argument. In NixOS
-        # 23.05, the Weston package still uses colord as a dependency, but it
-        # has been removed in NixOS Unstable. Otherwise there will be an
-        # error about unexpected argument.
-        // final.lib.optionalAttrs (final.lib.hasAttr "colord" (final.lib.functionArgs prev.weston.override)) {
-          colord = null;
-        }
-        # NixOS Unstable has added these variables to control whether
-        # pipewire, rdp or xwayland support should be present. They need to
-        # be defined to false to avoid errors during the build.
-        # TODO: When moving to NixOS 23.11, these optionalAttrs can just be
-        #       removed, and the attributes can be combined to single
-        #       attribute set.
-        // final.lib.optionalAttrs (final.lib.hasAttr "pipewireSupport" (final.lib.functionArgs prev.weston.override)) {
-          pipewireSupport = false;
-        }
-        // final.lib.optionalAttrs (final.lib.hasAttr "rdpSupport" (final.lib.functionArgs prev.weston.override)) {
-          rdpSupport = false;
-        }
-        // final.lib.optionalAttrs (final.lib.hasAttr "xwaylandSupport" (final.lib.functionArgs prev.weston.override)) {
-          xwaylandSupport = false;
-        }
-        // final.lib.optionalAttrs (final.lib.hasAttr "vncSupport" (final.lib.functionArgs prev.weston.override)) {
-          vncSupport = false;
-        }
-      )
+      prev.weston.override {
+        freerdp = null;
+        pipewire = null;
+        pipewireSupport = false;
+        rdpSupport = false;
+        vncSupport = false;
+        xwayland = null;
+        xwaylandSupport = false;
+      }
     )
     # and then this overridden package's attributes are overridden
     .overrideAttrs (
-      prevAttrs:
-        final.lib.optionalAttrs (final.lib.hasAttr "colord" (final.lib.functionArgs prev.weston.override)) {
-          # Only override mesonFlags if colord argument is accepted
-          mesonFlags = prevAttrs.mesonFlags ++ ["-Ddeprecated-color-management-colord=false"];
-        }
-        // {
-          patches = [./weston-backport-workspaces.patch];
-        }
+      _prevAttrs: {
+        patches = [./weston-backport-workspaces.patch];
+      }
     );
 })
