02c401fb2b79cd93cef22397911d7da57c17c3d4
Yuri Nesterov <yuriy.nesterov@unikie.com>
2024-01-18 12:17:59 +0200

    Fix waypipe in labwc
    Waypipe uses the wayland-1 socket which is created by systemd for weston
    and labwc. However, labwc doesn't have systemd integration and it
    creates its own socket which is wayland-0 by default. This patch makes
    waypipe use the correct socket from the WAYLAND_DISPLAY environmental
    variable both for weston and labwc. It also removes the unused wayland-1
    socket in labwc module and improves weston startup sequence to leverage
    systemd integration.
    
    Signed-off-by: Yuri Nesterov <yuriy.nesterov@unikie.com>

--- modules/graphics/labwc.nix
+++ modules/graphics/labwc.nix
@@ -22,26 +22,21 @@ in {
     # Next 2 services/targets are taken from official weston documentation
     # and adjusted for labwc
     # https://wayland.pages.freedesktop.org/weston/toc/running-weston.html
-
-    # labwc socket
-    systemd.user.sockets."labwc" = {
-      unitConfig = {
-        Description = "labwc, a Wayland compositor";
-        Documentation = "man:labwc(1)";
-      };
-      socketConfig = {
-        ListenStream = "%t/wayland-1";
-      };
-      wantedBy = ["labwc.service"];
-    };
-
-    # labwc service
-    systemd.user.services."labwc" = {
+    systemd.user.services."labwc" = let
+      autostart =
+        pkgs.concatScript "labwc-autostart"
+        [
+          "${pkgs.labwc}/share/doc/labwc/autostart"
+          (pkgs.writeText "autostart-extra" ''
+            # Import WAYLAND_DISPLAY variable to make it available to waypipe and other systemd services
+            ${pkgs.systemd}/bin/systemctl --user import-environment WAYLAND_DISPLAY 2>&1 &
+          '')
+        ];
+    in {
       enable = true;
       description = "labwc, a Wayland compositor, as a user service TEST";
       documentation = ["man:labwc(1)"];
-      requires = ["labwc.socket"];
-      after = ["labwc.socket" "ghaf-session.service"];
+      after = ["ghaf-session.service"];
       serviceConfig = {
         # Previously there was "notify" type, but for some reason
         # systemd kills labwc.service because of timeout (even if it is disabled).
@@ -52,7 +47,7 @@ in {
         # Defaults to journal
         StandardOutput = "journal";
         StandardError = "journal";
-        ExecStart = "${pkgs.labwc}/bin/labwc -C ${pkgs.labwc}/share/doc/labwc -s ${pkgs.labwc}/share/doc/labwc/autostart";
+        ExecStart = "${pkgs.labwc}/bin/labwc -C ${pkgs.labwc}/share/doc/labwc -s ${autostart}";
         #GPU pt needs some time to start - labwc fails to restart 3 times in avg.
         ExecStartPre = "${pkgs.coreutils}/bin/sleep 3";
         Restart = "on-failure";
