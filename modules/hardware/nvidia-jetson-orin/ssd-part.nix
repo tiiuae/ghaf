857409431e42a19060d54fb0c672b84c1a20e67f
Kim Sandström <kim.sandstrom@unikie.com>
2024-02-02 14:36:39 +0200

    tidyup

--- modules/hardware/nvidia-jetson-orin/ssd-part.nix
+++ modules/hardware/nvidia-jetson-orin/ssd-part.nix
@@ -11,41 +11,40 @@ let
 
   # check if at least one of HOME or STORAGE already exists
   # (condition can be changed to [ <condition> != "2" ] )
-  # failing mount of STORE or HOME is not critical
-#  ssdPartScript = pkgs.writeScript "ssdPartScript" ''
+  # failing mount of STORE or HOME is not critical (unless STORE data is needed)
   ssdPartScript = ''
-  #!/bin/bash -e
+    echo "Executing SSD partitioning script"
 
-  echo "Executing SSD partitioning script"
+    unset ssd
+    # grep for SSD in /dev/disk/by-id
+    # found SSD disk "should" be a link to /dev/nvme0n1
+    ssd=$(ls -X /dev/disk/by-id/*SSD* | head -1)
+    # ssd='/dev/nvme0n1'
+    # check if HOME or STORAGE partitions already exist
+    if [ "$ssd" ] && [ $(lsblk -o LABEL "$ssd" | grep -e"^HOME$" -e"^STORAGE$" | wc -l) == "0" ]
+    then
+      # calculate partition sizes
+      sectors=$(blockdev --getsz "$ssd")
+      # assuming 2024 Master Boot Record sectors.
+      let sect_25=($sectors-2048)/4 # ~25% of disk
 
-  unset ssd
-  # grep for SSD in /dev/disk/by-id
-  # found SSD disk "should" be a link to /dev/nvme0n1
-  ssd=$(ls -X /dev/disk/by-id/*SSD* | head -1)
-  # ssd='/dev/nvme0n1'
-  # check if HOME or STORAGE partitions already exist
-  if [ "$ssd" ] && [ $(lsblk -o LABEL "$ssd" | grep -e"^HOME$" -e"^STORAGE$" | wc -l) == "0" ]
-  then
-	# calculate partition sizes
-	sectors=$(blockdev --getsz "$ssd")
-	# assuming 2024 Master Boot Record sectors.
-	let sect_25=($sectors-2048)/4 # ~25% of disk
+      echo "Debug Alert -- Partitioning of the SSD would have been done on disk $ssd"
+      exit 0
 
-	echo "Debug Alert -- Partitioning of the SSD would have been done on disk $ssd"
-	exit 0
+      # Apply fdisk commands
+      fdisk_cmd="g\nn\n\n1\n$sect_25\nn\n\n2\nw\n"
+      fdisk "$ssd" <<< "$fdisk_cmd"
 
-	# Apply fdisk commands
-	fdisk_cmd="g\nn\n\n1\n$sect_25\nn\n\n2\nw\n"
-	fdisk "$ssd" <<< "$fdisk_cmd"
+      # label new partitions
+      e2label "$ssd""p1" HOME
+      e2label "$ssd""p2" STORE
 
-	# label new partitions
-	e2label "$ssd""p1" HOME
-	e2label "$ssd""p2" STORE
+      fdisk -l $ssd
 
-	fdisk -l $ssd
+      # TODO insert copy of /nix/store to STORE partition
 
-	# formatting not needed with Nix fileSystems.autoFormat 
-  fi
+      # formatting not needed with Nix fileSystems.autoFormat 
+        fi
   '';
 in
 {
