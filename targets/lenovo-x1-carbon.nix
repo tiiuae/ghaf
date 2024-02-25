8c543570f63306177ba2e0eccad56fd540dfcd94
Brian McGillion <bmg.avoin@gmail.com>
2024-02-25 13:14:16 +0400

    use disko for standard image
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- targets/lenovo-x1-carbon.nix
+++ targets/lenovo-x1-carbon.nix
@@ -4,14 +4,13 @@
 # Generic x86_64 computer -target
 {
   lib,
-  nixos-generators,
   microvm,
   lanzaboote,
+  disko,
   ...
 }: let
   name = "lenovo-x1-carbon-gen11";
   system = "x86_64-linux";
-  formatModule = nixos-generators.nixosModules.raw-efi;
 
   powerControlPkgPath = ../packages/powercontrol;
 
@@ -271,6 +270,8 @@
         [
           lanzaboote.nixosModules.lanzaboote
           microvm.nixosModules.host
+          disko.nixosModules.disko
+          (import ../modules/installer/partitioning/lenovo-x1-disko-basic.nix)
           ../modules/host
           ../modules/virtualization/microvm/microvm-host.nix
           ../modules/virtualization/microvm/netvm.nix
@@ -478,28 +479,29 @@
             };
           })
 
-          ({config, ...}: {
-            ghaf.installer = {
-              enable = true;
-              imgModules = [
-                nixos-generators.nixosModules.raw-efi
-              ];
-              enabledModules = ["flushImage"];
-              installerCode = ''
-                echo "Starting flushing..."
-                if sudo dd if=${config.system.build.${config.formatAttr}}/nixos.img of=/dev/${config.ghaf.installer.installerModules.flushImage.providedVariables.deviceName} conv=sync bs=4K status=progress; then
-                    sync
-                    echo "Flushing finished successfully!"
-                    echo "Now you can detach installation device and reboot to ghaf."
-                else
-                    echo "Some error occured during flushing process, exit code: $?."
-                    exit
-                fi
-              '';
-            };
-          })
+          # TODO delete this section
+          # ({config, ...}: {
+          #   ghaf.installer = {
+          #     enable = true;
+          #     imgModules = [
+          #       nixos-generators.nixosModules.raw-efi
+          #     ];
+          #     enabledModules = ["flushImage"];
+          #     installerCode = ''
+          #       echo "Starting flushing..."
+          #       if sudo dd if=${config.system.build.${config.formatAttr}}/nixos.img of=/dev/${config.ghaf.installer.installerModules.flushImage.providedVariables.deviceName} conv=sync bs=4K status=progress; then
+          #           sync
+          #           echo "Flushing finished successfully!"
+          #           echo "Now you can detach installation device and reboot to ghaf."
+          #       else
+          #           echo "Some error occured during flushing process, exit code: $?."
+          #           exit
+          #       fi
+          #     '';
+          #   };
+          # })
 
-          formatModule
+          ### disko here
 
           #TODO: how to handle the majority of laptops that need a little
           # something extra?
@@ -531,7 +533,7 @@
   in {
     inherit hostConfiguration;
     name = "${name}-${variant}";
-    package = hostConfiguration.config.system.build.${hostConfiguration.config.formatAttr};
+    package = hostConfiguration.config.system.build.diskoImages;
   };
   debugModules = [
     ../modules/development/usb-serial.nix
