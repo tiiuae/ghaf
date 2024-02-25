b30d0bcb89a0d1aa81be19a288e01ee068c579e0
Mika Tammi <mika.tammi@unikie.com>
2024-02-25 16:25:00 +0200

    Embed ghaf image into installation iso
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- targets/lenovo-x1-carbon-installer.nix
+++ targets/lenovo-x1-carbon-installer.nix
@@ -2,18 +2,38 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Lenovo X1 Carbon Installer
-{lib}: let
+{
+  self,
+  lib,
+}: let
   name = "lenovo-x1-carbon-gen11";
   system = "x86_64-linux";
   installer = variant: let
+    image = self.packages.x86_64-linux."lenovo-x1-carbon-gen11-${variant}";
     hostConfiguration = lib.nixosSystem {
       inherit system;
       specialArgs = {inherit lib;};
       modules = [
-        ({modulesPath, ...}: {
-          isoImage.isoBaseName = "ghaf";
+        ({
+          pkgs,
+          modulesPath,
+          ...
+        }: let
+          imageScript = pkgs.writeShellScriptBin "ghaf-image" ''
+            echo "${image}"
+          '';
+        in {
           imports = [
             "${toString modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+          ];
+
+          systemd.services.wpa_supplicant.wantedBy = lib.mkForce ["multi-user.target"];
+          systemd.services.sshd.wantedBy = lib.mkForce ["multi-user.target"];
+
+          isoImage.isoBaseName = "ghaf";
+
+          environment.systemPackages = [
+            imageScript
           ];
         })
       ];
