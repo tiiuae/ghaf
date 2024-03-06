5354cb084361221ab9f5eaaaa15fa6b255a479cf
Mika Tammi <mika.tammi@unikie.com>
2024-03-06 17:25:53 +0200

    targets: lenovo-x1-installer to flakeModule
    Signed-off-by: Mika Tammi <mika.tammi@unikie.com>

--- targets/flake-module.nix
+++ targets/flake-module.nix
@@ -14,12 +14,12 @@ in
     {
       imports = [
         ./generic-x86_64/flake-module.nix
+        ./lenovo-x1-installer/flake-module.nix
         ./lenovo-x1/flake-module.nix
         ./nvidia-jetson-orin/flake-module.nix
       ];
     }
     (import ./vm.nix {inherit self lib nixos-generators microvm;})
-    (import ./lenovo-x1-carbon-installer.nix {inherit self lib;})
     (import ./imx8qm-mek.nix {inherit self lib nixos-generators nixos-hardware microvm;})
     (import ./microchip-icicle-kit.nix {inherit self lib nixpkgs nixos-hardware;})
   ]
