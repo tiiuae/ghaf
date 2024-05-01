a11ac26c1a016b66180672e4ae20d957d1b9aeab
Brian McGillion <bmg.avoin@gmail.com>
2024-05-01 15:48:34 +0400

    Remove systemd overlay for timesyncd
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- overlays/custom-packages/default.nix
+++ overlays/custom-packages/default.nix
@@ -9,7 +9,6 @@
   element-desktop = import ./element-desktop {inherit prev;};
   element-gps = final.callPackage ../../packages/element-gps {};
   element-web = final.callPackage ../../packages/element-web {};
-  systemd = import ./systemd {inherit final prev;};
   waypipe = import ./waypipe {inherit final prev;};
   weston = import ./weston {inherit final prev;};
   wifi-connector = final.callPackage ../../packages/wifi-connector {};
