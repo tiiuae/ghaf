cb4b9c847510b74275d1bdf08eac47eb074c6e68
juliuskoskela-unikie <julius.koskela@unikie.com>
2024-05-02 09:59:01 +0300

    Add lkp-tests package derivation
    Signed-off-by: juliuskoskela-unikie <julius.koskela@unikie.com>

---
+++ packages/lkp-tests/default.nix
@@ -0,0 +1,29 @@
+{pkgs}:
+pkgs.stdenv.mkDerivation {
+  pname = "lkp-tests";
+  version = "unstable-2024-04-17";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "intel";
+    repo = "lkp-tests";
+    rev = "master";
+    sha256 = "sha256-+NTEVg1+Xwd05tEMvmFBCkCx2rplXk7rlXuIt6XuQf4=";
+  };
+
+  buildInputs = [pkgs.makeWrapper pkgs.ruby_2_7];
+
+  phases = ["unpackPhase" "installPhase"];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r . $out
+    make install TARGET_DIR_BIN=$out
+  '';
+
+  meta = with pkgs.lib; {
+    description = "Linux Kernel Performance tests";
+    homepage = "https://github.com/intel/lkp-tests";
+    license = licenses.gpl2;
+    platforms = platforms.linux;
+  };
+}
