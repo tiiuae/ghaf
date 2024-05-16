9abbd1e02115105f7b864714e292dce395e14267
Manuel Bluhm <manuel@ssrc.tii.ae>
2024-05-16 23:19:56 +0400

    Fix path to pkvm patch
    Signed-off-by: Manuel Bluhm <manuel@ssrc.tii.ae>

--- modules/hardware/x86_64-generic/kernel/host/pkvm/default.nix
+++ modules/hardware/x86_64-generic/kernel/host/pkvm/default.nix
@@ -20,7 +20,7 @@
   pkvm_patch = [
     {
       name = "pkvm-patch";
-      patch = ../../../../../virtualization/pkvm/0001-pkvm-enable-pkvm-on-intel-x86-6.1-lts.patch;
+      patch = ../../../../../common/virtualization/pkvm/0001-pkvm-enable-pkvm-on-intel-x86-6.1-lts.patch;
       structuredExtraConfig = with lib.kernel; {
         KVM_INTEL = yes;
         KSM = no;
