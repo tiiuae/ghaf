b84dc25b9395cdf6ece7629ab2aea8d707e834e1
Brian McGillion <bmg.avoin@gmail.com>
2024-05-24 06:11:13 +0400

    Cleanup jetpack
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- modules/jetpack/nvidia-jetson-orin/virtualization/host/uarta-host/default.nix
+++ modules/jetpack/nvidia-jetson-orin/virtualization/host/uarta-host/default.nix
@@ -15,31 +15,35 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    ghaf.hardware.nvidia.virtualization.enable = true;
-    ghaf.hardware.nvidia.virtualization.host.bpmp.enable = true;
-
-    systemd.services.enableVfioPlatform = {
-      description = "Enable the vfio-platform driver for UARTA";
-      wantedBy = ["bindSerial3100000.service"];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = "yes";
-        ExecStart = ''
-          ${pkgs.bash}/bin/bash -c "echo vfio-platform > /sys/bus/platform/devices/3100000.serial/driver_override"
-        '';
-      };
+    ghaf.hardware.nvidia.virtualization = {
+      enable = true;
+      host.bpmp.enable = true;
     };
 
-    systemd.services.bindSerial3100000 = {
-      description = "Bind UARTA to the vfio-platform driver";
-      wantedBy = ["multi-user.target"];
-      after = ["enableVfioPlatform.service"];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = "yes";
-        ExecStart = ''
-          ${pkgs.bash}/bin/bash -c "echo 3100000.serial > /sys/bus/platform/drivers/vfio-platform/bind"
-        '';
+    systemd.services = {
+      enableVfioPlatform = {
+        description = "Enable the vfio-platform driver for UARTA";
+        wantedBy = ["bindSerial3100000.service"];
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = "yes";
+          ExecStart = ''
+            ${pkgs.bash}/bin/bash -c "echo vfio-platform > /sys/bus/platform/devices/3100000.serial/driver_override"
+          '';
+        };
+      };
+
+      bindSerial3100000 = {
+        description = "Bind UARTA to the vfio-platform driver";
+        wantedBy = ["multi-user.target"];
+        after = ["enableVfioPlatform.service"];
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = "yes";
+          ExecStart = ''
+            ${pkgs.bash}/bin/bash -c "echo 3100000.serial > /sys/bus/platform/drivers/vfio-platform/bind"
+          '';
+        };
       };
     };
   };
