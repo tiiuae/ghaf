cd828a2a50e3f6e80216d0b422fa03bd9cedc141
Jörg Thalheim <joerg@thalheim.io>
2024-05-24 11:30:08 +0200

    waypipe: rebase patch on the latest release
    The latest release also has a "title-prefix" feature which is also very
    handy to i.e. prefix the VM name to an application.
    
    Signed-off-by: Jörg Thalheim <joerg@thalheim.io>

--- overlays/custom-packages/waypipe/default.nix
+++ overlays/custom-packages/waypipe/default.nix
@@ -1,17 +1,7 @@
 # Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{
-  final,
-  prev,
-}:
+{prev, ...}:
 # Waypipe with vsock and window borders
 prev.waypipe.overrideAttrs (_prevAttrs: {
-  src = final.pkgs.fetchFromGitLab {
-    domain = "gitlab.freedesktop.org";
-    owner = "mstoeckl";
-    repo = "waypipe";
-    rev = "ca4809435e781dfc6bd3006fde605860c8dcf179";
-    sha256 = "sha256-tSLPlf7fVq8vwbr7fHotqM/sBSXYMDM1V5yth5bhi38=";
-  };
   patches = [./waypipe-window-borders.patch];
 })
