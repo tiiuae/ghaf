b91fe04345d01324a7772868a621a644e03d04c8
Brian McGillion <bmg.avoin@gmail.com>
2024-06-07 08:48:46 +0400

    Move the programs to the reference module
    Create a new module that is for reference implementations. This module
    will contain a list of the applications, app vms and hardware enablers
    that can show case how the Ghaf framework can be made to function.
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- targets/lenovo-x1/appvms/zathura.nix
+++ targets/lenovo-x1/appvms/zathura.nix
@@ -1,13 +1,18 @@
 # Copyright 2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 #
-{pkgs, ...}: {
+{
+  pkgs,
+  self,
+  ...
+}: {
   name = "zathura";
   packages = [pkgs.zathura];
   macAddress = "02:00:00:03:07:01";
   ramMb = 512;
   cores = 1;
   extraModules = [
+    self.nixosModules.reference
     {
       time.timeZone = "Asia/Dubai";
       ghaf.programs.zathura.enable = true;
