bac45726a015115f9029d8e67e4f6a9721c8af15
Brian McGillion <bmg.avoin@gmail.com>
2024-06-28 20:22:32 +0400

    Rename the basic disko config
    The basic disko config is not hw specific so it can be renamed to
    reflect that it is only a very generic partitioning.
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- modules/disko/flake-module.nix
+++ modules/disko/flake-module.nix
@@ -2,11 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 {inputs, ...}: {
   flake.nixosModules = {
-    # TODO: rename this module to what it actually does rather than what model it's for.
-    # We version the disko partitiong module so that we can update it without breaking existing systems
-    disko-lenovo-x1-basic-v1.imports = [
+    disko-basic-partition-v1.imports = [
       inputs.disko.nixosModules.disko
-      ./lenovo-x1-disko-basic.nix
+      ./disko-basic-partition-v1.nix
       ./disko-basic-postboot.nix
     ];
   };
