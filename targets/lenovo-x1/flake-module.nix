b247035b08c51fc7b64485358c3404b9df5f3152
Brian McGillion <bmg.avoin@gmail.com>
2024-06-29 11:46:26 +0400

    Merge everything.nix back to flake-module
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- targets/lenovo-x1/flake-module.nix
+++ targets/lenovo-x1/flake-module.nix
@@ -7,9 +7,67 @@
   self,
   ...
 }: let
-  name = "lenovo-x1-carbon";
   system = "x86_64-linux";
-  targets = import ./everything.nix {inherit self lib name system;};
+
+  mvp-definition = machineType: variant: extraModules: let
+    hostConfiguration = lib.nixosSystem {
+      inherit system;
+      modules =
+        [
+          self.nixosModules.profiles
+          self.nixosModules.laptop
+
+          #TODO can we move microvm to the profile/laptop-x86?
+          self.nixosModules.microvm
+          #TODO see the twisted dependencies in common/desktop
+
+          ({config, ...}: {
+            time.timeZone = "Asia/Dubai";
+
+            ghaf = {
+              profiles = {
+                mvp-user-trial.enable = true;
+
+                laptop-x86 = {
+                  enable = true;
+                  netvmExtraModules = [self.nixosModules.reference-services];
+                  guivmExtraModules = [self.nixosModules.reference-programs];
+                  inherit (config.ghaf.reference.appvms) enabled-app-vms;
+                };
+
+                # variant type, turn on debug or release
+                debug.enable = variant == "debug";
+                release.enable = variant == "release";
+              };
+            };
+          })
+        ]
+        ++ extraModules;
+    };
+  in {
+    inherit hostConfiguration;
+    name = "${machineType}-${variant}";
+    package = hostConfiguration.config.system.build.diskoImages;
+  };
+
+  targets = [
+    (mvp-definition "lenovo-x1-carbon-gen10" "debug" [
+      self.nixosModules.disko-basic-partition-v1
+      {ghaf.hardware.definition.configFile = "/lenovo-x1/definitions/x1-gen10.nix";}
+    ])
+    (mvp-definition "lenovo-x1-carbon-gen11" "debug" [
+      self.nixosModules.disko-basic-partition-v1
+      {ghaf.hardware.definition.configFile = "/lenovo-x1/definitions/x1-gen11.nix";}
+    ])
+    (mvp-definition "lenovo-x1-carbon-gen10" "release" [
+      self.nixosModules.disko-basic-partition-v1
+      {ghaf.hardware.definition.configFile = "/lenovo-x1/definitions/x1-gen10.nix";}
+    ])
+    (mvp-definition "lenovo-x1-carbon-gen11" "release" [
+      self.nixosModules.disko-basic-partition-v1
+      {ghaf.hardware.definition.configFile = "/lenovo-x1/definitions/x1-gen11.nix";}
+    ])
+  ];
 in {
   flake = {
     nixosConfigurations =
