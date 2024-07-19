082b7b13f5d2ea1e2b00c28a49aed04aaf83e398
Vunny Sodhi <vunny.sodhi@unikie.com>
2024-07-19 11:39:24 +0300

    gtklock: Fix screen goes black after wrong passwords
    This patch will fix SP-4849 issue and also reduce
    the possibility of core dumps which can happen due
    to race condition.
    
    Signed-off-by: Vunny Sodhi <vunny.sodhi@unikie.com>

---
+++ overlays/custom-packages/gtklock/default.nix
@@ -0,0 +1,15 @@
+# Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# This overlay will apply gtklock patches which are merged in master branch
+# https://github.com/jovanlanik/gtklock/commit/d22127f0fd61bbeba8c12378b3c5b46cc3064d63
+# https://github.com/jovanlanik/gtklock/commit/e0e7f6d5ae7667fcc3479b6732046c67275b2f2f
+# TODO: Remove patches, once there new release for gtlk-lock
+#
+{prev}:
+prev.gtklock.overrideAttrs {
+  patches = [
+    ./auth-guard-against-race-condition-with-messages.patch
+    ./update.patch
+  ];
+}
