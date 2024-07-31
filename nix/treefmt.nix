102abcd2999bb3c2270ea812535a0b9d2ffd994c
Brian McGillion <bmg.avoin@gmail.com>
2024-07-31 07:10:07 +0400

    Fix the treefmt configuration
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -1,6 +1,11 @@
 # Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{
+{inputs, ...}: {
+  imports = [
+    inputs.flake-root.flakeModule
+    inputs.treefmt-nix.flakeModule
+    inputs.pre-commit-hooks-nix.flakeModule
+  ];
   perSystem = {
     config,
     pkgs,
@@ -29,7 +34,44 @@
 
         # Bash
         shellcheck.enable = true; # lints shell scripts https://github.com/koalaman/shellcheck
+
+        yamlfmt.enable = true; # YAML formatter
       };
+
+      settings.global.excludes = [
+        "*.key"
+        "*.lock"
+        "*.config"
+        "*.dts"
+        "*.pfx"
+        "*.p12"
+        "*.crt"
+        "*.cer"
+        "*.csr"
+        "*.der"
+        "*.jks"
+        "*.keystore"
+        "*.pem"
+        "*.pkcs12"
+        "*.pfx"
+        "*.p12"
+        "*.pem"
+        "*.pkcs7"
+        "*.p7b"
+        "*.p7c"
+        "*.p7r"
+        "*.p7m"
+        "*.p7s"
+        "*.p8"
+        "*.png"
+        "*.svg"
+        "*.license"
+        "*.db"
+        "*.mp3"
+        "*.txt"
+        #TODO: fix the MD
+        "*.md"
+      ];
     };
 
     formatter = config.treefmt.build.wrapper;
