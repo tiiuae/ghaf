93d5c8a2b7e951880306cf95aebe2f840022d736
Kim Sandström <kim.sandstrom@unikie.com>
2024-08-02 08:15:02 +0000

    kernel has working passthrough for GPIO
      - gpio-virt commit 71192f60bd8b8f6be4d529c49b7c33529e21a1e2
      - kernel 5.10 commit fb9220ac23c9892ec6433f0e839b8f919511a927
      - qemu in ghaf is not yet updated
      - use qemu 8.1.2 patch
              in commit 95d5cdb19713369a9dceab53659628292d9ff884
      - precompiled into generation 299 (on dev system)

--- modules/microvm/virtualization/microvm/simple-chardev-test.sh
+++ modules/microvm/virtualization/microvm/simple-chardev-test.sh
@@ -1,72 +1,86 @@
-  # For pin names look at gpio_40pin_header.png
-  # some examples
-  # GPIO09  PBB.00  - gpiochip 1, offset 8/0x08
-  # GPIO08  PBB.01  - gpiochip 1, offset 9/0x09
-  # GPIO17  PP.04  - gpiochip 0, offset 96/0x60
-  # GPIO27  PN.01  - gpiochip 0, offset 85/0x55
-  # GPIO35  PH.00  - gpiochip 0, offset 43/0x2B
+# For pin names look at gpio_40pin_header.pn8
+# some examples
+# GPIO09  PBB.00  - gpiochip 1, offset 8/0x00
+# GPIO08  PBB.01  - gpiochip 1, offset 9/0x09
+# GPIO17  PP.04  - gpiochip 0, offset 96/0x60
+# GPIO27  PN.01  - gpiochip 0, offset 85/0x55
+# GPIO35  PH.00  - gpiochip 0, offset 43/0x2B
 
-  # chipnum='\x00'      # tegra234-gpio
-  # chipnum='\x01'    # tegra234-gpio-aon
-  lvl0='\x00'
-  lvl1='\x01'
-  n_a='\x00'
-  offset='\x55'       # example: PN.01/GPIO27 has line offset hex '\x55'
-  pad='\x00\x00\x00\x00'
-  # chardev='/dev/gpio-host'
-  chardev='/dev/gpio-guest'
+chipnum='\x00'      # tegra234-gpio
+# chipnum='\x01'    # tegra234-gpio-aon
+offset='\x55'       # PN.01/GPIO27 # line offset in hex
 
-  function res {
-    signal='r'  # reserve line
-    echo -n -e ${signal}${chipnum}${n_a}${offset}${pad} >> ${chardev}
+lvl0='\x00'
+lvl1='\x01'
+n_a='\x00'
 
-    signal='o'  # set pin as output
-    # this will also set a level
-    echo -n -e ${signal}${chipnum}${lvl1}${offset}${pad} >> ${chardev}
-  }
+# wait_time='0.0005'
+wait_time='0'
+chardev=$(ls /dev/gpio-[gh]*st)
+echo -e "using ${chardev}\n"
 
-  function setlevel {
-    echo -n -e ${signal}${chipnum}${level}${offset}${pad} >> ${chardev}
-    sleep 0.0050
-  }
+function read_ret {
+  echo "retval:"
+  dd if=${chardev} bs=1 count=4 | hexdump -C
+}
 
-  function free {
-    signal='f'  # free line
-    echo -n -e ${signal}${chipnum}${n_a}${offset}${pad} >> ${chardev}
-  }
+function res {
+  signal='r'  # reserve line
+  echo -n -e ${chipnum}${signal}${n_a}${offset} >> ${chardev}
 
-while true
-do
-  sleep 10
-  echo -n '.'
+  read_ret
 
-  chipnum='\x01';offset='\x08'; res
-  chipnum='\x01';offset='\x09'; res
-  chipnum='\x00';offset='\x2B'; res
-  chipnum='\x00';offset='\x55'; res
+  signal='o'  # set pin as output
+  # this will also set a level
+  echo -n -e ${chipnum}${signal}${lvl1}${offset} >> ${chardev}
 
-  i=10;
-  signal='s'  # set level
-  while [ $i -gt 0 ]
-    do let i=$i-1
+  read_ret
+}
 
-      level='\x01'
-      chipnum='\x01';offset='\x08'; setlevel
-      chipnum='\x00';offset='\x55'; setlevel
-      chipnum='\x01';offset='\x09'; setlevel
-      chipnum='\x00';offset='\x2B'; setlevel
+function setlevel {
+  signal='s'
+  echo -n -e ${chipnum}${signal}${level}${offset} >> ${chardev}
+  sleep ${wait_time}
+}
 
-      level='\x00'
-      chipnum='\x01';offset='\x08'; setlevel
-      chipnum='\x00';offset='\x55'; setlevel
-      chipnum='\x01';offset='\x09'; setlevel
-      chipnum='\x00';offset='\x2B'; setlevel
+function getlevel {
+  signal='g'  # set pin as output
+  echo -n -e ${chipnum}${signal}${level}${offset} >> ${chardev}
+  sleep ${wait_time}
+
+  read_ret
+}
+
+function freeline {
+  signal='f'  # free line
+  echo -n -e ${chipnum}${signal}${n_a}${offset} >> ${chardev}
+}
+
+chipnum='\x01';offset='\x08'; res
+chipnum='\x00';offset='\x55'; res
+chipnum='\x01';offset='\x09'; res
+chipnum='\x00';offset='\x2B'; res
+
+i=10;
+signal='s'  # set level
+while [ $i -gt 0 ]
+  do let i=$i-1
+
+    level='\x01'
+    chipnum='\x01';offset='\x08'; setlevel
+    chipnum='\x00';offset='\x55'; setlevel
+    chipnum='\x01';offset='\x09'; setlevel
+    chipnum='\x00';offset='\x2B'; setlevel
+
+    level='\x00'
+    chipnum='\x01';offset='\x08'; setlevel
+    chipnum='\x00';offset='\x55'; setlevel
+    chipnum='\x01';offset='\x09'; setlevel
+    chipnum='\x00';offset='\x2B'; setlevel
 
   done
 
-  chipnum='\x01';offset='\x02'; free 
-  chipnum='\x01';offset='\x08'; free
-  chipnum='\x00';offset='\x2B'; free
-  chipnum='\x00';offset='\x55'; free
-
-done
+chipnum='\x01';offset='\x08'; freeline
+chipnum='\x00';offset='\x55'; freeline
+chipnum='\x01';offset='\x09'; freeline
+chipnum='\x00';offset='\x2B'; freeline
