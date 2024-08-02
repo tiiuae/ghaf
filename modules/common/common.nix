155da1d74105382ca90b31d41c931a85d8ffb1c7
Santtu Lakkala <santtu.lakkala@unikie.com>
2024-08-02 17:40:28 +0300

    Enable setting locale and timezone
    Bump ghaf-givc to a version with locale and timezone setting features.
    
    Add patches to re-enable set-locale command in localed, and pass
    LOCALE_ARCHIVE environment variable to localed. Add locales to be
    generated.
    
    Remove default timezone to enable set-timezone in timedatectl.
    
    Signed-off-by: Santtu Lakkala <santtu.lakkala@unikie.com>

--- modules/common/common.nix
+++ modules/common/common.nix
@@ -24,5 +24,18 @@
     boot.enableContainers = false;
     documentation.nixos.enable = false;
     ##### Remove to here
+
+    i18n.supportedLocales = [
+      "C.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+      "ar_AE.UTF-8/UTF-8"
+    ];
+    environment.extraInit = ''
+      if [ -f /etc/locale.conf ]; then
+          . /etc/locale.conf
+          export LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT LC_IDENTIFICATION LC_ALL
+      fi
+    '';
+
   };
 }
