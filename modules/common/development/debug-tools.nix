c15e06e94456ec68729349728d48f4f9c8b25446
Brian McGillion <bmg.avoin@gmail.com>
2024-08-14 10:25:26 +0400

    TMP: Disable the perf again
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- modules/common/development/debug-tools.nix
+++ modules/common/development/debug-tools.nix
@@ -59,13 +59,15 @@ in {
       ++
       # Match perf version with kernel.
       [
-        (config.boot.kernelPackages.perf.override {python3 = pkgs.python311;})
+        #(config.boot.kernelPackages.perf.override {python3 = pkgs.python311;})
         sysbench-test-script
         sysbench-fileio-test-script
         nvpmodel-check
         rm-linux-bootmgrs
       ]
       ++ rmDesktopEntries [pkgs.htop]
+      #TODO tmp disable perf as it is broken in cross-compiled Orin AGX/NX
+      ++ lib.optional (config.nixpkgs.hostPlatform.system != "aarch64-linux") config.boot.kernelPackages.perf
       # LuaJIT (which is sysbench dependency) not available on RISC-V
       ++ lib.optional (config.nixpkgs.hostPlatform.system != "riscv64-linux") pkgs.sysbench
       # Icicle Kit performance test script available on RISC-V
