01e103f16c6c1cdfabb5e17517270019fbbc3ec7
Brian McGillion <bmg.avoin@gmail.com>
2024-08-19 11:25:36 +0400

    Change the project description
    Fixes #33
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {
-  description = "Ghaf - Documentation and implementation for TII SSRC Secure Technologies Ghaf Framework";
+  description = "Ghaf Framework: Documentation and implementation for TII SSRC Secure Technologies";
 
   nixConfig = {
     substituters = [
