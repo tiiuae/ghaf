ca12ab796b953725a9abd9c568233b0763bd14f4
Manuel Bluhm <manuel@ssrc.tii.ae>
2024-08-26 17:20:19 +0400

    Move mitm certificate to appvms
    Signed-off-by: Manuel Bluhm <manuel@ssrc.tii.ae>

--- modules/reference/appvms/gala.nix
+++ modules/reference/appvms/gala.nix
@@ -1,25 +1,13 @@
 # Copyright 2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 #
-{
-  pkgs,
-  lib,
-  config,
-  ...
-}:
+{ pkgs, config, ... }:
 {
   name = "gala";
   packages = [ pkgs.gala-app ];
   macAddress = "02:00:00:03:06:01";
   ramMb = 1536;
   cores = 2;
-  extraModules = [
-    {
-      time.timeZone = config.time.timeZone;
-      security.pki.certificateFiles = lib.mkIf config.ghaf.virtualization.microvm.idsvm.mitmproxy.enable [
-        ../../../modules/microvm/virtualization/microvm/idsvm/mitmproxy/mitmproxy-ca/mitmproxy-ca-cert.pem
-      ];
-    }
-  ];
+  extraModules = [ { time.timeZone = config.time.timeZone; } ];
   borderColor = "#027d7b";
 }
