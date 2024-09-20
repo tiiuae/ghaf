d54efc844089058092664052d05a1d3c490fbbaf
Emrah Billur <emrah.billur@unikie.com>
2024-09-20 15:14:48 +0300

    AGX nmcli for netvm
    Remove unwanted visual NeworkManager plugins preventing cross compilation
    Added nmcli to netvm
    Wifi Module Addded
    
    
    Signed-off-by: Emrah Billur <emrah.billur@unikie.com>

--- modules/reference/hardware/jetpack-microvm/agx-netvm-wlan-pci-passthrough.nix
+++ modules/reference/hardware/jetpack-microvm/agx-netvm-wlan-pci-passthrough.nix
@@ -1,6 +1,11 @@
 # Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   cfg = config.ghaf.hardware.nvidia.orin.agx;
 in
@@ -10,14 +15,22 @@ in
     # Orin AGX WLAN card PCI passthrough
     ghaf.hardware.nvidia.orin.enablePCIPassthroughCommon = true;
 
+    # Common Wifi Service set
+
+    # Passthrough devices
     ghaf.virtualization.microvm.netvm.extraModules = [
       {
+        ghaf.services.wifi.enable = true;
         microvm.devices = [
           {
             bus = "pci";
             path = "0001:01:00.0";
           }
         ];
+        # Network Manager is defined for netvm of Orin Devices
+        environment.systemPackages = [ pkgs.networkmanager ];
+        # Network Manager package defines a gnome plugin with build failure on Orin
+        networking.networkmanager.plugins = lib.mkForce [ ];
       }
     ];
 
