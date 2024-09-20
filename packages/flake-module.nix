2a070c3eee70366d14378d3bddcec61b9f17a0de
Brian McGillion <bmg.avoin@gmail.com>
2024-09-20 19:30:56 +0400

    store the screenshots from grim across reboots
    In addition this patch adds the ability to select the capture area that
    is used for the screenshots. When the screenshot is selected there is
    Satty functionality that allows for some basic post capture editing in
    the form of an overlay to add annotations such as, arrows, boxes, and
    text.
    
    The user can the save the file, or copy it to the clipboard. Once in the
    clipboard it can be pasted into any of the other vms where there is an
    app that will accept those images. This is the case for e.g. slack and
    outlook.
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -24,6 +24,7 @@
         windows-launcher = callPackage ./windows-launcher { enableSpice = false; };
         windows-launcher-spice = callPackage ./windows-launcher { enableSpice = true; };
         hardware-scan = callPackage ./hardware-scan { };
+        ghaf-screenshot = callPackage ./ghaf-screenshot { };
         doc = callPackage ../docs {
           revision = lib.strings.fileContents ../.version;
           options =
