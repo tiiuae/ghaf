569e3df19e7211705edef42af018820a3ddc5543
vadik likholetov <vadikas@gmail.com>
2024-09-24 11:29:53 +0300

    Refactor Office 365 firewall setup for BusinessVM
    Signed-off-by: vadik likholetov <vadikas@gmail.com>

--- modules/reference/appvms/business.nix
+++ modules/reference/appvms/business.nix
@@ -11,6 +11,62 @@ let
   #TODO: Move this to a common place
   xdgPdfPort = 1200;
   name = "business";
+  
+  # use nix-prefetch-url to calculate sha256 checksum
+  endpointsFile = pkgs.fetchurl {
+    url = "https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7";
+    sha256 = "1zly0g23vray4wg6fjxxdys6zzksbymlzggbg75jxqcf8g9j6xnw";
+  };
+
+  generateO365FWScript = pkgs.writeShellScript "generate-o365fw-script" ''
+    #!/usr/bin/env bash
+
+    set -euo pipefail
+
+    ENDPOINTS_FILE="${endpointsFile}"
+
+    preprocess_url() {
+        local url="$1"
+        # Return the URL as-is without any filtering
+        echo "$url"
+    }
+
+    generate_iptables_rules() {
+        echo "# Office 365 Firewall Rules"
+        echo
+
+        jq -r '.[] | select(.category == "Optimize" or .category == "Allow" or .category == "Default") | .ips[]?' "$ENDPOINTS_FILE" | sort -u | while read -r ip; do
+            if [[ $ip == *":"* ]]; then
+                echo "ip6tables -I OUTPUT -d $ip -j ACCEPT"
+                echo "ip6tables -I INPUT -d $ip -j ACCEPT"
+            else
+                echo "iptables -I OUTPUT -d $ip -j ACCEPT"
+                echo "iptables -I INPUT -d $ip -j ACCEPT"
+            fi
+        done
+
+        echo
+
+        jq -r '.[] | select(.category == "Optimize" or .category == "Allow" or .category == "Default") | .urls[]?' "$ENDPOINTS_FILE" | sort -u | while read -r url; do
+            processed_url=$(preprocess_url "$url")
+            if [[ "$processed_url" != "" ]]; then
+                echo "iptables -I OUTPUT -p tcp --dport 80 -m string --string \"$processed_url\" --algo bm -j ACCEPT"
+                echo "iptables -I INPUT -p tcp --dport 80 -m string --string \"$processed_url\" --algo bm -j ACCEPT"
+                echo "iptables -I OUTPUT -p tcp --dport 443 -m string --string \"$processed_url\" --algo bm -j ACCEPT"
+                echo "iptables -I INPUT -p tcp --dport 443 -m string --string \"$processed_url\" --algo bm -j ACCEPT"
+            fi
+        done
+    }
+
+    generate_iptables_rules
+  '';
+
+  o365fw = pkgs.runCommand "o365fw" {
+    buildInputs = [ pkgs.jq ];
+  } ''
+    ${generateO365FWScript} > $out 
+  '';
+
 in
 {
   name = "${name}";
@@ -98,153 +154,31 @@ in
         csdWrapper = "${pkgs.openconnect}/libexec/openconnect/hipreport.sh";
       };
 
+
       #Firewall Settings
       networking = {
         firewall.enable = true;
-        firewall.extraCommands = ''
-
+        firewall.extraCommands = 
+        '' 
           iptables -F
-            add_rule() {
+
+          ${builtins.readFile o365fw} 
+        '' 
+           + 
+        ''
+
+          add_rule() {
               local ip=$1
               iptables -I OUTPUT -p tcp -d $ip --dport 80 -j ACCEPT
               iptables -I OUTPUT -p tcp -d $ip --dport 443 -j ACCEPT
               iptables -I INPUT -p tcp -s $ip --sport 80 -j ACCEPT
               iptables -I INPUT -p tcp -s $ip --sport 443 -j ACCEPT
-            }
-           # Urls can be found from Source: https://learn.microsoft.com/en-us/microsoft-365/enterprise/urls-and-ip-address-ranges
-           # Allow microsoft365.com
-           add_rule 13.107.6.156
-           add_rule 13.107.9.156
+          }
 
-           # Exchange
-           add_rule 13.107.6.152/31
-           add_rule 13.107.18.10/31
-           add_rule 13.107.128.0/22
-           add_rule 23.103.160.0/20
-           add_rule 40.96.0.0/13
-           add_rule 40.104.0.0/15
-           add_rule 52.96.0.0/14
-           add_rule 131.253.33.215/32
-           add_rule 132.245.0.0/16
-           add_rule 150.171.32.0/22
-           add_rule 204.79.197.215/32
-
-
-           # Exchange Online
-           add_rule 40.92.0.0/15
-           add_rule 40.107.0.0/16
-           add_rule 52.100.0.0/14
-           add_rule 52.238.78.88/32
-           add_rule 104.47.0.0/17
-
-
-           # Sharepoint
-           add_rule 13.107.136.0/22
-           add_rule 40.108.128.0/17
-           add_rule 52.104.0.0/14
-           add_rule 104.146.128.0/17
-           add_rule 150.171.40.0/22
-
-
-           # Common
-           add_rule 13.107.6.171/32
-           add_rule 13.107.18.15/32
-           add_rule 13.107.140.6/32
-           add_rule 52.108.0.0/14
-           add_rule 52.244.37.168/32
-           add_rule 20.20.32.0/19
-           add_rule 20.190.128.0/18
-           add_rule 20.231.128.0/19
-           add_rule 40.126.0.0/18
-           add_rule 13.107.6.192/32
-           add_rule 13.107.9.192/32
-           add_rule 52.108.0.0/14
-
-           # Teams
-           add_rule 13.107.64.0/18
-           add_rule 52.112.0.0/14
-           add_rule 52.122.0.0/15
-           add_rule 52.108.0.0/14
-           add_rule 52.238.119.141/32
-           add_rule 52.244.160.207/32
-           add_rule 2.16.234.57
-           add_rule 23.56.21.152
-           add_rule 23.33.233.129
-           add_rule 52.123.0.0/16
-
-
-           # Allow VPN access.tii.ae and iservice
+          # Allow VPN access.tii.ae and iservice
            add_rule 151.253.154.18
            add_rule 10.161.10.120
-
-           # To be checked
-           # Allow res.cdn.office.net
-           add_rule 152.199.21.175
-           add_rule 152.199.39.108
-           add_rule 2.21.231.0/24
-           add_rule 2.20.249.0/24
-           add_rule 152.199.0.0/16
-
-
-           # Allow js.monitor.azure.com
-           add_rule 13.107.246.0/24
-
-           # Allow c.s-microsoft.com
-           add_rule 23.207.193.242
-           add_rule 23.208.213.121
-           add_rule 23.208.173.122
-           add_rule 23.44.1.243
-           add_rule 104.65.229.0/24
-           add_rule 23.53.113.0/24
-           add_rule 2.19.105.47
-
-           # Allow microsoft.com
-           add_rule 20.70.246.20
-           add_rule 20.236.44.162
-           add_rule 20.76.201.171
-           add_rule 20.231.239.246
-           add_rule 20.112.250.133
-           add_rule 184.25.221.172
-
-           # statics.teams.cdn.office.net
-           add_rule 95.101.0.0/16
-           add_rule 184.87.193.0/24
-           add_rule 23.44.0.0/14
-           add_rule 96.16.53.0/24
-           add_rule 23.59.80.0/24
-           add_rule 23.202.33.0/24
-           add_rule 104.73.172.0/24
-           add_rule 184.27.123.0/24
-           add_rule 2.16.56.0/24
-           add_rule 23.219.73.130
-           add_rule 104.93.18.174
-           add_rule 2.21.225.158
-           add_rule 23.45.137.145
-           add_rule 23.48.121.167
-           add_rule 23.46.197.94
-           add_rule 104.80.21.47
-           add_rule 23.195.154.8
-           add_rule 193.229.113.0/24
-
-           # edge.skype.com for teams
-           add_rule 13.107.254.0/24
-           add_rule 13.107.3.0/24
-
-           # api.flightproxy.skype.com for teams
-           add_rule 98.66.0.0/16
-           add_rule 4.208.0.0/16
-           add_rule 4.225.208.0/24
-           add_rule 4.210.0.0/16
-           add_rule 108.141.240.0/24
-           add_rule 74.241.0.0/16
-           add_rule 20.216.0.0/16
-           add_rule 172.211.0.0/16
-           add_rule 20.50.217.0/24
-           add_rule 68.219.14.0/24
-           add_rule 20.107.136.0/24
-           add_rule 4.175.191.0/24
-           add_rule 98.64.0.0/16
-
+   
            # Allow tiiuae.sharepoint.com
            add_rule 52.104.7.53
            add_rule 52.105.255.39
@@ -254,26 +188,6 @@ in
            add_rule 104.69.171.0/24
            add_rule 13.107.136.10
            add_rule 23.15.111.0/24
-           # Allow shell.cdn.office.net
-           add_rule 23.50.92.176
-           add_rule 23.15.30.57
-           add_rule 23.50.187.58
-           add_rule 104.73.234.244
-           add_rule 104.83.143.131
-           # Allow res-1.cdn.office.net
-           add_rule 23.52.40.0/24
-           add_rule 23.64.122.0/24
-           add_rule 2.16.106.0/24
-           # Allow publiccdn.sharepointonline.com
-           add_rule 23.50.86.117
-           add_rule 104.69.168.125
-           add_rule 2.16.43.238
-           add_rule 23.34.79.0/24
-           add_rule 23.39.68.0/24
-           # r4.res.office365.com
-           add_rule 2.19.97.32
-           add_rule 2.22.61.139
-
 
            # Block all other HTTP and HTTPS traffic
            iptables -A OUTPUT -p tcp --dport 80 -j REJECT
