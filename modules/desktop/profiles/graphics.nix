8fcfa409caaa2391938c62f45eb8240e0bc81acd
Brian McGillion <bmg.avoin@gmail.com>
2024-10-01 11:23:08 +0400

    noXlibs has been deprecated
    The setting for environment.noXlibs has been removed and the
    functionality of the setting has been deprecated.
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- modules/desktop/profiles/graphics.nix
+++ modules/desktop/profiles/graphics.nix
@@ -88,19 +88,20 @@ in
       enable = true;
       extraPackages = mkIf pkgs.stdenv.hostPlatform.isx86 [ pkgs.intel-media-driver ];
     };
-    environment.noXlibs = false;
-    environment.sessionVariables = {
-      WLR_RENDERER = cfg.renderer;
-      XDG_SESSION_TYPE = "wayland";
-      WLR_NO_HARDWARE_CURSORS = if (cfg.renderer == "pixman") then 1 else 0;
-      XKB_DEFAULT_LAYOUT = "us,ara,fi";
-      XKB_DEFAULT_OPTIONS = "grp:alt_shift_toggle";
-      # Set by default in labwc, but possibly not in other compositors
-      XDG_CURRENT_DESKTOP = "wlroots";
-      _JAVA_AWT_WM_NONREPARENTING = 1;
-    };
+    environment = {
+      sessionVariables = {
+        WLR_RENDERER = cfg.renderer;
+        XDG_SESSION_TYPE = "wayland";
+        WLR_NO_HARDWARE_CURSORS = if (cfg.renderer == "pixman") then 1 else 0;
+        XKB_DEFAULT_LAYOUT = "us,ara,fi";
+        XKB_DEFAULT_OPTIONS = "grp:alt_shift_toggle";
+        # Set by default in labwc, but possibly not in other compositors
+        XDG_CURRENT_DESKTOP = "wlroots";
+        _JAVA_AWT_WM_NONREPARENTING = 1;
+      };
 
-    environment.systemPackages = lib.optionals config.ghaf.profiles.debug.enable [ ghaf-open ];
+      systemPackages = lib.optionals config.ghaf.profiles.debug.enable [ ghaf-open ];
+    };
 
     ghaf.graphics = {
       labwc.enable = cfg.compositor == "labwc";
