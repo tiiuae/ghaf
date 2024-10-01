91b95dfbd339f857f25174f33baa3f77290a3f92
Kajus Naujokaitis <kajus.naujokaitis@unikie.com>
2024-10-01 17:04:07 +0300

    Added additional options to audio-ctrl script
    Signed-off-by: Kajus Naujokaitis <kajus.naujokaitis@unikie.com>

--- packages/audio-ctrl/default.nix
+++ packages/audio-ctrl/default.nix
@@ -12,18 +12,34 @@ writeShellApplication {
 
     case "$1" in
       inc)
+        # Increase volume by 5%
         pamixer -i 5
         ;;
       dec)
+        # Decrease volume by 5%
         pamixer -d 5
         ;;
       mut)
+        # Toggle mute
         if [ "$(pamixer --get-mute)" = "false" ]; then
           pamixer -m
         else
           pamixer -u
         fi
         ;;
-      esac
+      get)
+        # Get current volume level
+        pamixer --get-volume
+        ;;
+      set)
+        # Set volume to a specific level
+        if [ -n "$2" ]; then
+          pamixer --set-volume "$2"
+        fi
+        ;;
+      *)
+        echo "Usage: $0 {inc|dec|mut|get|set <volume>}"
+        ;;
+    esac
   '';
 }
