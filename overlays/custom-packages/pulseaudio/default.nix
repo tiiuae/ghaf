f2b9680ba825938e4040f329cc724a9575f76e36
Jon Sahlberg <jon.sahlberg@unikie.com>
2024-11-05 09:42:25 +0200

    Change to use pulseaudio old tunnelin module
    There seems to be issues with pulseaudio module-tunnel -new modules
    The moules cause desynch in audio that is not recoverable. The bugs
    of that are some 10 years old and seem not to get fixed.

--- overlays/custom-packages/pulseaudio/default.nix
+++
@@ -1,7 +0,0 @@
-# Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
-# SPDX-License-Identifier: Apache-2.0
-{ prev, ... }:
-prev.pulseaudio.overrideAttrs (_prevAttrs: {
-  # This patch enables the live switching of pulse tunnels to different sinks
-  patches = _prevAttrs.patches ++ [ ./pulseaudio-remove-dont-move.patch ];
-})
