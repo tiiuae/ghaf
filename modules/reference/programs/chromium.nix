8c7feb2e4d7972d592f3c01ff6263c8d4db8f474
Yuri Nesterov <yuriy.nesterov@unikie.com>
2024-11-29 16:43:51 +0200

    Refactor VM and application definitions
    Signed-off-by: Yuri Nesterov <yuriy.nesterov@unikie.com>

--- modules/reference/programs/chromium.nix
+++ modules/reference/programs/chromium.nix
@@ -1,13 +1,18 @@
 # Copyright 2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.ghaf.reference.programs.chromium;
 in
 {
   options.ghaf.reference.programs.chromium = {
     enable = lib.mkEnableOption "Enable Chromium program settings";
-    useZathuraVM = lib.mkEnableOption "Open PDFs in Zathura VM";
+    openInNormalExtension = lib.mkEnableOption "browser extension to open links in the normal browser";
   };
   config = lib.mkIf cfg.enable {
     programs.chromium = {
@@ -21,6 +26,32 @@ in
 
       # Don't use pdf.js, open externally.
       extraOpts."AlwaysOpenPdfExternally" = true;
+
+    };
+
+    environment.etc = lib.mkIf (cfg.openInNormalExtension && config.ghaf.givc.enable) {
+      "chromium/native-messaging-hosts/fi.ssrc.open_normal.json" = {
+        source = "${pkgs.open-normal-extension}/fi.ssrc.open_normal.json";
+      };
+
+      "open-normal-extension.cfg" = {
+        text =
+          let
+            cliArgs = builtins.replaceStrings [ "\n" ] [ " " ] ''
+              --name ${config.ghaf.givc.adminConfig.name}
+              --addr ${config.ghaf.givc.adminConfig.addr}
+              --port ${config.ghaf.givc.adminConfig.port}
+              ${lib.optionalString config.ghaf.givc.enableTls "--cacert /run/givc/ca-cert.pem"}
+              ${lib.optionalString config.ghaf.givc.enableTls "--cert /run/givc/business-vm-cert.pem"}
+              ${lib.optionalString config.ghaf.givc.enableTls "--key /run/givc/business-vm-key.pem"}
+              ${lib.optionalString (!config.ghaf.givc.enableTls) "--notls"}
+            '';
+          in
+          ''
+            export GIVC_PATH="${pkgs.givc-cli}"
+            export GIVC_OPTS="${cliArgs}"
+          '';
+      };
     };
   };
 }
--- modules/reference/programs/google-chrome.nix
+++ modules/reference/programs/google-chrome.nix
@@ -1,17 +1,22 @@
 # Copyright 2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.ghaf.reference.programs.google-chrome;
 in
 {
   options.ghaf.reference.programs.google-chrome = {
     enable = lib.mkEnableOption "Enable Google chrome program settings";
-    useZathuraVM = lib.mkEnableOption "Open PDFs in Zathura VM";
+    openInNormalExtension = lib.mkEnableOption "browser extension to open links in the normal browser";
     defaultPolicy = lib.mkOption {
       type = lib.types.attrs;
       description = ''
-          Google chrome policy options. A list of available policies
+        Google chrome policy options. A list of available policies
         can be found in the Chrome Enterprise documentation:
         <https://cloud.google.com/docs/chrome-enterprise/policies/>
         Make sure the selected policy is supported on Linux and your browser version.
@@ -68,20 +73,45 @@ in
   };
   config = lib.mkIf cfg.enable {
 
-    environment.etc = {
-      "opt/chrome/policies/managed/default.json" = lib.mkIf (cfg.defaultPolicy != { }) {
-        text = builtins.toJSON cfg.defaultPolicy;
-        user = "${cfg.policyOwner}"; # Owner is proxy-user
-        group = "${cfg.policyOwnerGroup}"; # Group is proxy-admin
-        mode = "0664"; # Permissions: read/write for owner/group, no permissions for others
-      };
-      "opt/chrome/policies/managed/extra.json" = {
-        text = builtins.toJSON cfg.extraOpts;
-        user = "${cfg.policyOwner}"; # Owner is proxy-user
-        group = "${cfg.policyOwnerGroup}"; # Group is proxy-admin
-        mode = "0664"; # Permissions: read/write for owner/group, no permissions for others
-      };
+    environment.etc = lib.mkMerge [
+      {
+        "opt/chrome/policies/managed/default.json" = {
+          text = builtins.toJSON cfg.defaultPolicy;
+          user = "${cfg.policyOwner}"; # Owner is proxy-user
+          group = "${cfg.policyOwnerGroup}"; # Group is proxy-admin
+          mode = "0664"; # Permissions: read/write for owner/group, no permissions for others
+        };
+        "opt/chrome/policies/managed/extra.json" = {
+          text = builtins.toJSON cfg.extraOpts;
+          user = "${cfg.policyOwner}"; # Owner is proxy-user
+          group = "${cfg.policyOwnerGroup}"; # Group is proxy-admin
+          mode = "0664"; # Permissions: read/write for owner/group, no permissions for others
+        };
+      }
+      (lib.mkIf (cfg.openInNormalExtension && config.ghaf.givc.enable) {
+        "opt/chrome/native-messaging-hosts/fi.ssrc.open_normal.json" = {
+          source = "${pkgs.open-normal-extension}/fi.ssrc.open_normal.json";
+        };
 
-    };
+        "open-normal-extension.cfg" = {
+          text =
+            let
+              cliArgs = builtins.replaceStrings [ "\n" ] [ " " ] ''
+                --name ${config.ghaf.givc.adminConfig.name}
+                --addr ${config.ghaf.givc.adminConfig.addr}
+                --port ${config.ghaf.givc.adminConfig.port}
+                ${lib.optionalString config.ghaf.givc.enableTls "--cacert /run/givc/ca-cert.pem"}
+                ${lib.optionalString config.ghaf.givc.enableTls "--cert /run/givc/business-vm-cert.pem"}
+                ${lib.optionalString config.ghaf.givc.enableTls "--key /run/givc/business-vm-key.pem"}
+                ${lib.optionalString (!config.ghaf.givc.enableTls) "--notls"}
+              '';
+            in
+            ''
+              export GIVC_PATH="${pkgs.givc-cli}"
+              export GIVC_OPTS="${cliArgs}"
+            '';
+        };
+      })
+    ];
   };
 }
