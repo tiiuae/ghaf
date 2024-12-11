62385df7208d18e29a68a371929f231362a1663b
Brian McGillion <bmg.avoin@gmail.com>
2024-12-11 08:47:45 +0400

    fix the formatting according to nixfmt
    somehow the formatting is off in these files. this has been adjusted
    now through running `nix fmt`
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- modules/reference/hardware/jetpack/nvidia-jetson-orin/pci-passthrough-common.nix
+++ modules/reference/hardware/jetpack/nvidia-jetson-orin/pci-passthrough-common.nix
@@ -5,7 +5,8 @@ let
   cfg = config.ghaf.hardware.nvidia.orin;
 in
 {
-  options.ghaf.hardware.nvidia.orin.enablePCIPassthroughCommon = lib.mkEnableOption "Enable common options related to PCI passthrough on Orin AGX and NX";
+  options.ghaf.hardware.nvidia.orin.enablePCIPassthroughCommon =
+    lib.mkEnableOption "Enable common options related to PCI passthrough on Orin AGX and NX";
   config = lib.mkIf cfg.enablePCIPassthroughCommon {
     boot.kernelModules = [
       "vfio_pci"
