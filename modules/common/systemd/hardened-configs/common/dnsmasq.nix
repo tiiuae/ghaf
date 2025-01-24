63bc463cf76f31c0366eb847e132f227dbedf673
Ganga Ram <Ganga.Ram@tii.ae>
2025-01-24 14:52:23 +0400

    More strict configs for dnsmasq service
    - Cleanup dbus dervice
    - Strict configs for dnsmasq service
    - Service is not enabled by default in Ghaf
    - Tested in net-vm using following config and allowing the port 1053:
    
    ```
      services.dnsmasq = {
        enable = true;
          settings = {
            bogus-priv = true;
            domain-needed = true;
            interface = "wlp0s5f0";
            listen-address = [ "0.0.0.0" ];
            port = 1053;
          };
         };
    ```
    
    Signed-off-by: Ganga Ram <Ganga.Ram@tii.ae>

--- modules/common/systemd/hardened-configs/common/dnsmasq.nix
+++ modules/common/systemd/hardened-configs/common/dnsmasq.nix
@@ -2,150 +2,60 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 {
-  ##############
-  # Networking #
-  ##############
-
-  # PrivateNetwork=true;
-  # IPAccounting=yes
-  # IPAddressDeny="any";
-  RestrictAddressFamilies = [
-    "AF_PACKET"
-    "AF_NETLINK"
-    "AF_UNIX"
-    "AF_INET"
-    "AF_INET6"
-  ];
-
-  ###############
-  # File system #
-  ###############
-
+  PrivateNetwork = false;
+  IPAccounting = true;
+  IPAddressDeny = "any";
   ProtectHome = true;
   ProtectSystem = true;
-  ProtectProc = "invisible";
-  # ReadWritePaths=[ "/etc"];
+  ProtectProc = "noaccess";
   PrivateTmp = true;
-
-  # Not applicable for the service runs as root
-  # PrivateMounts=true;
-  # ProcSubset="all";
-
-  ###################
-  # User separation #
-  ###################
-
-  # Not applicable for the service runs as root
-  # PrivateUsers=true;
-  # DynamicUser=true;
-
-  ###########
-  # Devices #
-  ###########
-
+  PrivateMounts = true;
+  ProcSubset = "pid";
+  PrivateUsers = false;
+  DynamicUser = false;
   PrivateDevices = true;
-  # DeviceAllow=/dev/null
-
-  ##########
-  # Kernel #
-  ##########
-
   ProtectKernelTunables = true;
   ProtectKernelModules = true;
   ProtectKernelLogs = true;
-
-  ########
-  # Misc #
-  ########
-
-  # Delegate=false;
-  # KeyringMode="private";
+  Delegate = false;
+  KeyringMode = "private";
   NoNewPrivileges = true;
-  # UMask=077;
+  UMask = 65;
   ProtectHostname = true;
   ProtectClock = true;
-  # ProtectControlGroups=true;
-  # RestrictNamespaces=true;
-  /*
-      RestrictNamespaces=[
-     #"~user"
-     #"~pid"
-     #"~net"
-     #"~uts"
-     #"~mnt"
-     #"~cgroup"
-     #"~ipc"
-    ];
-  */
+  ProtectControlGroups = true;
+  RestrictNamespaces = true;
   LockPersonality = true;
   MemoryDenyWriteExecute = true;
   RestrictRealtime = true;
   RestrictSUIDSGID = true;
-  # RemoveIPC=true
+  RemoveIPC = true;
   SystemCallArchitectures = "native";
-  # NotifyAccess=false;
+  NotifyAccess = "main";
 
-  ################
-  # Capabilities #
-  ################
-
-  #AmbientCapabilities=
-  CapabilityBoundingSet = [
-    "~CAP_SYS_PACCT"
-    "~CAP_KILL"
-    # "~CAP_WAKE_ALARM"
-    # "~CAP_DAC_*
-    "~CAP_FOWNER"
-    # "~CAP_IPC_OWNER"
-    # "~CAP_BPF"
-    "~CAP_LINUX_IMMUTABLE"
-    # "~CAP_IPC_LOCK"
-    "~CAP_SYS_MODULE"
-    "~CAP_SYS_TTY_CONFIG"
-    "~CAP_SYS_BOOT"
-    "~CAP_SYS_CHROOT"
-    # "~CAP_BLOCK_SUSPEND"
-    "~CAP_LEASE"
-    "~CAP_MKNOD"
-    # "~CAP_CHOWN"
-    # "~CAP_FSETID"
-    # "~CAP_SETFCAP"
-    # "~CAP_SETUID"
-    # "~CAP_SETGID"
-    # "~CAP_SETPCAP"
-    # "~CAP_MAC_ADMIN"
-    # "~CAP_MAC_OVERRIDE"
-    "~CAP_SYS_RAWIO"
-    "~CAP_SYS_PTRACE"
-    # "~CAP_SYS_NICE"
-    # "~CAP_SYS_RESOURCE"
-    # "~CAP_NET_ADMIN"
-    # "~CAP_NET_BIND_SERVICE"
-    # "~CAP_NET_BROADCAST"
-    # "~CAP_NET_RAW"
-    # "~CAP_AUDIT_CONTROL"
-    # "~CAP_AUDIT_READ"
-    # "~CAP_AUDIT_WRITE"
-    # "~CAP_SYS_ADMIN"
-    # "~CAP_SYSLOG"
-    # "~CAP_SYS_TIME
+  RestrictAddressFamilies = [
+    "~AF_PACKET"
   ];
 
-  ################
-  # System calls #
-  ################
+  CapabilityBoundingSet = [
+    "CAP_NET_BIND_SERVICE"
+    "CAP_NET_RAW"
+    "CAP_SYS_RESOURCE"
+    "CAP_SETGID"
+    "CAP_SETUID"
+    "CAP_CHOWN"
+    "CAP_DAC_OVERRIDE"
+  ];
 
   SystemCallFilter = [
     "~@clock"
-    # "~@cpu-emulation"
     "~@debug"
     "~@module"
     "~@mount"
     "~@obsolete"
-    # "~@privileged"
-    # "~@raw-io"
     "~@reboot"
     "~@resources"
     "~@swap"
   ];
+
 }
