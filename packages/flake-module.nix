75083f2073637462efb336ea2ddc7433815cdfa7
Henri Rosten <henri.rosten@unikie.com>
2025-01-29 12:07:54 +0200

    Temp: strip Ghaf flake
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -1,7 +1,6 @@
 # Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {
-  self,
   inputs,
   ...
 }:
@@ -10,47 +9,9 @@
     inputs.pkgs-by-name-for-flake-parts.flakeModule
     ./own-pkgs-overlay.nix
   ];
-  perSystem =
-    {
-      pkgs,
-      system,
-      lib,
-      ...
-    }:
-    let
-      inherit (pkgs) callPackage;
-    in
-    {
-      #use the pkgs-by-name-for-flake-parts to get the packages
-      # exposed to downstream projects
-      pkgsDirectory = ./pkgs-by-name;
-
-      #fix these to be the correct packages placement
-      packages = self.lib.platformPkgs system {
-        doc = callPackage ../docs {
-          revision = lib.strings.fileContents ../.version;
-          options =
-            let
-              cfg = lib.nixosSystem {
-                # derived from targets/laptop/laptop-configuration-builder.nix + lenovo-x1-carbon-gen10
-                modules = [
-                  self.nixosModules.reference-profiles
-                  self.nixosModules.disko-debug-partition
-                  self.nixosModules.hardware-lenovo-x1-carbon-gen11
-                  self.nixosModules.profiles-workstation
-                  {
-                    nixpkgs = {
-                      hostPlatform = "x86_64-linux";
-                      overlays = [
-                        inputs.ghafpkgs.overlays.default
-                      ];
-                    };
-                  }
-                ];
-              };
-            in
-            cfg.options;
-        };
-      };
-    };
+  perSystem = {
+    #use the pkgs-by-name-for-flake-parts to get the packages
+    # exposed to downstream projects
+    pkgsDirectory = ./pkgs-by-name;
+  };
 }
