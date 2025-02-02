52cf24e44723b9f19d78c37fae91861b04bda545
Brian McGillion <bmg.avoin@gmail.com>
2025-02-02 20:24:44 +0400

    Bump lanzaboot to the latest version
    Fixes and IFD in the process for the nix flake check.
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- flake.nix
+++ flake.nix
@@ -123,7 +123,7 @@
     # Security
     #
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.1";
+      url = "github:nix-community/lanzaboote/v0.4.2";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
