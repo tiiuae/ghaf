2b407afd586c847debd18d1dfd4a26c5b9f8bb9b
Juan Pablo Ruiz <juanpablo.ruiz@unikie.com>
2025-02-10 13:04:11 +0200

    virt: updated passthrough qemu patches for version 9.2
    Signed-off-by: Juan Pablo Ruiz <juanpablo.ruiz@unikie.com>

--- modules/reference/hardware/jetpack/nvidia-jetson-orin/virtualization/host/bpmp-virt-host/overlays/qemu/default.nix
+++ modules/reference/hardware/jetpack/nvidia-jetson-orin/virtualization/host/bpmp-virt-host/overlays/qemu/default.nix
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 (_final: prev: {
   qemu_kvm = prev.qemu_kvm.overrideAttrs (
+    # Patches from https://github.com/jpruiz84/qemu/tree/bpmp_for_v9.2
     _final: prev: { patches = prev.patches ++ [ 
       ./patches/0001-nvidia-bpmp-guest-driver-initial-commit.patch
       ./patches/0002-NOP_PREDEFINED_DTB_MEMORY.patch
