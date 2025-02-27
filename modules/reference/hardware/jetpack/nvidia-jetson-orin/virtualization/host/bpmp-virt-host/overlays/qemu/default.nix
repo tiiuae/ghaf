d7b1b87f5bdab5676ae6c318e804b499bfc31e1e
Juan Pablo Ruiz <juanpablo.ruiz@unikie.com>
2025-02-27 22:51:40 +0200

    jetson: update BPMP module for kernel 5.15 and 6.6
    - Update BPMP patches to support Linux kernel 5.15 and 6.6 for Jetson Jetpack 6
    - Removed no BPMP related Linux modules
    - Added BPMP allows all domains for debugging
    - Converted device tree patches to device tree overlay
    - Updated qemu patch for version 9.2
    
    Signed-off-by: Juan Pablo Ruiz <juanpablo.ruiz@unikie.com>

--- modules/reference/hardware/jetpack/nvidia-jetson-orin/virtualization/host/bpmp-virt-host/overlays/qemu/default.nix
+++ modules/reference/hardware/jetpack/nvidia-jetson-orin/virtualization/host/bpmp-virt-host/overlays/qemu/default.nix
@@ -2,6 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 (_final: prev: {
   qemu_kvm = prev.qemu_kvm.overrideAttrs (
-    _final: prev: { patches = prev.patches ++ [ ./patches/0001-qemu-v8.1.3_bpmp-virt.patch ]; }
+    # Patches from https://github.com/jpruiz84/qemu/tree/bpmp_for_v9.2
+    _final: prev: { patches = prev.patches ++ [ 
+      ./patches/0001-nvidia-bpmp-guest-driver-initial-commit.patch
+    ];}
   );
 })
