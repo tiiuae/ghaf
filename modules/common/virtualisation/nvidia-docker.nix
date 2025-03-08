571a6daf58fc34a9278bff34cf25366499a5bbde
Brian McGillion <bmg.avoin@gmail.com>
2025-03-08 16:30:25 +0400

    docker: rename nvidia docker specific config
    use the nvidia-docker namespace to contain the nvidia specific
    configurations.
    
    Also seems that nix uses virtualisation and not virtualization (s,z)
    naming convention.
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- modules/common/virtualisation/nvidia-docker.nix
+++ modules/common/virtualisation/nvidia-docker.nix
@@ -2,13 +2,14 @@
 # SPDX-License-Identifier: Apache-2.0
 { lib, config, ... }:
 let
-  cfg = config.ghaf.virtualization.podman.daemon;
+  cfg = config.ghaf.virtualisation.nvidia-docker.daemon;
   inherit (lib) mkEnableOption mkIf;
 in
 {
-  options.ghaf.virtualization.podman.daemon = {
-    enable = mkEnableOption "Podman Daemon";
+  options.ghaf.virtualisation.nvidia-docker.daemon = {
+    enable = mkEnableOption "Nvidia Docker Daemon";
   };
+
   config = mkIf cfg.enable {
     # Just ensure containers are enabled by boot.
     boot.enableContainers = lib.mkForce true;
@@ -16,7 +17,7 @@ in
     # Enable Opengl renamed to hardware.graphics.enable
     hardware.graphics.enable = lib.mkForce true;
 
-    # For CUDA support: Enable if not already enabled.
+    # For CUDA support unfree libraries and CudaSupport should be set
     ghaf.development.cuda.enable = lib.mkForce true;
 
     # Enabling CDI NVIDIA devices in podman or docker (nvidia docker container)
@@ -27,19 +28,36 @@ in
     # but deprecated and changed to hardware.nvidia-container-toolkit.enable
     # We enable below setting if architecture ix x86_64 and if the video driver is nvidia set it true
     hardware.nvidia-container-toolkit.enable = lib.mkIf (
-      config.nixpkgs.localSystem.isx86_64 && (builtins.elem "nvidia" config.services.xserver.videoDrivers)
+      config.nixpkgs.hostPlatform.isx86_64
+      && (builtins.elem "nvidia" config.services.xserver.videoDrivers)
     ) true;
 
-    virtualisation.podman = {
+    # Temporary fix for nvidia service restart remove with new nixpkgs reference.
+    # systemd.services.nvidia-container-toolkit-cdi-generator.WantedBy = [ "multi-user.target" ];
+    systemd.services.nvidia-cdi-generate.after = [
+      "multi-user.target"
+      "greetd.service"
+      "avahi-daemon.service"
+    ];
+
+    # Docker Daemon Settings
+    virtualisation.docker = {
+      # To force Docker package version settings need to import pkgs first
+      # package = pkgs.docker_26;
+
       enable = true;
       # The enableNvidia option is still used in jetpack-nixos while it is obsolete in nixpkgs
-      # but it is still only option for nvidia-orin devices.
-      enableNvidia = config.nixpkgs.localSystem.isAarch64 && config.hardware.nvidia-jetpack.enable;
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = !config.virtualisation.docker.enable;
-      dockerSocket.enable = !config.virtualisation.docker.enable;
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
+      # but it is still only option for nvidia-orin devices. Added extra fix for CDI to
+      # make it run with docker.
+      enableNvidia = config.nixpkgs.hostPlatform.isAarch64 && config.hardware.nvidia-jetpack.enable;
+      daemon.settings.features.cdi = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+        daemon.settings.features.cdi = true;
+        daemon.settings.cdi-spec-dirs = [ "/var/run/cdi/" ];
+      };
+
       # Container file and processor limits
       # daemon.settings = {
       #   default-ulimits = {
@@ -57,12 +75,10 @@ in
       #   };
     };
 
-    # Add user to podman and docker group (due to compatibility mode)
-    # and dialout group for access to serial ports
+    # Add user to docker group and dialout group for access to serial ports
     users.users."ghaf".extraGroups = [
       "docker"
       "dialout"
-      "podman"
     ];
   };
 }
--- modules/common/virtualisation/nvidia-podman.nix
+++ modules/common/virtualisation/nvidia-podman.nix
@@ -2,14 +2,13 @@
 # SPDX-License-Identifier: Apache-2.0
 { lib, config, ... }:
 let
-  cfg = config.ghaf.virtualization.docker.daemon;
+  cfg = config.ghaf.virtualisation.nvidia-podman.daemon;
   inherit (lib) mkEnableOption mkIf;
 in
 {
-  options.ghaf.virtualization.docker.daemon = {
-    enable = mkEnableOption "Docker Daemon";
+  options.ghaf.virtualisation.nvidia-podman.daemon = {
+    enable = mkEnableOption "Nvidia Podman Daemon";
   };
-
   config = mkIf cfg.enable {
     # Just ensure containers are enabled by boot.
     boot.enableContainers = lib.mkForce true;
@@ -17,7 +16,7 @@ in
     # Enable Opengl renamed to hardware.graphics.enable
     hardware.graphics.enable = lib.mkForce true;
 
-    # For CUDA support unfree libraries and CudaSupport should be set
+    # For CUDA support: Enable if not already enabled.
     ghaf.development.cuda.enable = lib.mkForce true;
 
     # Enabling CDI NVIDIA devices in podman or docker (nvidia docker container)
@@ -28,35 +27,20 @@ in
     # but deprecated and changed to hardware.nvidia-container-toolkit.enable
     # We enable below setting if architecture ix x86_64 and if the video driver is nvidia set it true
     hardware.nvidia-container-toolkit.enable = lib.mkIf (
-      config.nixpkgs.localSystem.isx86_64 && (builtins.elem "nvidia" config.services.xserver.videoDrivers)
+      config.nixpkgs.hostPlatform.isx86_64
+      && (builtins.elem "nvidia" config.services.xserver.videoDrivers)
     ) true;
 
-    # Temporary fix for nvidia service restart remove with new nixpkgs reference.
-    # systemd.services.nvidia-container-toolkit-cdi-generator.WantedBy = [ "multi-user.target" ];
-    systemd.services.nvidia-cdi-generate.after = [
-      "multi-user.target"
-      "greetd.service"
-      "avahi-daemon.service"
-    ];
-
-    # Docker Daemon Settings
-    virtualisation.docker = {
-      # To force Docker package version settings need to import pkgs first
-      # package = pkgs.docker_26;
-
+    virtualisation.podman = {
       enable = true;
       # The enableNvidia option is still used in jetpack-nixos while it is obsolete in nixpkgs
-      # but it is still only option for nvidia-orin devices. Added extra fix for CDI to
-      # make it run with docker.
-      enableNvidia = config.nixpkgs.localSystem.isAarch64 && config.hardware.nvidia-jetpack.enable;
-      daemon.settings.features.cdi = true;
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
-        daemon.settings.features.cdi = true;
-        daemon.settings.cdi-spec-dirs = [ "/var/run/cdi/" ];
-      };
-
+      # but it is still only option for nvidia-orin devices.
+      enableNvidia = config.nixpkgs.hostPlatform.isAarch64 && config.hardware.nvidia-jetpack.enable;
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = !config.virtualisation.docker.enable;
+      dockerSocket.enable = !config.virtualisation.docker.enable;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
       # Container file and processor limits
       # daemon.settings = {
       #   default-ulimits = {
@@ -74,10 +58,12 @@ in
       #   };
     };
 
-    # Add user to docker group and dialout group for access to serial ports
+    # Add user to podman and docker group (due to compatibility mode)
+    # and dialout group for access to serial ports
     users.users."ghaf".extraGroups = [
       "docker"
       "dialout"
+      "podman"
     ];
   };
 }
