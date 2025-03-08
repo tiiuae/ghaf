2620ef3462679cb7f9ea83dd524d8c9dbdd9f940
Brian McGillion <bmg.avoin@gmail.com>
2025-03-08 15:02:11 +0400

    nvidia: clean up the script generation
    After the latest changes to the jetpack mainline the mkFlashScript was a
    one liner that returned the flash script. so have inlined the script retrieval.
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- lib/mk-flash-script/default.nix
+++
@@ -1,8 +0,0 @@
-# Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
-# SPDX-License-Identifier: Apache-2.0
-#
-# Function to generate NVIDIA Jetson Orin flash script
-{
-  hostConfiguration,
-}:
-hostConfiguration.pkgs.nvidia-jetpack.flashScript
