14c007ce51ba4b28c3395f05729c7ed560593622
Manuel Bluhm <manuel@ssrc.tii.ae>
2025-03-19 16:08:41 +0400

    refactor(flake): common and development
    Create common and development flake module files.
    
    Signed-off-by: Manuel Bluhm <manuel@ssrc.tii.ae>

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -3,7 +3,7 @@
 #
 # Modules to be exported from Flake
 #
-{ inputs, ... }:
+{ ... }:
 {
   imports = [
     ./partitioning/flake-module.nix
@@ -12,19 +12,13 @@
     ./microvm/flake-module.nix
     ./reference/hardware/flake-module.nix
     ./profiles/flake-module.nix
+    ./common/flake-module.nix
+    ./development/flake-module.nix
   ];
 
   flake.nixosModules = {
-    common.imports = [
-      ./common
-      {
-        ghaf.development.nix-setup.nixpkgs = inputs.nixpkgs;
-      }
-    ];
-
     #TODO: Add the rest of the modules in their own directories with flake-module.nix
     desktop.imports = [ ./desktop ];
-    development.imports = [ ./development ];
     reference-appvms.imports = [ ./reference/appvms ];
     reference-host-demo-apps.imports = [ ./reference/host-demo-apps ];
     reference-personalize.imports = [ ./reference/personalize ];
