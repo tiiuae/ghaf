0b9e859a2b6bb96073e66d21ef212923372669c9
Manuel Bluhm <manuel@ssrc.tii.ae>
2025-03-19 16:08:41 +0400

    refactor(flake): common and development
    Create common and development flake module files.
    
    Signed-off-by: Manuel Bluhm <manuel@ssrc.tii.ae>

---
+++ modules/common/flake-module.nix
@@ -0,0 +1,25 @@
+# Copyright 2024 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# Common ghaf modules
+#
+{
+  flake.nixosModules = {
+    common.imports = [
+      ./common.nix
+      ./firewall
+      ./profiles
+      ./security
+      ./users
+      ./version
+      ./virtualisation
+      ./systemd
+      ./services
+      ./networking
+      #TODO: this should be moved to where it is needed and included on demand
+      # if it is a common then the file should be moved to common
+      ../hardware/definition.nix
+      ./logging
+    ];
+  };
+}
