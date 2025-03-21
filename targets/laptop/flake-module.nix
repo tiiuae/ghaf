13a1f4eba49d14556cc945a6bc4b8fdf134dcfef
Vunny Sodhi <vunny.sodhi@unikie.com>
2025-03-21 13:07:52 +0200

    dell-latitude-7330-72: Add new build targets
    This is needed as net-vm will fail to start as we cannot
    have two same network devices which may enumerate on
    different PCI bus on dell latitude 7330.
    This is can be workaround for time
    being until we find proper way to identify on which
    PCI bus network controller is detected.
    So adding debug and release build variants for
    "dell-latitude-7330-72"
    
    Signed-off-by: Vunny Sodhi <vunny.sodhi@unikie.com>

--- targets/laptop/flake-module.nix
+++ targets/laptop/flake-module.nix
@@ -95,6 +95,16 @@ let
         };
       }
     ]))
+    # TODO: Remove once we can have better way to detect network PCI device
+    (laptop-configuration "dell-latitude-7330-72" "debug" (withCommonModules [
+      self.nixosModules.hardware-dell-latitude-7330-72
+      {
+        ghaf = {
+          reference.profiles.mvp-user-trial.enable = true;
+          partitioning.disko.enable = true;
+        };
+      }
+    ]))
     (laptop-configuration "alienware-m18-R2" "debug" (withCommonModules [
       self.nixosModules.hardware-alienware-m18-r2
       {
@@ -155,6 +165,16 @@ let
     ]))
     (laptop-configuration "dell-latitude-7330" "release" (withCommonModules [
       self.nixosModules.hardware-dell-latitude-7330
+      {
+        ghaf = {
+          reference.profiles.mvp-user-trial.enable = true;
+          partitioning.disko.enable = true;
+        };
+      }
+    ]))
+    # TODO: Remove once we can have better way to detect network PCI device
+    (laptop-configuration "dell-latitude-7330-72" "release" (withCommonModules [
+      self.nixosModules.hardware-dell-latitude-7330-72
       {
         ghaf = {
           reference.profiles.mvp-user-trial.enable = true;
