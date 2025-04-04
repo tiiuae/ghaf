401b48883aaf6595cac80046b7684ce056038256
Julius Koskela <julius.koskela@unikie.com>
2025-04-04 11:43:12 +0300

    Correctly pass the gpu-vm configuration into the Nvidia Jetson target
    Signed-off-by: Julius Koskela <julius.koskela@unikie.com>

--- modules/microvm/modules.nix
+++ modules/microvm/modules.nix
@@ -244,6 +244,12 @@ in
       idsvm.extraModules = optionals cfg.idsvm.enable [
         commonModule
       ];
+    } // {
+      # Common modules
+      gpuvm.extraModules = optionals cfg.gpuvm.enable [
+        managedUserAccounts
+        commonModule
+      ];
     };
   };
 }
