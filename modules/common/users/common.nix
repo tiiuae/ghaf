17015a3e1b95a5d50694ceadf5313ad70a235bec
goatser4 <goatser4@proton.me>
2025-05-05 01:22:24 +0000

    Add proper message-of-the-day
    Add proper message-of-the-day which resembles values of the ghaf-project
    
    Signed-off-by: goatser4 <goatser4@proton.me>

--- modules/common/users/common.nix
+++ modules/common/users/common.nix
@@ -13,6 +13,34 @@ in
   # Common ghaf user settings
   config = {
 
+    # Proper message-of-the-day which resembled Ghaf-project's values
+    users.motd = ''
+* g o a t s e x * g o a t s e x * g o a t s e x *
+g                                               g  
+o /     \             \            /    \       o
+a|       |             \          |      |      a
+t|       `.             |         |       :     t
+s`        |             |        \|       |     s
+e \       | /       /  \\\   --__ \\       :    e
+x  \      \/   _--~~          ~--__| \     |    x  
+*   \      \_-~                    ~-_\    |    *
+g    \_     \        _.--------.______\|   |    g
+o      \     \______// _ ___ _ (_(__>  \   |    o
+a       \   .  C ___)  ______ (_(____>  |  /    a
+t       /\ |   C ____)/      \ (_____>  |_/     t
+s      / /\|   C_____)       |  (___>   /  \    s
+e     |   (   _C_____)\______/  // _/ /     \   e
+x     |    \  |__   \\_________// (__/       |  x
+*    | \    \____)   `----   --'             |  *
+g    |  \_          ___\       /_          _/ | g
+o   |              /    |     |  \            | o
+a   |             |    /       \  \           | a
+t   |          / /    |         |  \           |t
+s   |         / /      \__/\___/    |          |s
+e  |           /        |    |       |         |e
+x  |          |         |    |       |         |x
+* g o a t s e x * g o a t s e x * g o a t s e x *
+    '';
     # Disable mutable users
     users.mutableUsers = mkDefault false;
 
