8f860cff8263c1bb275eaa8d09f8cef95376869d
Hugo Ros <Hugo.Ros@tii.ae>
2025-06-06 15:10:46 +0300

    checks: add/fix copyright info
    Signed-off-by: Hugo Ros <Hugo.Ros@tii.ae>

--- modules/reference/profiles/mvp-user-trial-hardening.nix
+++ modules/reference/profiles/mvp-user-trial-hardening.nix
@@ -1,4 +1,4 @@
-# Copyright 2022-2024 TII (SSRC) and the Ghaf contributors
+# Copyright 2022-2025 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 { config, lib, ... }:
 
