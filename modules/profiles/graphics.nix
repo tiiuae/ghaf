f25d5b4bb88c65c765ef8ffbb4bb34f8abd30438
Kajus Naujokaitis <kajus.naujokaitis@unikie.com>
2025-06-13 16:17:11 +0300

    wip: cosmic for orins
    Signed-off-by: Kajus Naujokaitis <kajus.naujokaitis@unikie.com>

--- modules/profiles/graphics.nix
+++ modules/profiles/graphics.nix
@@ -47,6 +47,23 @@ in
         Choose one of: ${lib.concatStringsSep "," renderers}
       '';
     };
+    renderDevice = mkOption {
+      type = types.nullOr types.path;
+      default = null;
+      defaultText = "null";
+      example = "/dev/dri/renderD129";
+      description = ''
+        Path to the render device to be used by the COSMIC compositor.
+
+        If set, this will be assigned to the `COSMIC_RENDER_DEVICE` environment variable,
+        directing COSMIC to use the specified device (e.g., /dev/dri/renderD129).
+
+        This option can be useful in systems with multiple GPUs to explicitly select
+        which device the compositor should use.
+
+        If unset, COSMIC will attempt to automatically detect a suitable render device.
+      '';
+    };
     idleManagement = {
       enable = mkOption {
         type = types.bool;
@@ -66,6 +83,30 @@ in
         lid close, or button press.
       '';
     };
+
+    autoLogin = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to enable automatic login.
+
+          When enabled, the system will automatically log in the specified user
+          without requiring credentials at the login screen.
+        '';
+      };
+
+      user = mkOption {
+        type = lib.types.nullOr lib.types.str;
+        default = null;
+        example = "ghaf";
+        description = ''
+          Username to automatically log in as when auto-login is enabled.
+
+          This should correspond to a valid user defined in the system configuration.
+        '';
+      };
+    };
   };
 
   config = mkIf cfg.enable {
@@ -79,18 +120,26 @@ in
       ];
     };
     environment = {
-      sessionVariables = {
-        WLR_RENDERER = cfg.renderer;
-        XDG_SESSION_TYPE = "wayland";
-        WLR_NO_HARDWARE_CURSORS = if (cfg.renderer == "pixman") then 1 else 0;
-        XKB_DEFAULT_LAYOUT = "us,ara,fi";
-        XKB_DEFAULT_OPTIONS = "grp:alt_shift_toggle";
-        # Set by default in labwc, but possibly not in other compositors
-        XDG_CURRENT_DESKTOP = "wlroots";
-        _JAVA_AWT_WM_NONREPARENTING = 1;
-      };
+      sessionVariables =
+        {
+          XKB_DEFAULT_LAYOUT = "us,ara,fi";
+          XKB_DEFAULT_OPTIONS = "grp:alt_shift_toggle";
+        }
+        // lib.optionalAttrs (cfg.compositor == "labwc") {
+          WLR_NO_HARDWARE_CURSORS = if (cfg.renderer == "pixman") then 1 else 0;
+          WLR_RENDERER = cfg.renderer;
+          _JAVA_AWT_WM_NONREPARENTING = 1;
+          XDG_SESSION_TYPE = "wayland";
+        };
 
       systemPackages = lib.optionals config.ghaf.profiles.debug.enable [ pkgs.ghaf-open ];
+    };
+
+    services.displayManager = {
+      autoLogin = mkIf cfg.autoLogin.enable {
+        enable = true;
+        inherit (cfg.autoLogin) user;
+      };
     };
 
     ghaf.graphics = {
@@ -98,5 +147,12 @@ in
       labwc.autolock.enable = cfg.idleManagement.enable;
       cosmic.enable = cfg.compositor == "cosmic";
     };
+
+    assertions = [
+      {
+        assertion = !cfg.autoLogin.enable || cfg.autoLogin.user != "";
+        message = "autoLogin.user must be set when autoLogin.enable is true.";
+      }
+    ];
   };
 }
