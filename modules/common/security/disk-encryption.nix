dd221b7617f37b14405ec508429e5832329d3585
Hugo Ros <Hugo.Ros@tii.ae>
2025-06-16 16:02:01 +0300

    disk encryption: multiple changes
    - move decryption of the partitions to initrd
    - enroll swap partition
    - add FIDO2 option during provisioning
    
    Signed-off-by: Hugo Ros <Hugo.Ros@tii.ae>

--- modules/common/security/disk-encryption.nix
+++ modules/common/security/disk-encryption.nix
@@ -10,66 +10,104 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.ghaf.storage.encryption;
+
+  persistConf = rec {
+    partConf = config.image.repart.partitions."50-persist".repartConfig;
+    device = "/dev/disk/by-partuuid/${partConf.UUID}";
+  };
+  swapConf = rec {
+    partConf = config.image.repart.partitions."01-swap".repartConfig;
+    device = "/dev/disk/by-partuuid/${partConf.UUID}";
+  };
+
 in
 {
   options.ghaf.storage.encryption = {
     enable = mkEnableOption "Encryption of the data partition";
   };
 
-  config =
-    let
-      partConf = config.image.repart.partitions."50-persist".repartConfig;
-      device = "/dev/disk/by-partuuid/${partConf.UUID}";
-    in
-    mkIf cfg.enable {
+  config = mkIf cfg.enable {
+    security.tpm2.enable = true;
 
-      security.tpm2.enable = true;
-
-      environment.systemPackages = [
-        pkgs.cryptsetup
-        pkgs.tpm2-tools
+    environment.systemPackages =
+      with pkgs;
+      lib.mkIf config.ghaf.profiles.debug.enable [
+        cryptsetup
+        tpm2-tools
+        parted
+        util-linux
+        gptfdisk
       ];
 
-      environment.etc.crypttab.text = ''
-        persist ${device} - tpm2-device=auto,luks,try-empty-password=true
-      '';
+    boot.initrd.luks.devices = {
+      persist = {
+        inherit (persistConf) device;
+        tryEmptyPassphrase = true;
+        crypttabExtraOpts = [
+          "tpm2-device=auto"
+          "fido2-device=auto"
+        ];
+      };
+      swap = {
+        inherit (swapConf) device;
+        tryEmptyPassphrase = true;
+        crypttabExtraOpts = [ "tpm2-device=auto" ];
+      };
+    };
 
-      boot.plymouth.enable = lib.mkForce false;
-
-      # POSSIBLE IMPROVEMENTS
-      # - Disable service after first boot
-      # - Integrate with plymouth (https://systemd.io/PASSWORD_AGENTS ?)
-      systemd.services.luks-enroll-tpm = {
-        wantedBy = [ "systemd-cryptsetup@persist.service" ];
+    systemd.services.luks-enroll-tpm =
+      let
+        unitScript = pkgs.writeShellApplication {
+          name = "luks-enroll-tpm-unit-script";
+          runtimeInputs = with pkgs; [
+            gnugrep
+            cryptsetup
+          ];
+          text = ''
+            if cryptsetup luksDump ${persistConf.device} | grep -E '(systemd-tpm2|systemd-fido2)'; then
+              exit 0
+            fi
+            echo '========== Enrolling TPM for persist partition =========='
+            read -r -p 'Choose the type of device to enroll: 1 = TPM2, 2 = FIDO2 (default: TPM2): ' CHOICE
+            if [[ "$CHOICE" -eq 2 ]]; then
+              systemd-cryptenroll --fido2-device=auto --fido2-with-user-presence=false ${persistConf.device}
+            else
+              systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=7 --tpm2-with-pin=true ${persistConf.device}
+            fi
+            echo '-- adding recovery key --'
+            systemd-cryptenroll --recovery ${persistConf.device}
+            echo '========== Setting up encrypted swap =========='
+            systemd-cryptenroll --tpm2-device=auto ${swapConf.device}
+            echo '========== Removing default passphrase =========='
+            systemd-cryptenroll --wipe-slot=password ${persistConf.device}
+            systemd-cryptenroll --wipe-slot=password ${swapConf.device}
+            read -r -s -p 'All done. Press Enter to continue'
+          '';
+        };
+      in
+      {
+        description = "Enroll encrypted partitions to the TPM and/or to a Yubikey";
+        unitConfig.DefaultDependencies = "no"; # run before VMs are launched
+        wantedBy = [
+          "sysinit.target"
+        ];
         before = [
-          "systemd-cryptsetup@persist.service"
+          "sysinit.target"
           "shutdown.target"
         ];
         after = [
-          "systemd-tpm2-setup-early.service"
-          "${utils.escapeSystemdPath device}.device"
+          "systemd-tpm2-setup.service"
           "nix-store.mount"
         ];
         wants = [
-          "tpm2.target"
-          "${utils.escapeSystemdPath device}.device"
+          "systemd-tpm2-setup.service"
           "nix-store.mount"
+          "${utils.escapeSystemdPath persistConf.device}.device"
+          "${utils.escapeSystemdPath swapConf.device}.device"
         ];
-        script = ''
-          if ${lib.getExe pkgs.cryptsetup} --dump-json-metadata luksDump ${device} | grep systemd-tpm2; then
-            exit 0
-          fi
-          echo '========== Enrolling TPM for persist partition =========='
-          systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=7 --tpm2-with-pin=true ${device}
-          echo '-- adding recovery key --'
-          systemd-cryptenroll --recovery ${device}
-          echo '-- removing initial empty passphrase --'
-          systemd-cryptenroll --wipe-slot=empty ${device}
-          read -p 'All done. Press Enter to continue'
-        '';
-        unitConfig.DefaultDependencies = "no";
         serviceConfig = {
           Type = "oneshot";
+          ExecStart = "${lib.getExe unitScript}";
           RemainAfterExit = true;
           StandardOutput = "tty";
           StandardInput = "tty";
@@ -79,5 +117,5 @@ in
           TTYVTDisallocate = "yes";
         };
       };
-    };
+  };
 }
