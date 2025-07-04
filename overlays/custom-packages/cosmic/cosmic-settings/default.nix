9cbf979167a33d2656b47b827f5e51c27fc5034e
Kajus Naujokaitis <kajus.naujokaitis@unikie.com>
2025-07-04 16:45:46 +0300

    refactor: refactor cosmic login config, simplify overlays, adjust swayidle
    - simplified cosmic login config by using `login-manager` module
    - use proper cargo attrs in cosmic package overlays
    - adjust swayidle behavior in cosmic
    - other cosmic config cleanup
    
    Signed-off-by: Kajus Naujokaitis <kajus.naujokaitis@unikie.com>

--- overlays/custom-packages/cosmic/cosmic-settings/default.nix
+++ overlays/custom-packages/cosmic/cosmic-settings/default.nix
@@ -1,13 +1,31 @@
 # Copyright 2022-2025 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 # Disable certain settings pages in cosmic-settings
-# Disabled pages: page-power, page-sound, page-users, page-legacy-applications
 # Ref: https://github.com/pop-os/cosmic-settings/blob/master/cosmic-settings/Cargo.toml
 { prev }:
-prev.cosmic-settings.overrideAttrs (oldAttrs: {
-  cargoBuildFlags = (oldAttrs.cargoBuildFlags or [ ]) ++ [
-    "--no-default-features"
-    "--features"
-    "a11y,dbus-config,single-instance,wgpu,page-accessibility,page-about,page-bluetooth,page-date,page-default-apps,page-display,page-input,page-networking,page-region,page-window-management,page-workspaces,xdg-portal,wayland"
+(prev.cosmic-settings.overrideAttrs (_oldAttrs: rec {
+  cargoBuildNoDefaultFeatures = true;
+  cargoBuildFeatures = [
+    "a11y"
+    "dbus-config"
+    "page-about"
+    "page-accessibility"
+    "page-date"
+    "page-default-apps"
+    "page-display"
+    "page-input"
+    "page-region"
+    # "page-power"
+    # "page-sound"
+    # "page-users"
+    "page-legacy-applications"
+    # "page-bluetooth"
+    # "page-networking"
+    "page-window-management"
+    "page-workspaces"
+    "single-instance"
+    "wayland"
+    "wgpu"
+    "xdg-portal"
   ];
-})
+}))
