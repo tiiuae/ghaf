a60115a0e9fe98bed6e83e020f9c7fe941f77e75
Kajus Naujokaitis <kajus.naujokaitis@unikie.com>
2025-07-07 15:01:24 +0300

    refactor: refactor cosmic login config, simplify overlays, adjust swayidle
    - simplified cosmic login config by using `login-manager` module
    - use proper cargo attrs in cosmic package overlays
    - adjust swayidle behavior in cosmic
    - other cosmic config cleanup
    
    Signed-off-by: Kajus Naujokaitis <kajus.naujokaitis@unikie.com>

--- modules/desktop/graphics/login-manager.nix
+++ modules/desktop/graphics/login-manager.nix
@@ -9,15 +9,26 @@
 let
   cfg = config.ghaf.graphics.login-manager;
 
+  inherit (lib)
+    mkIf
+    mkEnableOption
+    ;
+
+  useCosmic = config.ghaf.profiles.graphics.compositor == "cosmic";
+
+  useLabwc = config.ghaf.profiles.graphics.compositor == "labwc";
+
+  greeterUser = if useCosmic then "cosmic-greeter" else "greeter";
+
   gtkgreetStyle = pkgs.callPackage ./styles/login-style.nix { };
 in
 {
   options.ghaf.graphics.login-manager = {
-    enable = lib.mkEnableOption "login manager using greetd";
+    enable = mkEnableOption "Ghaf login manager config using greetd";
   };
 
-  config = lib.mkIf cfg.enable {
-    services = {
+  config = mkIf cfg.enable {
+    services = mkIf useLabwc {
       greetd = {
         enable = true;
         settings = {
@@ -28,18 +39,15 @@ in
                 runtimeInputs = [
                   pkgs.greetd.gtkgreet
                   pkgs.wayland-logout
-                  pkgs.brightnessctl
                 ];
                 text = ''
-                  # By default set system brightness to 100% which can be configured later
-                  brightnessctl set 100%
                   gtkgreet -l -s ${gtkgreetStyle}
                   wayland-logout
                 '';
               };
             in
             {
-              command = "${pkgs.labwc}/bin/labwc -C /etc/labwc -s ${greeter-autostart}/bin/greeter-autostart >/tmp/greeter.labwc.log 2>&1";
+              command = lib.mkForce "${lib.getExe pkgs.labwc} -C /etc/labwc -s ${lib.getExe greeter-autostart} >/tmp/greeter.labwc.log 2>&1";
             };
         };
       };
@@ -48,17 +56,60 @@ in
         enable = true;
         group = "video";
       };
+    };
 
-      #Allow video group to change brightness
-      udev.extraRules = ''
-        ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video $sys$devpath/brightness", RUN+="${pkgs.coreutils}/bin/chmod a+w $sys$devpath/brightness"
-      '';
+    # Ensure there is always a backlight brightness value to restore from on boot
+    # ghaf-powercontrol will store the value here via systemd-backlight.service
+    ghaf = lib.optionalAttrs (lib.hasAttr "storagevm" config.ghaf) {
+      storagevm.directories = [
+        {
+          directory = "/var/lib/systemd/backlight";
+          user = "root";
+          group = "root";
+          mode = "0700";
+        }
+      ];
     };
 
     systemd.services.greetd.serviceConfig = {
       RestartSec = "5";
     };
 
-    users.users.greeter.extraGroups = [ "video" ];
+    users.users.${greeterUser}.extraGroups = [ "video" ];
+
+    # Needed for the greeter to query systemd-homed users correctly
+    systemd.services.cosmic-greeter-daemon.environment.LD_LIBRARY_PATH = mkIf useCosmic "${
+      pkgs.lib.makeLibraryPath
+      [
+        pkgs.systemd
+      ]
+    }";
+
+    security.pam.services = {
+      cosmic-greeter.rules.auth = mkIf useCosmic {
+        systemd_home.order = 11399; # Re-order to allow either password _or_ fingerprint on lockscreen
+        fprintd.args = [ "maxtries=3" ];
+      };
+      gtklock.rules.auth = mkIf useLabwc {
+        systemd_home.order = 11399; # Re-order to allow either password _or_ fingerprint on lockscreen
+        fprintd.args = [ "maxtries=3" ];
+      };
+      greetd = {
+        fprintAuth = false; # User needs to enter password to decrypt home on login
+        rules = {
+          account.group_video = {
+            enable = true;
+            control = "requisite";
+            modulePath = "${pkgs.linux-pam}/lib/security/pam_succeed_if.so";
+            order = 10000;
+            args = [
+              "user"
+              "ingroup"
+              "video"
+            ];
+          };
+        };
+      };
+    };
   };
 }
