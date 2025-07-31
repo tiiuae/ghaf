b96056a9bfe90c2e0e94cf35ff8dd8755f95383c
Vunny Sodhi <vunny.sodhi@unikie.com>
2025-07-31 16:56:11 +0300

    feat(xpadneo): Add module to enable wireless Xbox controller support
    This patch includes
    - Adds ghaf.services.xpadneo.enable option
    - Loads hid-xpadneo and uhid kernel modules
    - Adds udev rule for uhid under Bluetooth user
    - Requires ghaf Bluetooth service to be enabled
    
    Signed-off-by: Vunny Sodhi <vunny.sodhi@unikie.com>

---
+++ modules/common/services/xpadneo.nix
@@ -0,0 +1,47 @@
+# Copyright 2025 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.ghaf.services.xpadneo;
+  inherit (lib) mkIf mkEnableOption;
+  bluetoothUser = config.ghaf.services.bluetooth.user;
+in
+{
+  options.ghaf.services.xpadneo = {
+    enable = mkEnableOption "The support for wireless Xbox Controllers";
+  };
+
+  config = mkIf cfg.enable {
+
+    assertions = [
+      {
+        assertion = config.ghaf.services.bluetooth.enable;
+        message = "Please enable ghaf bluetooth service to use xpadneo module";
+      }
+    ];
+
+    # Enable the xpadneo driver for Xbox wireless controllers
+    hardware.xpadneo = {
+      enable = true;
+    };
+
+    hardware.bluetooth = {
+      settings.General = {
+        Privacy = "device";
+      };
+    };
+
+    boot.initrd.kernelModules = [
+      "uhid"
+      "hid-xpadneo"
+    ];
+
+    services.udev.extraRules = ''
+      KERNEL=="uhid", SUBSYSTEM=="misc", GROUP="${bluetoothUser}"
+    '';
+  };
+}
