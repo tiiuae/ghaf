34da5ffce5cc591142879e529ea259193e73d488
Brian McGillion <bmg.avoin@gmail.com>
2025-08-25 17:08:24 +0400

    icon: fix the icon path for the orin boot logo
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- modules/reference/hardware/jetpack/nx/orin-nx.nix
+++ modules/reference/hardware/jetpack/nx/orin-nx.nix
@@ -3,6 +3,7 @@
 #
 # Reference hardware modules
 #
+{ pkgs, ... }:
 {
   imports = [ ../../../../common/services/hwinfo ];
 
@@ -35,7 +36,7 @@
       carrierBoard = "xavierNXdevkit";
       modesetting.enable = true;
       firmware.uefi = {
-        #       logo = ../../../../../../docs/src/img/1600px-Ghaf_logo.svg;
+        logo = "${pkgs.ghaf-artwork}/1600px-Ghaf_logo.svg";
         edk2NvidiaPatches = [
           # This effectively disables EFI FB Simple Framebuffer, which does
           # not work properly but causes kernel panic during the boot if the
