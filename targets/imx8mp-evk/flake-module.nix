0c4d55e2769141cc50b0b94a2f0c5071016a1651
Brian McGillion <bmg.avoin@gmail.com>
2025-08-28 08:29:31 +0400

    qt5-webkit: marked as insecure
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- targets/imx8mp-evk/flake-module.nix
+++ targets/imx8mp-evk/flake-module.nix
@@ -58,6 +58,7 @@ let
                 #vulnerability but it is still used by many people
                 permittedInsecurePackages = [
                   "jitsi-meet-1.0.8043"
+                  "qtwebengine-5.15.19"
                 ];
               };
 
