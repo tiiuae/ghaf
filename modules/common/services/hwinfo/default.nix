59e0fb1984a6eeaf2102dba7a7d75b22e339bf35
Julius Koskela <julius.koskela@unikie.com>
2025-09-05 14:20:23 +0300

    Add hardware information service for host-to-guest data passing
    Introduces a service for passing hardware information from host systems
    to guest VMs using QEMU's fw_cfg interface. Detects NVMe serial numbers
    and MAC addresses on the host and makes them available to guest VMs.
    
    The implementation is cross-platform and integrates with Ghaf's microvm
    infrastructure.
    
    Signed-off-by: Julius Koskela <julius.koskela@unikie.com>
    Signed-off-by: juliuskoskela <julius@koskela.email>

---
+++ modules/common/services/hwinfo/default.nix
@@ -0,0 +1,11 @@
+# Copyright 2025 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# Hardware information modules
+#
+{
+  imports = [
+    ./host.nix
+    ./guest.nix
+  ];
+}
