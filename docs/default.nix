c3ae3552920479f5f83a70a27a9cc265e4c1a890
Brian McGillion <bmg.avoin@gmail.com>
2025-09-23 08:10:49 +0400

    docs: bump NPM depends
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- docs/default.nix
+++ docs/default.nix
@@ -71,6 +71,6 @@ buildNpmPackage (_finalAttrs: {
     runHook postInstall
   '';
 
-  npmDepsHash = "sha256-p0khQ9/y2GXq2aaRFLxzNgXf7KSLE/Fv6YiS15whA7Q=";
+  npmDepsHash = "sha256-R1EoGYg8wKE5bpQYjA1yGsNLt1//AQTbU83cxLhi4PU=";
 
 })
