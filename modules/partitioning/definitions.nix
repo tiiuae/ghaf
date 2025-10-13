490823d4c18ddf28f28bf96b0b4cb0990ac48328
Alexander Nikolaev <anikolaev@tii.ae>
2025-10-13 12:29:12 +0300

    take 2
    Signed-off-by: Alexander Nikolaev <anikolaev@tii.ae>

--- modules/partitioning/definitions.nix
+++ modules/partitioning/definitions.nix
@@ -1,60 +1,72 @@
-# Copyright 2025 TII (SSRC) and the Ghaf contributors
+# SPDX-FileCopyrightText: 2022-2026 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 { config, lib, ...}:
 let
   inherit (lib) mkOption types;
-  fsDef = defaults: types.submodule {
+  partDef = types.submodule {
     options = {
       size = mkOption {
         types = types.str; # FIXME: validation
-        default = defaults.size;
         description = "Partition minimal size";
       };
       mountPoint = mkOption {
         types = types.str; # FIXME: validation
-        default = defaults.mountPoint;
         description = "Partition's filesystem mount point";
       };
       fileSystem = mkOption {
         types = types.enum ["ext4" "btrfs" "xfs" "erofs" "swap" "vfat"];
-        default = defaults.fileSystem;
         description = "File system to put on partition";
       };
     };
   };
+  assertions =
+      let
+        cfg = config.ghaf.partitions;
+      in
+      [
+        {
+          assertion = builtins.hasAttr cfg.definition "esp";
+          message = "ESP partition must be defined";
+        }
+      ];
 in
 {
   options = {
-    ghaf.partitions.definition = {
-      esp = mkOption {
-        type = fsDef {
-          size = "512M";
-          mountPoint = "/boot";
-          fileSystem = "vfat";
-        };
-      };
-      # FIXME: Need build time assertion, that resulting image fits
-      root = mkOption {
-        type = fsDef {
-          size = "64G";
-          mountPoint = "/nix/store";
-          fileSystem = "ext4"; # FIXME: ext4/erofs autoselection? "auto" type?
-        };
-      };
-      persist = mkOption {
-        type = fsDef {
-          size = "1G";
-          mountPoint = "/persist";
-          fileSystem = "btrfs";
-        };
-      };
+    ghaf.partitions = {
       enableABScheme = mkOption {
         type = types.bool;
         default = true;
       };
-      extraPartitions = mkOption {
-        type = types.listOf types.freeform;
-        description = "FIXME: point for extensions, in case if we need extra partitions added";
+      rootFilesystemType = mkOption {
+        internal = true;
+        default = "ext4";
+        description = ''
+          Type of rootfs (A slot)
+          Defaulted to "ext4" for disko, but should be overriden for erofs images
+        '';
+      };
+      definition = mkOption {
+        type = types.attrsOf partDef;
+      };
+    };
+
+    config.ghaf.partitions.definition = {
+      inherit assertions;
+      esp = mkOption {
+        size = "512M";
+        mountPoint = "/boot";
+        fileSystem = "vfat";
+      };
+      # FIXME: Need build time assertion, that resulting image fits
+      root = mkOption {
+        size = "64G";
+        mountPoint = "/nix/store";
+        fileSystem = config.ghaf.partitions.rootFilesystemType; 
+      };
+      persist = mkOption {
+        size = "1G";
+        mountPoint = "/persist";
+        fileSystem = "btrfs";
       };
     };
   };
