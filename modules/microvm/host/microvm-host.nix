f4b06bd7af6a1004de7a5a34ce507ce2ed306477
juliuskoskela <julius.koskela@unikie.com>
2025-10-13 22:52:24 +0300

    microvm: only enable logging.client when admin-vm exists
    Conditionally enable logging.client based on whether admin-vm is
    configured in ghaf.networking.hosts. This prevents assertion failures
    for targets like generic-x86_64 that use microvm-host but don't have
    an admin-vm for log aggregation.
    
    Signed-off-by: juliuskoskela <julius.koskela@unikie.com>

--- modules/microvm/host/microvm-host.nix
+++ modules/microvm/host/microvm-host.nix
@@ -100,7 +100,7 @@ in
           create-fake-battery.enable = true;
         };
         development.nix-setup.automatic-gc.enable = config.ghaf.development.nix-setup.enable;
-        logging.client = true;
+        logging.client = config.ghaf.networking.hosts ? admin-vm;
         common.extraNetworking.hosts.ghaf-host = cfg.extraNetworking;
       };
 
