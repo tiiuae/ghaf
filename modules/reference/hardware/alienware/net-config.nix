4a65957fa1dbc1e2d0cf61e0618025f8dbee3e11
Brian McGillion <bmg.avoin@gmail.com>
2025-10-21 18:21:56 +0400

    rtl8126: fix the kernel version to match kernel
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- modules/reference/hardware/alienware/net-config.nix
+++ modules/reference/hardware/alienware/net-config.nix
@@ -1,11 +1,14 @@
 # SPDX-FileCopyrightText: 2022-2026 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {
+  config,
   pkgs,
   ...
 }:
 {
   config = {
-    boot.extraModulePackages = [ pkgs.rtl8126 ];
+    boot.extraModulePackages = [
+      (pkgs.rtl8126.override { linuxPackages = config.boot.kernelPackages; })
+    ];
   };
 }
