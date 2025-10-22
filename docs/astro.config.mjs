fbb2f4093e4a775ee8b2d0ed21d463248efb3428
Enes Öztürk <enes.ozturk@unikie.com>
2025-10-22 12:48:40 +0300

    doc: wireguard-gui
    Signed-off-by: Enes Öztürk <enes.ozturk@unikie.com>

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -89,6 +89,7 @@ export default defineConfig({
                         "ghaf/dev/ref/idsvm-development",
                         "ghaf/dev/ref/systemd-service-config",
                         "ghaf/dev/ref/kill_switch",
+                        "ghaf/dev/ref/wireguard-gui",
                       ],
                     },
                     {
