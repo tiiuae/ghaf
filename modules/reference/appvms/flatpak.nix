b77a5900812d147c2b9d75a2e5a6d40900d21953
Vunny Sodhi <vunny.sodhi@unikie.com>
2025-10-23 13:31:41 +0300

    Changes to enable cosmic-store
    Signed-off-by: Vunny Sodhi <vunny.sodhi@unikie.com>

--- modules/reference/appvms/flatpak.nix
+++ modules/reference/appvms/flatpak.nix
@@ -1,4 +1,4 @@
-# Copyright 2025 TII (SSRC) and the Ghaf contributors
+# SPDX-FileCopyrightText: 2022-2026 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 #
 {
@@ -12,27 +12,46 @@
     cores = 4;
     bootPriority = "low";
     borderColor = "#027d7b";
+    vtpm = {
+      enable = true;
+      runInVM = config.ghaf.virtualization.storagevm-encryption.enable;
+      basePort = 9170;
+    };
     applications = [
       {
-        name = "APPStore";
+        name = "App Store";
         description = "Appstore to install Flatpak applications";
         packages = [
-          pkgs.gnome-software
-          pkgs.flatpak
+          pkgs.cosmic-store
         ];
 
-        icon = "${pkgs.papirus-icon-theme}/share/icons/Papirus/64x64/apps/rocs.svg";
-        command = "gnome-software";
+        icon = "rocs";
+        command = "cosmic-store";
       }
     ];
     extraModules = [
       {
         services.flatpak.enable = true;
         security.rtkit.enable = true;
+        services.packagekit.enable = true;
+
+        security.polkit = {
+          enable = true;
+          debug = true;
+          extraConfig = ''
+              polkit.addRule(function(action, subject) {
+                if (action.id.startsWith("org.freedesktop.Flatpak.") &&
+                    subject.user == "${config.ghaf.users.appUser.name}") {
+                      return polkit.Result.YES;
+                }
+            });
+          '';
+        };
+
         xdg.portal = {
           enable = true;
           extraPortals = [
-            pkgs.xdg-desktop-portal-gtk
+            pkgs.xdg-desktop-portal-cosmic
           ];
           config = {
             common = {
@@ -42,19 +61,21 @@
             };
           };
         };
+
         systemd.services.flatpak-repo = {
+          description = "Add Flathub system-wide Flatpak repository";
           wantedBy = [ "multi-user.target" ];
+          after = [ "network-online.target" ];
+          requires = [ "network-online.target" ];
+          serviceConfig = {
+            Type = "oneshot";
+          };
           path = [ pkgs.flatpak ];
           script = ''
-            flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            flatpak --system remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            flatpak update --appstream --system
           '';
         };
-
-        imports = [
-          ../services/wireguard-gui/wireguard-gui.nix
-        ];
-        # Enable WireGuard GUI
-        ghaf.reference.services.wireguard-gui.enable = config.ghaf.reference.services.wireguard-gui;
       }
     ];
   };
