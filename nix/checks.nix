3888d3ab82480444999cef1c282765c8f4a52437
Brian McGillion <bmg.avoin@gmail.com>
2025-11-11 00:40:52 +0400

    DUMMY DONT MERGE
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- nix/checks.nix
+++ nix/checks.nix
@@ -1,5 +1,3 @@
-# SPDX-FileCopyrightText: 2022-2026 TII (SSRC) and the Ghaf contributors
-# SPDX-License-Identifier: Apache-2.0
 { inputs, ... }:
 {
   imports = [ inputs.git-hooks-nix.flakeModule ];
