219ba3daee93883286dd750dbc1e29bcca1bcae3
Kajus Naujokaitis <kajus.naujokaitis@unikie.com>
2025-11-24 13:12:44 +0200

    cosmic-applets: hide some buttons
    - audio-applet: patch to hide sound settings button
    - bluetooth-applet: patch to hide bluetooth settings button
    - network-applet: patch to hide airplane mode toggle
    
    Signed-off-by: Kajus Naujokaitis <kajus.naujokaitis@unikie.com>

--- overlays/custom-packages/cosmic/cosmic-applets/default.nix
+++ overlays/custom-packages/cosmic/cosmic-applets/default.nix
@@ -1,9 +1,18 @@
 # SPDX-FileCopyrightText: 2022-2026 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
-# Add DBUS proxy socket for audio, network, and Bluetooth applets
+# Add DBUS proxy socket for audio and Bluetooth applets
+# audio-applet: patch to hide sound settings button
+# bluetooth-applet: patch to hide bluetooth settings button
+# network-applet: patch to hide airplane mode toggle
 # Ref: https://github.com/pop-os/cosmic-applets
 { prev }:
 prev.cosmic-applets.overrideAttrs (oldAttrs: {
+  patches = oldAttrs.patches ++ [
+    # audio and bluetooth patches should be removed when dbus-proxy allows
+    ./0001-audio-applet-hide-sound-settings-button.patch
+    ./0001-bluetooth-applet-hide-bluetooth-settings-button.patch
+    ./0001-network-applet-hide-airplane-mode-toggle.patch
+  ];
   postInstall = oldAttrs.postInstall or "" + ''
     sed -i 's|^Exec=.*|Exec=env DBUS_SYSTEM_BUS_ADDRESS=unix:path=/tmp/dbusproxy_snd.sock cosmic-applet-audio|' $out/share/applications/com.system76.CosmicAppletAudio.desktop
     sed -i 's|^Exec=.*|Exec=env DBUS_SYSTEM_BUS_ADDRESS=unix:path=/tmp/dbusproxy_snd.sock cosmic-applet-bluetooth|' $out/share/applications/com.system76.CosmicAppletBluetooth.desktop
