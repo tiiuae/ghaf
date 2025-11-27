b49f5124971947b9927113d29ebde47a6aae76c7
vadik likholetov <vadikas@gmail.com>
2025-11-27 14:51:58 +0000

    Create /persist/sysupdate on the host and mount it into netvm.
    Host Configuration: Added an entry to systemd.tmpfiles.rules in modules/microvm/host/microvm-host.nix to ensure the /persist/sysupdate directory is created on the host with 0755 permissions owned by root.
    
    modules/microvm/host/microvm-host.nix
    
    "d /persist/sysupdate 0755 root root -"
    NetVM Configuration: Added a share configuration in modules/microvm/sysvms/netvm.nix to mount the host's /persist/sysupdate to /persist/sysupdate inside the netvm using virtiofs.
    
    modules/microvm/sysvms/netvm.nix
    
    {
      tag = "sysupdate";
      source = "/persist/sysupdate";
      mountPoint = "/persist/sysupdate";
      proto = "virtiofs";
    }
    
    Signed-off-by: vadik likholetov <vadikas@gmail.com>
    Signed-off-by: Jörg Thalheim <joerg@thalheim.io>

--- modules/microvm/sysvms/netvm-base.nix
+++ modules/microvm/sysvms/netvm-base.nix
@@ -215,6 +215,12 @@ in
         mountPoint = "/etc/common";
         proto = "virtiofs";
       }
+      {
+        tag = "sysupdate";
+        source = "/persist/sysupdate";
+        mountPoint = "/persist/sysupdate";
+        proto = "virtiofs";
+      }
     ]
     # Shared store (when not using storeOnDisk)
     ++ lib.optionals (!(globalConfig.storage.storeOnDisk or false)) [
