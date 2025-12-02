a8cbf269f023ce743641d7dac2d466d22c9aac3c
Brian McGillion <bmg.avoin@gmail.com>
2025-12-02 14:50:36 +0400

    shrink: reduce the image sizes
    Put the test tools into the vms where they are actually needed.
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

---
+++ modules/development/dt-host.nix
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2022-2026 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.ghaf.development.debug.tools.host;
+
+  rm-linux-bootmgrs = pkgs.callPackage ./scripts/rm_linux_bootmgr_entries.nix { };
+
+  inherit (lib) rmDesktopEntries;
+in
+{
+  options.ghaf.development.debug.tools.host = {
+    enable = lib.mkEnableOption "Host Debugging Tools";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages =
+      lib.optionals (config.nixpkgs.hostPlatform.system == "x86_64-linux")
+        (rmDesktopEntries [
+          rm-linux-bootmgrs
+          # To inspect LUKS partitions metadata
+          pkgs.cryptsetup
+          # check hardware info
+          pkgs.lshw
+        ]);
+  };
+}
