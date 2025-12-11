3e9c7f3c20277cb2dec200c6e470b086542c82ba
Alexander Nikolaev <alexander.nikolaev@tii.ae>
2025-12-11 17:21:09 +0200

    Return encrypted swap in place
    Signed-off-by: Alexander Nikolaev <alexander.nikolaev@tii.ae>

--- modules/partitioning/repart-common.nix
+++ modules/partitioning/repart-common.nix
@@ -2,10 +2,15 @@
 # SPDX-License-Identifier: Apache-2.0
 {
   config,
+  pkgs,
   ...
 }:
 let
   inherit (config.ghaf.partitions) definition;
+  defaultPassword = pkgs.writeTextFile {
+    name = "disko-default-password";
+    text = "";
+  };
 in
 {
   boot.initrd.systemd.enable = true;
@@ -18,6 +23,14 @@ in
 
   systemd.repart = {
     enable = true;
+    extraArgs =
+      if config.ghaf.storage.encryption.enable then
+        [
+          "--key-file"
+          defaultPassword
+        ]
+      else
+        [ ];
     partitions = {
       "10-root-a" = {
         Type = "root";
@@ -61,6 +74,9 @@ in
         UUID = "0657fd6d-a4ab-43c4-84e5-0933c84b4f4f";
         SizeMinBytes = definition.swap.size;
         SizeMaxBytes = definition.swap.size;
+        # When Encrypt is "key-file" and the key file isn't specified, the
+        # disk will be LUKS formatted with an empty passphrase
+        Encrypt = if config.ghaf.storage.encryption.enable then "key-file" else "off";
       };
       # Persistence partition.
       "50-persist" = {
@@ -68,7 +84,7 @@ in
         Label = definition.persist.label;
         Format = "btrfs";
         SizeMinBytes = definition.persist.size;
-        MakeDirectories = builtins.toString [
+        MakeDirectories = toString [
           "/storagevm"
         ];
         UUID = "20936304-3d57-49c2-8762-bbba07edbe75";
