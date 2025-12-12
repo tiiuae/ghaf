1c508c382c3b529fe95ef2fdca643df51b5f5a05
Kajus Naujokaitis <kajus.naujokaitis@unikie.com>
2025-12-12 14:23:42 +0200

    desktop: add proper light/dark themes, unify chrome/chromium colors
    - added light and dark ghaf themes
    - changed chrome and chromium vms to use the same color
    - changed chrome-vm color to a lighter shade of red
    
    Signed-off-by: Kajus Naujokaitis <kajus.naujokaitis@unikie.com>

--- modules/reference/appvms/chromium.nix
+++ modules/reference/appvms/chromium.nix
@@ -12,7 +12,7 @@
     packages = lib.optional config.ghaf.development.debug.tools.enable pkgs.alsa-utils;
     ramMb = 6144;
     cores = 4;
-    borderColor = "#B83232";
+    borderColor = "#9C0000";
     ghafAudio.enable = true;
     vtpm = {
       enable = true;
