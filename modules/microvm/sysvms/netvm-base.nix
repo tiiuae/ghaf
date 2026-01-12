99bee306c18d299b0038efe89f23fc616e3dd4b1
Alexander Nikolaev <alexander.nikolaev@tii.ae>
2026-01-12 13:54:54 +0200

    Set permissions for mountpoint
    Signed-off-by: Alexander Nikolaev <alexander.nikolaev@tii.ae>
    Signed-off-by: Jörg Thalheim <joerg@thalheim.io>

--- modules/microvm/sysvms/netvm-base.nix
+++ modules/microvm/sysvms/netvm-base.nix
@@ -199,6 +199,7 @@ in
       allowedUDPPorts = [ dnsPort ];
     };
 
+  systemd.tmpfiles.rules = [ "d /persist/sysupdate 0755 ghaf root -" ]; # Set permissions for mountpoint
   microvm = {
     # Optimize is disabled because when it is enabled, qemu is built without libusb
     optimize.enable = false;
