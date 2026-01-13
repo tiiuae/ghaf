90dbdc71565352fee003c8cf3913b535a01b5cac
Alexander Nikolaev <alexander.nikolaev@tii.ae>
2026-01-13 13:52:13 +0200

    feat: add lvm2 to host debug tools
    Signed-off-by: Alexander Nikolaev <alexander.nikolaev@tii.ae>

--- modules/development/dt-host.nix
+++ modules/development/dt-host.nix
@@ -30,6 +30,7 @@ in
         rm-linux-bootmgrs
         # To inspect LUKS partitions metadata
         pkgs.cryptsetup
+        pkgs.lvm2
         # check hardware info
         pkgs.lshw
         # List microvm status
