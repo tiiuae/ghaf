32266f4d850a7bad4d6a8f9e9f27d4c01a42a285
Brian McGillion <bmg.avoin@gmail.com>
2026-01-27 11:05:27 +0400

    cosmic-applet: fixup patches for latest version
    Co-authored-by: Kajus Naujokaitis <kajus.naujokaitis@unikie.com>
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- overlays/custom-packages/cosmic/cosmic-applets/default.nix
+++ overlays/custom-packages/cosmic/cosmic-applets/default.nix
@@ -9,7 +9,7 @@ prev.cosmic-applets.overrideAttrs (oldAttrs: {
   patches = oldAttrs.patches ++ [
     # audio and bluetooth patches should be removed when dbus-proxy allows
     ./0001-bluetooth-applet-hide-bluetooth-settings-button.patch
-    ./0001-network-applet-hide-airplane-mode-toggle.patch
+    ./0002-network-applet-hide-airplane-mode-toggle.patch
   ];
   postInstall = oldAttrs.postInstall or "" + ''
     sed -i 's|^Exec=.*|Exec=env PIPEWIRE_RUNTIME_DIR=/tmp cosmic-applet-audio|' $out/share/applications/com.system76.CosmicAppletAudio.desktop
