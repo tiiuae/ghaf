b55ed065b59e630a6d944b6b54d52a28ab247e1b
Brian McGillion <bmg.avoin@gmail.com>
2026-02-02 11:32:29 +0400

    efitools: fix cross compilation
    need to apply some overlays to fix the efitools and sbsign cross
    compilation problems
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- overlays/cross-compilation/default.nix
+++ overlays/cross-compilation/default.nix
@@ -27,4 +27,74 @@
           python-prev.setuptools-rust;
     })
   ];
+
+  # Fix for sbsigntool cross-compilation.
+  # Multiple issues need to be addressed:
+  # 1. The create-ccan-tree script uses getopt which is not available in nativeBuildInputs.
+  # 2. The configure script uses `uname -m` to detect EFI_ARCH, which returns the build
+  #    machine architecture instead of the target architecture during cross-compilation.
+  # 3. The lib/ccan Makefile uses hardcoded `ar` instead of the autoconf-detected AR.
+  # This fix adds util-linux for getopt, patches configure.ac to use the correct
+  # target architecture, and sets AR/RANLIB for make.
+  sbsigntool = prev.sbsigntool.overrideAttrs (oldAttrs: {
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [
+      final.buildPackages.util-linux
+    ];
+    makeFlags = (oldAttrs.makeFlags or [ ]) ++ [
+      "AR=${final.stdenv.cc.targetPrefix}ar"
+      "RANLIB=${final.stdenv.cc.targetPrefix}ranlib"
+    ];
+    configurePhase = ''
+      runHook preConfigure
+
+      substituteInPlace configure.ac --replace-warn "@@NIX_GNUEFI@@" "${final.gnu-efi}"
+
+      # Fix EFI_ARCH to use target architecture instead of build machine's uname -m
+      substituteInPlace configure.ac \
+        --replace-warn 'EFI_ARCH=$(uname -m | sed' 'EFI_ARCH=$(echo ${final.stdenv.hostPlatform.parsed.cpu.name} | sed'
+
+      lib/ccan.git/tools/create-ccan-tree --build-type=automake lib/ccan "talloc read_write_all build_assert array_size endian"
+      touch AUTHORS
+      touch ChangeLog
+
+      echo "SUBDIRS = lib/ccan src docs" >> Makefile.am
+
+      aclocal
+      autoheader
+      autoconf
+      automake --add-missing -Wno-portability
+
+      ./configure --prefix=$out
+
+      runHook postConfigure
+    '';
+  });
+
+  # Fix for efitools cross-compilation.
+  # The Make.rules uses `uname -m` to detect ARCH, which returns the build machine
+  # architecture instead of the target architecture during cross-compilation.
+  # This causes x86_64-specific compiler flags (like -mno-red-zone) to be used
+  # when building for aarch64, and wrong include paths to be used.
+  # Also, the Makefile uses hardcoded `ar`, `nm`, and `objcopy` instead of the
+  # cross-toolchain versions.
+  # Additionally, openssl is needed at build time for certificate generation.
+  efitools = prev.efitools.overrideAttrs (oldAttrs: {
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [
+      final.buildPackages.openssl
+      final.buildPackages.sbsigntool
+    ];
+    postPatch = (oldAttrs.postPatch or "") + ''
+      # Fix ar to use $(AR) variable
+      substituteInPlace Make.rules --replace-warn 'ar rcv' '$(AR) rcv'
+      # Fix nm to use $(NM) variable
+      substituteInPlace Make.rules --replace-warn 'nm -D' '$(NM) -D'
+      # Fix objcopy to use the cross-toolchain version
+      substituteInPlace Make.rules --replace-warn 'OBJCOPY		= objcopy' 'OBJCOPY		= ${final.stdenv.cc.targetPrefix}objcopy'
+    '';
+    makeFlags = (oldAttrs.makeFlags or [ ]) ++ [
+      "ARCH=${final.stdenv.hostPlatform.parsed.cpu.name}"
+      "AR=${final.stdenv.cc.targetPrefix}ar"
+      "NM=${final.stdenv.cc.targetPrefix}nm"
+    ];
+  });
 })
