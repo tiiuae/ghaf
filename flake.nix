23c5131e840c56e614c98a5c05492f9d435a164a
Brian McGillion <bmg.avoin@gmail.com>
2026-02-02 10:53:49 +0400

    bump: start feb bump
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- flake.nix
+++ flake.nix
@@ -133,7 +133,7 @@
     # Nvidia Orin support for NixOS
     jetpack-nixos = {
       #url = "github:anduril/jetpack-nixos";
-      url = "github:tiiuae/jetpack-nixos/bump-jan-rebase";
+      url = "github:tiiuae/jetpack-nixos/feb-rebase";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
