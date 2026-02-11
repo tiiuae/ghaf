52171115104fb136031355bd477d3c37b966aa93
Brian McGillion <bmg.avoin@gmail.com>
2026-02-11 23:22:29 +0400

    docs: bump the docs
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- docs/default.nix
+++ docs/default.nix
@@ -64,6 +64,6 @@ buildNpmPackage (_finalAttrs: {
     runHook postInstall
   '';
 
-  npmDepsHash = "sha256-W/NRypR7xAejjhLUCefgdenyh2tNWg0neyY2/iUKr2g=";
+  npmDepsHash = "sha256-uLTiakE1vu+atwPyo6srqtLsx5BVEw7loQuxd7oqAj0=";
 
 })
