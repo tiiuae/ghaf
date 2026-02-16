1510009323837880c7af654c2a9441340aebd2f0
Ganga Ram <Ganga.Ram@tii.ae>
2026-02-16 15:37:46 +0400

    enable policy admin in admin vm
    - update givc configs for yubikey and boot-ui
    
    Signed-off-by: Ganga Ram <Ganga.Ram@tii.ae>

--- modules/microvm/sysvms/adminvm-base.nix
+++ modules/microvm/sysvms/adminvm-base.nix
@@ -25,6 +25,7 @@
 let
   vmName = "admin-vm";
   timezoneEnabled = lib.ghaf.features.isEnabledFor globalConfig "timezone" vmName;
+  policyDir = "/etc/admin-policies";
 in
 {
   _file = ./adminvm-base.nix;
@@ -79,6 +80,9 @@ in
     };
 
     givc.adminvm.enable = true;
+    givc.policyAdmin.enable = true;
+    givc.policyAdmin.storePath = policyDir;
+    givc.policyAdmin.updater.perPolicy.enable = true;
 
     # Enable dynamic hostname export for VMs
     identity.vmHostNameExport.enable = true;
@@ -91,7 +95,10 @@ in
         "/etc/locale-givc.conf"
         "/etc/timezone.conf"
       ];
-      directories = lib.mkIf (globalConfig.storage.encryption.enable or false) [
+      directories = [
+        policyDir
+      ]
+      ++ lib.optionals (globalConfig.storage.encryption.enable or false) [
         "/var/lib/swtpm"
       ];
       encryption.enable = globalConfig.storage.encryption.enable or false;
