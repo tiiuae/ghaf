540e8eff0ad7eb2ea060cbe96d4ae798cef9eed8
Samuli Leivo <samuli.leivo@unikie.com>
2026-02-25 12:31:56 +0200

    Update smoke test
    Update ci-test-automation lock to include
    improvements done for smoke-test env.
    
    In local testing exact form of DEVICE name
    is not relevant but DEVICE_TYPE has to match
    with one of the options. So decided to use
    DEVICE_TYPE both for DEVICE and DEVICE_TYPE
    variables.
    
    Add option to define username / user password,
    instead of smoke test using the defaults.
    
    Use cleaner case block instead of if else.
    
    Add 'result-send' command which sends the Robot Framework
    result files to chrome-vm for visualization with the
    browser. I found this helpful if running the tests locally
    from the ghaf laptop itself.
    
    Signed-off-by: Samuli Leivo <samuli.leivo@unikie.com>

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -90,15 +90,20 @@
                                   127.0.0.1 for orin-agx
                                   192.168.100.1 for lenovo-x1 and orin-nx)
                 -d  --device      Device name of the target. Use exactly one of these options:
-                                    Lenovo-X1
-                                    Orin-AGX
-                                    Orin-NX
-                                    NUC
-                                    darter-pro
-                                    dell-7330
-                -p  --password    Password for the ghaf user
+                                  orin-agx
+                                  orin-agx-64
+                                  orin-nx
+                                  lenovo-x1
+                                  dell-7330
+                                  darter-pro
+                                  x1-sec-boot
+                -p  --password    Password for the ghaf (admin) user
 
                 Optional arguments:
+                --username        Username
+                                  Defaults to 'testuser'
+                --userpasswd    Password for the normal user
+                                  Defaults to 'testpw'
                 -t  --tag         Test tag which defines which test cases will be run.
                                   Defaults to 'pre-merge'.
                 -c  --commit      This can be commit hash or any identifier.
@@ -124,50 +129,39 @@
                 outputdir="/tmp/test_results"
                 threads=8
                 threads_manual_set=false
+                username="testuser"
+                userpasswd="testpw"
 
                 while [ ''$# -gt 0 ]; do
-                  if [[ ''$1 == "-i" || ''$1 == "--ip" ]]; then
-                    ip="''$2"
-                    shift
-                  elif [[ ''$1 == "-d" || ''$1 == "--device" ]]; then
-                    device="''$2"
-                    shift
-                  elif [[ ''$1 == "-p" || ''$1 == "--password" ]]; then
-                    pw="''$2"
-                    shift
-                  elif [[ ''$1 == "-t" || ''$1 == "--tag" ]]; then
-                    tag="''$2"
-                    shift
-                  elif [[ ''$1 == "-c" || ''$1 == "--commit" ]]; then
-                    commit="''$2"
-                    shift
-                  elif [[ ''$1 == "-n" || ''$1 == "--threads" ]]; then
-                    threads="''$2"
-                    threads_manual_set=true
-                    shift
-                  elif [[ ''$1 == "-f" || ''$1 == "--config" ]]; then
-                    configpath="''$2"
-                    shift
-                  elif [[ ''$1 == "-o" || ''$1 == "--outputdir" ]]; then
-                    outputdir="''$2"
-                    shift
-                  else
-                    echo "Unknown option: ''$1"
-                    exit 1
-                  fi
-                  shift
+                  case "''$1" in
+                    (-i|--ip)        ip="''$2";;
+                    (-d|--device)    device="''$2";;
+                    (-p|--password)  pw="''$2";;
+                    (--username)     username="''$2";;
+                    (--userpasswd)   userpasswd="''$2";;
+                    (-t|--tag)       tag="''$2";;
+                    (-c|--commit)    commit="''$2";;
+                    (--threads)      threads="''$2";;
+                    (--config)       configpath="''$2";;
+                    (--outputdir)    outputdir="''$2";;
+                    (*)
+                      echo "Unknown option: ''$1"
+                      exit 1
+                      ;;
+                  esac
+                  shift 2
                 done
 
                 if [[ ''${threads_manual_set} == false ]]; then
-                  grep -q "X1" <<< "''$device" && threads=20
-                  grep -q "AGX" <<< "''$device" && threads=12
+                  grep -q "x1" <<< "''$device" && threads=20
+                  grep -q "agx" <<< "''$device" && threads=12
                   grep -q "darter-pro" <<< "''$device" && threads=16
                 fi
 
                 cd ${inputs.ci-test-automation.outPath}/Robot-Framework/test-suites
                 ${
                   inputs.ci-test-automation.packages.${system}.ghaf-robot
-                }/bin/ghaf-robot -v CONFIG_PATH:''${configpath} -v DEVICE_IP_ADDRESS:''${ip} -v THREADS_NUMBER:''${threads} -v COMMIT_HASH:''${commit} -v DEVICE:''${device} -v DEVICE_TYPE:''${device} -v PASSWORD:''${pw} -i ''${device,,}AND''${tag} --outputdir ''${outputdir} .
+                }/bin/ghaf-robot -v CONFIG_PATH:''${configpath} -v DEVICE_IP_ADDRESS:''${ip} -v THREADS_NUMBER:''${threads} -v COMMIT_HASH:''${commit} -v DEVICE:''${device} -v DEVICE_TYPE:''${device} -v PASSWORD:''${pw} -v USER_LOGIN:''${username} -v USER_PASSWORD:''${userpasswd} -i ''${device,,}AND''${tag} --outputdir ''${outputdir} .
               '';
               category = "test";
             }
@@ -175,6 +169,35 @@
               help = "Show path to ci-test-automation repo in nix store";
               name = "robot-path";
               command = "echo ${inputs.ci-test-automation.outPath}";
+              category = "test";
+            }
+            {
+              help = "
+                If running smoke tests locally from ghaf laptop target this
+                command can be called after the test run to send the test
+                report files to chrome-vm for visualization with the browser:
+                file:///tmp/report.html
+
+                Required arguments:
+                --password    Password for the ghaf (admin) user
+                --resultdir   Path to the result files
+              ";
+              name = "results-send";
+              command = ''
+                while [ ''$# -gt 0 ]; do
+                  case "''$1" in
+                    (--password)   password="''$2";;
+                    (--resultdir)  resultdir="''$2";;
+                    (*)
+                      echo "Unknown option: ''$1"
+                      exit 1
+                      ;;
+                  esac
+                  shift 2
+                done
+
+                python ${inputs.ci-test-automation.outPath}/Robot-Framework/lib/send_report.py ''${password} ''${resultdir}
+              '';
               category = "test";
             }
           ];
