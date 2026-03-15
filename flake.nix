d576311b222ed270b0e8649ad59b63f06e81cef3
vadik likholetov <vadikas@gmail.com>
2026-03-15 18:44:17 +0000

    logging/givc: enable nvidia logging with cross-aware GIVC pin
    Enable GIVC and logging wiring for Orin targets, remove temporary host TLS guard logic, and pin ghaf-givc to the cross-aware branch revision used for x86_64 -> aarch64 flash builds. Also drop redundant Jetson logging endpoint override now provided by global defaults.
    
    Signed-off-by: vadik likholetov <vadikas@gmail.com>

--- flake.nix
+++ flake.nix
@@ -105,7 +105,7 @@
 
     # Ghaf Inter VM communication and control library
     givc = {
-      url = "github:tiiuae/ghaf-givc";
+      url = "github:vadika/ghaf-givc/vadika/cross-givc-packages";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-parts.follows = "flake-parts";
--- modules/reference/hardware/jetpack/nvidia-jetson-orin/jetson-orin.nix
+++ modules/reference/hardware/jetpack/nvidia-jetson-orin/jetson-orin.nix
@@ -117,6 +117,11 @@ in
     hardware.nvidia-jetpack.kernel.version = "${cfg.kernelVersion}";
     nixpkgs.hostPlatform.system = "aarch64-linux";
 
+    ghaf.givc.enable = true;
+    ghaf.givc.debug = false;
+
+    ghaf.global-config.givc.enable = true;
+    ghaf.global-config.logging.enable = true;
     ghaf.hardware = {
       aarch64.systemd-boot-dtb.enable = true;
       passthrough = {
