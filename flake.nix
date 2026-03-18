d8c1c8d0b9f4ed9fde5d2599b4faa8e5ba6d7e57
vadik likholetov <vadikas@gmail.com>
2026-03-18 21:38:57 +0000

    jetson-orin: consume upstream signed flash support
    Switch ghaf to a jetpack-nixos branch that includes signed sd-image flashing support and remove the local nvidia-jetpack overlay/package duplication. This keeps flashing behavior in jetpack-nixos and reduces maintenance in ghaf.
    
    Signed-off-by: vadik likholetov <vadikas@gmail.com>

--- flake.nix
+++ flake.nix
@@ -133,7 +133,7 @@
     # Nvidia Orin support for NixOS
     jetpack-nixos = {
       #url = "github:anduril/jetpack-nixos";
-      url = "github:tiiuae/jetpack-nixos/feb-rebase";
+      url = "github:vadika/jetpack-nixos/refactor/flash-signed-sd-image";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
