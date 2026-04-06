0e8733db22c488970f0863bc4821eafff3bf6d40
Ganga Ram <Ganga.Ram@tii.ae>
2026-04-06 11:24:51 +0400

    spire version update
    - fixes compilation issue on mainline
    
    Signed-off-by: Ganga Ram <Ganga.Ram@tii.ae>

---
+++ overlays/custom-packages/spire/default.nix
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2022-2026 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{ prev }:
+prev.spire.overrideAttrs (_old: rec {
+  version = "1.14.4";
+
+  src = prev.fetchFromGitHub {
+    owner = "spiffe";
+    repo = "spire";
+    rev = "v${version}";
+    hash = "sha256-Ga4fV1a3vlOez12a6lMHoh2CUF9Rkclvjz2FScu6krc=";
+  };
+
+  vendorHash = "sha256-Ajoxxpf6oWW6jioMTgeyaIszVhp4j7E2+msE0nhfKpk=";
+})
