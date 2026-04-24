a8217ee5fbf244b62cd9284d20e057159ae462e2
Ganga Ram <Ganga.Ram@tii.ae>
2026-04-24 14:05:23 +0400

    chore(admin-vm): increase memory to 1g
    Signed-off-by: Ganga Ram <Ganga.Ram@tii.ae>

--- modules/microvm/sysvms/adminvm-base.nix
+++ modules/microvm/sysvms/adminvm-base.nix
@@ -197,7 +197,7 @@ in
     optimize.enable = false;
     # Sensible defaults - can be overridden via vmConfig
     vcpu = lib.mkDefault 2;
-    mem = lib.mkDefault 512;
+    mem = lib.mkDefault 1024;
     #TODO: Add back support cloud-hypervisor
     #the system fails to switch root to the stage2 with cloud-hypervisor
     hypervisor = "qemu";
