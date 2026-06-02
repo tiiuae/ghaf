1f54f5ed95c59cb8a829d6a99cd2730449e4b8e3
Brian McGillion <bmg.avoin@gmail.com>
2026-06-02 10:19:26 +0400

    bump(docs): upgrade to latest
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- docs/default.nix
+++ docs/default.nix
@@ -64,6 +64,6 @@ buildNpmPackage (_finalAttrs: {
     runHook postInstall
   '';
 
-  npmDepsHash = "sha256-QalSE9wyz4/rykwrAhhdkiO23uBn/0zLz2aQWF9DGpk=";
+  npmDepsHash = "sha256-q4LLzvxqmsqHiks2IW8ANVVip5rdcQpevpJER3k8Eb8=";
 
 })
