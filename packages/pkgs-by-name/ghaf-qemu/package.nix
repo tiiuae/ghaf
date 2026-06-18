d9dc7c4bb31caef9e6f789599c2952cbad31b657
Kajus Naujokaitis <kajus.naujokaitis@unikie.com>
2026-06-18 15:10:36 +0300

    fix(qemu): rebase patches for 11.0.1, rename patches
    Rebased patches for 11.0.1
    Renamed patches
    Disabled building docs due to compilation error
    
    Signed-off-by: Kajus Naujokaitis <kajus.naujokaitis@unikie.com>

--- packages/pkgs-by-name/ghaf-qemu/package.nix
+++ packages/pkgs-by-name/ghaf-qemu/package.nix
@@ -11,30 +11,47 @@
   qemu_kvm,
   ...
 }:
-qemu_kvm.overrideAttrs (
-  _final: prev: {
-    patches =
-      prev.patches
-      ++ [
-        # Shared memory support for inter-VM communication
-        ./patches/0001-ivshmem-flat-memory-support.patch
-        # Increase TPM command timeout
-        ./patches/0002-Increase-timeout-in-tpm_util_request.patch
-        # USB host autoscan for bus/addr passthrough
-        ./patches/usb-host-enable-autoscan-for-bus-addr.patch
-      ]
-      ++ lib.optionals stdenv.hostPlatform.isx86_64 [
-        # ACPI battery/power management for VMs
-        # https://github.com/blochl/qemu/pull/3
-        # TODO: remove when merged upstream
-        ./patches/0001-hw-acpi-Support-extended-GPE-handling-for-additional.patch
-        ./patches/0002-hw-acpi-Introduce-the-QEMU-Battery.patch
-        ./patches/0003-hw-acpi-Introduce-the-QEMU-AC-adapter.patch
-        ./patches/0004-hw-acpi-Introduce-the-QEMU-lid-button.patch
-      ];
+(qemu_kvm.override {
+  # Display backends
+  # sdlSupport = false;
+  # gtkSupport = false;
+  # vncSupport = false;
+  # virglSupport = false;
+  # ncursesSupport = false;
+  # Audio backends
+  # alsaSupport = false;
+  # pulseSupport = false;
+  # jackSupport = false;
+  # Storage/peripheral
+  # libiscsiSupport = false;
+  # smartcardSupport = false;
+  # Sphinx-based doc build fails (ninja step 1837); docs not needed in production
+  enableDocs = false;
+}).overrideAttrs
+  (
+    _final: prev: {
+      patches =
+        prev.patches
+        ++ [
+          # Shared memory support for inter-VM communication
+          ./patches/0006-ivshmem-flat-memory-support.patch
+          # Increase TPM command timeout
+          ./patches/0005-Increase-timeout-in-tpm_util_request.patch
+          # USB host autoscan for bus/addr passthrough
+          ./patches/0007-usb-host-enable-autoscan-for-bus-addr.patch
+        ]
+        ++ lib.optionals stdenv.hostPlatform.isx86_64 [
+          # ACPI battery/power management for VMs
+          # https://github.com/blochl/qemu/pull/3
+          # TODO: remove when merged upstream
+          ./patches/0001-hw-acpi-Support-extended-GPE-handling-for-additional.patch
+          ./patches/0002-hw-acpi-Introduce-the-QEMU-Battery.patch
+          ./patches/0003-hw-acpi-Introduce-the-QEMU-AC-adapter.patch
+          ./patches/0004-hw-acpi-Introduce-the-QEMU-lid-button.patch
+        ];
 
-    postInstall = (prev.postInstall or "") + ''
-      cp contrib/ivshmem-server/ivshmem-server $out/bin
-    '';
-  }
-)
+      postInstall = (prev.postInstall or "") + ''
+        cp contrib/ivshmem-server/ivshmem-server $out/bin
+      '';
+    }
+  )
