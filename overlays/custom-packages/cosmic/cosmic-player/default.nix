379bbc02b53114ae70c770ce29b18c2ff4d5194e
Janne Pirskanen <janne.pirskanen@tii.ae>
2026-06-23 12:14:19 +0300

    fix(media): Add cosmic player plugin path
    Signed-off-by: Janne Pirskanen <janne.pirskanen@tii.ae>

---
+++ overlays/custom-packages/cosmic/cosmic-player/default.nix
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2026 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+
+# Add missing plugins and path
+
+{ prev }:
+(prev.cosmic-player.overrideAttrs (oldAttrs: {
+  dontWrapLibcosmicApp = true;
+
+  postFixup = oldAttrs.postFixup or "" + ''
+    wrapLibcosmicApp "$out/bin/cosmic-player" \
+      --prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0"
+  '';
+}))
