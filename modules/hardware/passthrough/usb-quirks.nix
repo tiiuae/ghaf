837646f006fa2affa2195660c5694c6da0c42e07
Emrah Billur <emrah.billur@tii.ae>
2026-07-06 12:05:16 +0300

    fix(net-vm):ax88179 power management quirks
    Signed-off-by: Emrah Billur <emrah.billur@tii.ae>

--- modules/hardware/passthrough/usb-quirks.nix
+++ modules/hardware/passthrough/usb-quirks.nix
@@ -14,7 +14,8 @@ let
   # https://github.com/eihqnh/.nixconfig/blob/a685389652c3df60b1ece49125f3e2db993aeebf/common.nix#L102
   # https://docs.redhat.com/en/documentation/red_hat_enterprise_linux/7/html/7.7_release_notes/kernel_parameters_changes
   quirks = [
-    "2357:0601:k" # TP-Link’s USB 3.0 Ethernet adapters (AX88179 chipset)
+    "2357:0601:k" # TP-Link’s USB 3.0 Ethernet adapters (Realtek chipset)
+    "0b95:1790:k" # TP-Link's ASIX chipset USB 3.x ethernet adapters(AX88179 chipset)
     "0bda:8153:k" # Realtek Semiconductor Corp. RTL8153 Gigabit Ethernet Adapter
   ];
 
