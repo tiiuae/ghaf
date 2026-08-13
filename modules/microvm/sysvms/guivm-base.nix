d94a18f1452a3f847aa448e7dbdbf522700df8a5
Manuel Bluhm <manuel.bluhm@tii.ae>
2026-08-13 17:48:28 +0400

    fix(ssh): remove dead waypipe-ssh
    Remove dead waypipe-ssh setup.
    
    Signed-off-by: Manuel Bluhm <manuel.bluhm@tii.ae>

--- modules/microvm/sysvms/guivm-base.nix
+++ modules/microvm/sysvms/guivm-base.nix
@@ -297,37 +297,6 @@ in
 
   systemd = {
     packages = [ pkgs.blueman ];
-    services."waypipe-ssh-keygen" =
-      let
-        uid =
-          if hostConfig.users.homedUser.enable or false then
-            "${toString (hostConfig.users.homedUser.uid or 1000)}"
-          else
-            "${toString (hostConfig.users.admin.uid or 1000)}";
-        pubDir = hostConfig.security.sshKeys.waypipeSshPublicKeyDir or "/run/waypipe-ssh-public-key";
-        keygenScript = pkgs.writeShellScriptBin "waypipe-ssh-keygen" ''
-          set -xeuo pipefail
-          mkdir -p /run/waypipe-ssh
-          mkdir -p ${pubDir}
-          echo -en "\n\n\n" | ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f /run/waypipe-ssh/id_ed25519 -C ""
-          chown ${uid}:users /run/waypipe-ssh/*
-          cp /run/waypipe-ssh/id_ed25519.pub ${pubDir}/id_ed25519.pub
-          chown -R ${uid}:users ${pubDir}
-        '';
-      in
-      {
-        enable = true;
-        description = "Generate SSH keys for Waypipe";
-        path = [ keygenScript ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-          StandardOutput = "journal";
-          StandardError = "journal";
-          ExecStart = "${keygenScript}/bin/waypipe-ssh-keygen";
-        };
-      };
   };
 
   environment = {
