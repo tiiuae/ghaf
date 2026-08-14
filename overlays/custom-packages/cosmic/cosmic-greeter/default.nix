66f79715d2eb737b47de3ebf616c95c8b6e70c2f
Brian McGillion <bmg.avoin@gmail.com>
2026-08-14 23:49:11 +0400

    fix(cosmic-greeter-daemon): authentication errors
    user home not available, so falls back to "/" as the default home path.
    
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- overlays/custom-packages/cosmic/cosmic-greeter/default.nix
+++ overlays/custom-packages/cosmic/cosmic-greeter/default.nix
@@ -5,5 +5,6 @@ prev.cosmic-greeter.overrideAttrs (oldAttrs: {
   patches = oldAttrs.patches ++ [
     ./0001-Replace-fallback-background-with-Ghaf-default.patch
     ./0002-fix-username-handle-empty-usernames.patch
+    ./0003-skip-config-probe-for-locked-homes.patch
   ];
 })
