33cad2e9c5a8ac1f73680da2744650715184f1b1
Hugo Ros <Hugo.Ros@tii.ae>
2026-08-14 15:36:48 +0300

    fix(orin): Use lib.getExe withSB/noSB for the flash script
    Signed-off-by: Hugo Ros <hros@census-labs.com>

--- targets/nvidia-jetson-orin/flake-module.nix
+++ targets/nvidia-jetson-orin/flake-module.nix
@@ -479,7 +479,6 @@ let
   flashTarget =
     t: qspiOnly:
     let
-      innerName = t.hostConfiguration.config.hardware.nvidia-jetpack.name;
       noSB =
         (t.hostConfiguration.extendModules {
           modules = [
@@ -561,9 +560,9 @@ let
           esac
         done
         if [ "$sb" = 1 ]; then
-          exec ${withSB}/bin/flash-${innerName} "''${args[@]}"
+          exec ${lib.getExe withSB} "''${args[@]}"
         else
-          exec ${noSB}/bin/flash-${innerName} "''${args[@]}"
+          exec ${lib.getExe noSB} "''${args[@]}"
         fi
       '';
     };
