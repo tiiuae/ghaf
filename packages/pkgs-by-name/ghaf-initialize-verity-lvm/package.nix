24ba2988d43b553a921dc7e0fd509889c620d717
vadik likholetov <vadikas@gmail.com>
2026-09-05 11:42:31 +0300

    fix(packages): expose shared image tools in the package scope
    Signed-off-by: vadik likholetov <vadikas@gmail.com>

---
+++ packages/pkgs-by-name/ghaf-initialize-verity-lvm/package.nix
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2026 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{ inputs, stdenv }:
+inputs.ghafpkgs.packages.${stdenv.hostPlatform.system}.ghaf-initialize-verity-lvm
