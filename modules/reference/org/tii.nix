5b07d3cd818a663d2a5f85223e9c1b1d32c338d7
Manuel Bluhm <manuel.bluhm@tii.ae>
2026-09-22 01:07:36 +0400

    feat(org): move firewall updater URL
    Move ghaf's firewall updater URL to org config.
    
    Signed-off-by: Manuel Bluhm <manuel.bluhm@tii.ae>

--- modules/reference/org/tii.nix
+++ modules/reference/org/tii.nix
@@ -24,6 +24,7 @@ in
         };
       };
       management.fleet.url = "https://fleetdm.vedenemo.dev";
+      network.firewallRulesUrl = "https://raw.githubusercontent.com/tiiuae/ghaf-policies/deploy/vm-policies/firewall-rules/iptables.rules";
 
       # TII development AD test domain (formerly hardcoded in ad-users.nix). Realm,
       # KDC servers and LDAP URIs derive from domain and controllers (active-directory/options.nix).
